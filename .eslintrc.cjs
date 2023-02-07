83446c988ea482e18f079cdab4813d35cf3a665f
coyotte508 <coyotte508@gmail.com>
2023-02-07 16:43:34 +0100

    🎉 Add base modules

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "prettier"],
+	root: true,
+	env: {
+		browser: true,
+		amd: true,
+		node: true,
+	},
+	rules: {
+		"@typescript-eslint/no-empty-function": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "error",
+		"@typescript-eslint/consistent-type-imports": "error",
+		"@typescript-eslint/key-spacing": ["error", { align: "value" }],
+	},
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # huggingface.js
-Utilities to use the Hugging Face hub API
+
+Official utilities to use the Hugging Face hub API
+
+See also [TimMikeladze/huggingface](https://github.com/TimMikeladze/huggingface) for a non-official wrapper to use the inference API
