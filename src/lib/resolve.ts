2ca7fa307618392d122471320db0a4fd446ad2a2
coyotte508 <coyotte508@gmail.com>
2023-02-16 00:06:41 +0100

    🐛 Fix throw createApiError

--- src/lib/resolve.ts
+++ src/lib/resolve.ts
@@ -26,7 +26,7 @@ export async function resolve(params: {
 	if (resp.status === 404 && resp.headers.get("X-Error-Code") === "EntryNotFound") {
 		return null;
 	} else if (!resp.ok) {
-		throw createApiError(resp);
+		throw await createApiError(resp);
 	}
 
 	return resp;
