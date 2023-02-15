7730ec81cbf6362fb4791c0591f0b0356ce2c185
coyotte508 <coyotte508@gmail.com>
2023-02-15 17:48:40 +0100

    🔐 Update test credentials

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const HUB_URL = process.env.NODE_ENV === "test" ? "https://hub-ci.huggingface.co" : "https://huggingface.co";
 export const HUB_API_URL = `${HUB_URL}/api`;
-export const TEST_USER = "huggingface-hub.js";
-export const TEST_ACCESS_TOKEN = "hf_huggingface-hub.js";
+export const TEST_USER = "hub.js";
+export const TEST_ACCESS_TOKEN = "hf_hub.js";
