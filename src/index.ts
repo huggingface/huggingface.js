b5e08fdce13ee55348bda35489760f2790a6c8c5
coyotte508 <coyotte508@gmail.com>
2023-02-15 02:44:56 +0100

    🩹 Use 'export type' properly

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
 export * from "./lib";
-export * from "./types";
+// Typescript 5 will add 'export type *'
+export type {
+	AccessToken,
+	ApiCommitDeletedEntry,
+	ApiCommitFile,
+	ApiCommitHeader,
+	ApiCommitLfsFile,
+	ApiCommitOperation,
+	ApiCreateRepoPayload,
+	ApiLfsAction,
+	ApiLfsBatchRequest,
+	ApiLfsBatchResponse,
+	ApiLfsCompleteMultipartRequest,
+	ApiLfsResponseObject,
+	ApiLfsResponseTransfer,
+	ApiPreuploadRequest,
+	ApiPreuploadResponse,
+	Credentials,
+	RepoId,
+	RepoType,
+	SpaceHardwareFlavor,
+	SpaceSdk,
+} from "./types";
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,2 +1,18 @@
-export * from "./api";
-export * from "./repo";
+// Typescript 5 will add 'export type *'
+export type {
+	ApiCommitDeletedEntry,
+	ApiLfsBatchRequest,
+	ApiCommitFile,
+	ApiCommitHeader,
+	ApiCommitLfsFile,
+	ApiCommitOperation,
+	ApiCreateRepoPayload,
+	ApiLfsAction,
+	ApiLfsBatchResponse,
+	ApiLfsCompleteMultipartRequest,
+	ApiLfsResponseObject,
+	ApiLfsResponseTransfer,
+	ApiPreuploadRequest,
+	ApiPreuploadResponse,
+} from "./api";
+export type { AccessToken, Credentials, RepoId, RepoType, SpaceHardwareFlavor, SpaceSdk } from "./repo";
