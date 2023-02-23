fe190bf9627fab7afc44deb19cf3820a151ae4f0
coyotte508 <coyotte508@gmail.com>
2023-02-23 15:43:47 +0100

    ✨ Support specifying files when creating repo

--- packages/hub/src/lib/create-repo.spec.ts
+++ packages/hub/src/lib/create-repo.spec.ts
@@ -2,6 +2,8 @@ import { randomBytes } from "crypto";
 import { TEST_ACCESS_TOKEN, TEST_USER } from "../consts";
 import { createRepo } from "./create-repo";
 import { deleteRepo } from "./delete-repo";
+import { downloadFile } from "./download-file";
+import * as assert from "assert";
 
 describe("createRepo", () => {
 	it("should create a repo", async () => {
@@ -15,7 +17,19 @@ describe("createRepo", () => {
 				name: repoName,
 				type: "model",
 			},
+			files: [{ path: ".gitattributes", content: new Blob(["*.html filter=lfs diff=lfs merge=lfs -text"]) }],
 		});
+
+		const content = await downloadFile({
+			repo: {
+				name: repoName,
+				type: "model",
+			},
+			path: ".gitattributes",
+		});
+
+		assert(content);
+		assert.strictEqual(await content.text(), "*.html filter=lfs diff=lfs merge=lfs -text");
 
 		await deleteRepo({
 			repo: {
--- packages/hub/src/lib/create-repo.ts
+++ packages/hub/src/lib/create-repo.ts
@@ -1,12 +1,17 @@
 import { HUB_URL } from "../consts";
 import { createApiError } from "../error";
 import type { ApiCreateRepoPayload, Credentials, RepoId, SpaceSdk } from "../types";
+import { base64FromBytes } from "../utils";
 
 export async function createRepo(params: {
 	repo:        RepoId;
 	credentials: Credentials;
 	private?:    boolean;
 	license?:    string;
+	/**
+	 * Only a few lightweight files are supported at repo creation
+	 */
+	files?:      Array<{ content: ArrayBuffer | Blob; path: string }>;
 	/** @required for when {@link repo.type} === "space" */
 	sdk?:        SpaceSdk;
 	hubUrl?:     string;
@@ -28,6 +33,17 @@ export async function createRepo(params: {
 				: {
 						type: params.repo.type,
 				  }),
+			files: params.files
+				? await Promise.all(
+						params.files.map(async (file) => ({
+							encoding: "base64",
+							path:     file.path,
+							content:  base64FromBytes(
+								new Uint8Array(file.content instanceof Blob ? await file.content.arrayBuffer() : file.content)
+							),
+						}))
+				  )
+				: undefined,
 		} satisfies ApiCreateRepoPayload),
 		headers: {
 			Authorization:  `Bearer ${params.credentials.accessToken}`,
