fbdfd5635aff09173765d9e8853cff9b9d74bffd
coyotte508 <coyotte508@gmail.com>
2023-03-03 14:09:15 +0100

    🏷️ Export ApiError

--- packages/hub/src/index.ts
+++ packages/hub/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./lib";
 // Typescript 5 will add 'export type *'
 export type { AccessToken, Credentials, RepoId, RepoType, SpaceHardwareFlavor, SpaceSdk } from "./types";
+export { ApiError } from "./error";
