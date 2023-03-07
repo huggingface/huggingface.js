12422d47b414242f420417a772a31f75d78cb7fe
coyotte508 <coyotte508@gmail.com>
2023-03-08 00:32:18 +0100

    ✅ remote rate-limiting test

--- packages/inference/test/HfInference.spec.ts
+++ packages/inference/test/HfInference.spec.ts
@@ -11,28 +11,8 @@ const FLAC_FILE = join(dir, "..", "test", "sample1.flac");
 const CHEETAH_FILE = join(dir, "..", "test", "cheetah.png");
 const CAT_FILE = join(dir, "..", "test", "cats.png");
 
-describe(
-	"HfInference without Autentication",
-	() => {
-		// Testing rate limte without authentication
-		const hf = new HfInference();
-		it("should throw a rate limit too  many requests", async () => {
-			const model = "bert-base-uncased";
-			const input = "[MASK] world!";
-			const promises = [];
-			for (let i = 0; i < 100; i++) {
-				promises.push(hf.fillMask({ model, inputs: input }));
-			}
-			await expect(Promise.all(promises)).rejects.toThrowError(
-				"Rate limit reached. Please log in or use your apiToken"
-			);
-		});
-	},
-	TIMEOUT
-);
-
 if (!process.env.HF_ACCESS_TOKEN) {
-	throw new Error("Set HF_ACCESS_TOKEN in the env to run the tests");
+	console.warn("Set HF_ACCESS_TOKEN in the env to run the tests for better rate limits");
 }
 
 describe.concurrent(
