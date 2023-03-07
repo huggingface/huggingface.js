e685526ad317a5d388372dc7ba844c809d5d2fc4
coyotte508 <coyotte508@gmail.com>
2023-03-08 00:38:54 +0100

    ⚗️

--- packages/inference/test/HfInference.spec.ts
+++ packages/inference/test/HfInference.spec.ts
@@ -13,6 +13,7 @@ const CAT_FILE = join(dir, "..", "test", "cats.png");
 
 if (!process.env.HF_ACCESS_TOKEN) {
 	console.warn("Set HF_ACCESS_TOKEN in the env to run the tests for better rate limits");
+	process.exit(1);
 }
 
 describe.concurrent(
