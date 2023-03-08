4585cb907cc620b438046fd6c136dd509b5db58d
coyotte508 <coyotte508@gmail.com>
2023-03-08 11:40:43 +0100

    ✅ Add test on createRepo output

--- packages/hub/src/lib/create-repo.spec.ts
+++ packages/hub/src/lib/create-repo.spec.ts
@@ -1,7 +1,7 @@
 import { assert, it, describe } from "vitest";
 
 import { randomBytes } from "crypto";
-import { TEST_ACCESS_TOKEN, TEST_USER } from "../consts";
+import { HUB_URL, TEST_ACCESS_TOKEN, TEST_USER } from "../consts";
 import { createRepo } from "./create-repo";
 import { deleteRepo } from "./delete-repo";
 import { downloadFile } from "./download-file";
@@ -10,7 +10,7 @@ describe("createRepo", () => {
 	it("should create a repo", async () => {
 		const repoName = `${TEST_USER}/TEST-${randomBytes(10).toString("hex")}`;
 
-		await createRepo({
+		const result = await createRepo({
 			credentials: {
 				accessToken: TEST_ACCESS_TOKEN,
 			},
@@ -19,6 +19,10 @@ describe("createRepo", () => {
 				type: "model",
 			},
 			files: [{ path: ".gitattributes", content: new Blob(["*.html filter=lfs diff=lfs merge=lfs -text"]) }],
+		});
+
+		assert.deepStrictEqual(result, {
+			repoUrl: `${HUB_URL}/${repoName}`,
 		});
 
 		const content = await downloadFile({
