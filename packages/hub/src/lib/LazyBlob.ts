2ce190c1d398644b7301b4c4dcecff000b2b3959
Aschen <amaret@kuzzle.io>
2023-03-18 00:47:02 +0100

    fix reviews and delete oopsi

--- packages/hub/src/lib/LazyBlob.ts
+++ packages/hub/src/lib/LazyBlob.ts
@@ -1,28 +1,28 @@
 import { createReadStream } from "node:fs";
-import { open } from "node:fs/promises";
+import { open, stat } from "node:fs/promises";
 import type { FileHandle } from "node:fs/promises";
 import { Readable } from "node:stream";
 
 export class LazyBlob extends Blob {
-	static async create(path: string, start?: number, end?: number): Promise<LazyBlob> {
-		const lazyBlob = new LazyBlob(path, start, end);
+	static async create(path: string, start: number, end: number): Promise<LazyBlob> {
+		const { size } = await stat(path);
 
-		await lazyBlob.init();
+		const lazyBlob = new LazyBlob(path, 0, size);
 
 		return lazyBlob;
 	}
 
 	private path: string;
-	private start: number | null;
-	private end: number | null;
+	private start: number;
+	private end: number;
 	private totalSize: number;
 
-	private constructor(path: string, start?: number, end?: number) {
+	private constructor(path: string, start: number, end: number) {
 		super();
 
 		this.path = path;
-		this.start = start || null;
-		this.end = end || null;
+		this.start = start;
+		this.end = end;
 
 		this.totalSize = 0;
 	}
@@ -44,18 +44,17 @@ export class LazyBlob extends Blob {
 	}
 
 	slice(start = 0, end = this.size): LazyBlob {
-		const slice = new LazyBlob(this.path, start, end);
+		if (start < 0 || end < 0) {
+			new TypeError("Unsupported negative start/end on LazyBlob.slice");
+		}
+
+		const slice = new LazyBlob(this.path, this.start + start, Math.min(this.start + end, this.end));
 
 		return slice;
 	}
 
 	async arrayBuffer(): Promise<ArrayBuffer> {
-		const start = this.start || 0;
-		const end = this.end || this.size;
-		const size = Math.abs(end - start);
-		const cappedSize = size > this.size ? this.size : size;
-
-		const slice = await this.execute((file) => file.read(Buffer.alloc(cappedSize), 0, cappedSize, 0));
+		const slice = await this.execute((file) => file.read(Buffer.alloc(this.size), 0, this.size, this.start));
 
 		return slice.buffer;
 	}
