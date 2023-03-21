5b94274976d43f81bb0a9f4eb3e89bb28b05210d
coyotte508 <coyotte508@gmail.com>
2023-03-21 01:48:48 +0100

    ⏪ ... and again

--- packages/hub/src/utils/LazyBlob.spec.ts
+++ packages/hub/src/utils/LazyBlob.spec.ts
@@ -1,23 +1,20 @@
 import { open, stat } from "node:fs/promises";
 import { TextDecoder } from "node:util";
 import { describe, expect, it } from "vitest";
-import { pathToFileURL } from "url";
 import { LazyBlob } from "./LazyBlob";
-import { resolve } from "node:path";
 
 describe("LazyBlob", () => {
 	it("should create a LazyBlob with a slice on the entire file", async () => {
 		const file = await open("package.json", "r");
 		const { size } = await stat("package.json");
 
-		const lazyBlob = await LazyBlob.create(pathToFileURL("package.json"));
+		const lazyBlob = await LazyBlob.create("package.json");
 
 		expect(lazyBlob).toMatchObject({
-			path: resolve("package.json"),
+			path: "package.json",
 			start: 0,
 			end: size,
 		});
-
 		expect(lazyBlob.size).toBe(size);
 		expect(lazyBlob.type).toBe("");
 		const text = await lazyBlob.text();
@@ -29,16 +26,15 @@ describe("LazyBlob", () => {
 
 	it("should create a slice on the file", async () => {
 		const file = await open("package.json", "r");
-		const lazyBlob = await LazyBlob.create(pathToFileURL("package.json"));
+		const lazyBlob = await LazyBlob.create("package.json");
 
 		const slice = lazyBlob.slice(10, 20);
 
 		expect(slice).toMatchObject({
-			path: resolve("package.json"),
+			path: "package.json",
 			start: 10,
 			end: 20,
 		});
-
 		expect(slice.size).toBe(10);
 		const sliceText = await slice.text();
 		const expectedText = (await file.read(Buffer.alloc(10), 0, 10, 10)).buffer.toString("utf8");
