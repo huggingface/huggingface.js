12e3a698d9c732d3b8c85df5d7d81d52f0667246
Aschen <amaret@kuzzle.io>
2023-03-22 23:48:31 +0100

    start WebBlob

---
+++ packages/hub/src/utils/WebBlob.ts
@@ -0,0 +1,77 @@
+export class WebBlob {
+	static async create(url: URL) {
+		const response = await fetch(url, { method: "HEAD" });
+
+		const size = Number(response.headers.get("content-length"));
+		const contentType = response.headers.get("content-type") || "";
+		const supportRange = response.headers.get("accept-ranges") === "bytes";
+
+		if (!supportRange) {
+			return await (await fetch(url)).blob();
+		}
+
+		return new WebBlob(url, 0, size, contentType);
+	}
+
+	private url: URL;
+	private start: number;
+	private end: number;
+	private contentType: string;
+
+	constructor(url: URL, start: number, end: number, contentType: string) {
+		this.url = url;
+		this.start = start;
+		this.end = end;
+		this.contentType = contentType;
+	}
+
+	get size(): number {
+		return this.end - this.start;
+	}
+
+	get type(): string {
+		return this.contentType;
+	}
+
+	slice(start = 0, end = this.size): WebBlob {
+		if (start < 0 || end < 0) {
+			new TypeError("Unsupported negative start/end on FileBlob.slice");
+		}
+
+		const slice = new WebBlob(this.url, this.start + start, Math.min(this.start + end, this.end), this.contentType);
+
+		return slice;
+	}
+
+	async arrayBuffer(): Promise<ArrayBuffer> {
+		const result = await this.fetchRange();
+
+		return result.arrayBuffer();
+	}
+
+	async text(): Promise<string> {
+		const result = await this.fetchRange();
+
+		return result.text();
+	}
+
+	async stream(): Promise<ReturnType<Blob["stream"]>> {
+		const response = await this.fetchRange();
+
+		return response.body as ReturnType<Blob["stream"]>;
+	}
+
+	private fetchRange(): Promise<Response> {
+		return fetch(this.url, {
+			headers: {
+				Range: `bytes=${this.start}-${this.end - 1}`,
+			},
+		});
+	}
+}
+
+fetch(new URL("https://huggingface.co/spaces/aschen/push-model-from-web/raw/main/mobilenet/model.json"), {
+	method: "HEAD",
+}).then((response) => {
+	console.log(response.headers.get("accept-ranges"));
+});
