a985930e0d55666c737920b627cc1a5e8ab631c5
Mishig Davaadorj <dmishig@gmail.com>
2023-03-27 12:11:07 +0200

    Support cookie credentials

--- packages/inference/src/HfInference.ts
+++ packages/inference/src/HfInference.ts
@@ -680,6 +680,7 @@ export class HfInference {
 		options?: Options & {
 			binary?: boolean;
 			blob?: boolean;
+			includeCredentials?: boolean;
 		}
 	): Promise<T> {
 		const mergedOptions = { ...this.defaultOptions, ...options };
@@ -707,6 +708,7 @@ export class HfInference {
 						...otherArgs,
 						options: mergedOptions,
 				  }),
+			credentials: options?.includeCredentials ? "include" : "same-origin",
 		});
 
 		if (mergedOptions.retry_on_error !== false && response.status === 503 && !mergedOptions.wait_for_model) {
