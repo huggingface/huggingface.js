581fef9939edfcc1e2f2aecbf68e72e9e72de867
Eliott C <coyotte508@gmail.com>
2023-03-29 23:22:24 +0200

    🐛 Add guard on `process` (#122)

--- packages/hub/src/consts.ts
+++ packages/hub/src/consts.ts
@@ -1,3 +1,6 @@
-export const HUB_URL = process.env.NODE_ENV === "test" ? "https://hub-ci.huggingface.co" : "https://huggingface.co";
+export const HUB_URL =
+	typeof process !== undefined && process.env.NODE_ENV === "test"
+		? "https://hub-ci.huggingface.co"
+		: "https://huggingface.co";
 export const TEST_USER = "hub.js";
 export const TEST_ACCESS_TOKEN = "hf_hub.js";
