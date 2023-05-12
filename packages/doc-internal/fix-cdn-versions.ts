d4ed524eed68997f9d3f4d1ed5b5abc444e91250
Eliott C <coyotte508@gmail.com>
2023-05-12 14:07:41 +0200

    🎨 Simpler, more future-proof code to fix package versions (#191)

--- packages/doc-internal/fix-cdn-versions.ts
+++ packages/doc-internal/fix-cdn-versions.ts
@@ -3,29 +3,14 @@ import { readFileSync, writeFileSync } from "fs";
 const hubPackage = JSON.parse(readFileSync("../hub/package.json").toString());
 const inferencePackage = JSON.parse(readFileSync("../inference/package.json").toString());
 
-let content = readFileSync("../../README.md").toString();
+for (const readme of ["../../README.md", "../../packages/hub/README.md", "../../packages/inference/README.md"]) {
+	let content = readFileSync(readme, "utf-8");
 
-content = content
-	.replace(
-		/https:[/][/]cdn[.]jsdelivr[.]net[/]npm[/]@huggingface[/]inference@\d([.]\d)?([.]\d)?[/][+]esm/,
-		`https://cdn.jsdelivr.net/npm/@huggingface/inference@${inferencePackage.version}/+esm`
-	)
-	.replace(
-		/https:[/][/]esm[.]sh[/]@huggingface[/]inference@\d([.]\d)?([.]\d)?/,
-		`https://esm.sh/@huggingface/inference@${inferencePackage.version}`
-	)
-	.replace(
-		/https:[/][/]cdn[.]jsdelivr[.]net[/]npm[/]@huggingface[/]hub@\d([.]\d)?([.]\d)?[/][+]esm/,
-		`https://cdn.jsdelivr.net/npm/@huggingface/hub@${hubPackage.version}/+esm`
+	content = content.replace(
+		/@huggingface[/]inference@\d([.]\d)?([.]\d)?/g,
+		`@huggingface/inference@${inferencePackage.version}`
 	);
+	content = content.replace(/@huggingface[/]hub@\d([.]\d)?([.]\d)?/g, `@huggingface/hub@${hubPackage.version}`);
 
-writeFileSync("../../README.md", Buffer.from(content));
-
-content = readFileSync("../inference/README.md").toString();
-
-content = content.replace(
-	/https:[/][/]esm[.]sh[/]@huggingface[/]inference@\d([.]\d)?([.]\d)?/,
-	`https://esm.sh/@huggingface/inference@${inferencePackage.version}`
-);
-
-writeFileSync("../inference/README.md", Buffer.from(content));
+	writeFileSync(readme, Buffer.from(content));
+}
