44ccb4fd04df4a1f35e96c99d42c0ad176d45fc7
coyotte508 <coyotte508@gmail.com>
2023-05-22 19:40:55 +0200

    ✨ Only use pipeline url when necessary
    Using pipeline url has downsides like not failing early, or failing with a 500 if using wrong task.
    
    We ask the hub about the task for text classification / feature extraction / sentence similarity tasks

--- packages/inference/src/lib/makeRequestOptions.ts
+++ packages/inference/src/lib/makeRequestOptions.ts
@@ -1,11 +1,16 @@
 import type { InferenceTask, Options, RequestArgs } from "../types";
 
 const HF_INFERENCE_API_BASE_URL = "https://api-inference.huggingface.co/";
+const HF_HUB_URL = "https://huggingface.co/";
+
+const taskCache = new Map<string, { task: string | null; date: Date }>();
+const CACHE_DURATION = 10 * 60 * 1000;
+const MAX_CACHE_ITEMS = 1000;
 
 /**
  * Helper that prepares request arguments
  */
-export function makeRequestOptions(
+export async function makeRequestOptions(
 	args: RequestArgs & {
 		data?: Blob | ArrayBuffer;
 		stream?: boolean;
@@ -13,9 +18,10 @@ export function makeRequestOptions(
 	options?: Options & {
 		/** For internal HF use, which is why it's not exposed in {@link Options} */
 		includeCredentials?: boolean;
+		/** When a model can be used for multiple tasks, and we want to run a non-default task */
 		task?: string | InferenceTask;
 	}
-): { url: string; info: RequestInit } {
+): Promise<{ url: string; info: RequestInit }> {
 	const { model, accessToken, ...otherArgs } = args;
 
 	const headers: Record<string, string> = {};
@@ -39,14 +45,40 @@ export function makeRequestOptions(
 		}
 	}
 
-	const url = (() => {
+	const url = await (async () => {
 		// If model is an URL, do not change it
 		if (/^http(s?):/.test(model) || model.startsWith("/")) {
 			return model;
 		}
 
 		if (options?.task) {
-			return `${HF_INFERENCE_API_BASE_URL}pipeline/${options.task}/${model}`;
+			const key = `${model}:${accessToken}`;
+			let cachedTask = taskCache.get(key);
+
+			if (cachedTask && cachedTask.date < new Date(Date.now() - CACHE_DURATION)) {
+				taskCache.delete(key);
+				cachedTask = undefined;
+			}
+
+			if (cachedTask === undefined) {
+				const task = await fetch(`${HF_HUB_URL}/api/models/${model}?expand[]=pipeline_tag`, {
+					headers: accessToken ? { Authorization: `Bearer ${accessToken}` } : {},
+				})
+					.then((resp) => resp.json())
+					.then((json) => json.pipeline_tag)
+					.catch(() => null);
+
+				cachedTask = { task, date: new Date() };
+				taskCache.set(key, { task, date: new Date() });
+
+				if (taskCache.size > MAX_CACHE_ITEMS) {
+					taskCache.delete(taskCache.keys().next().value);
+				}
+			}
+
+			if (cachedTask.task !== options.task) {
+				return `${HF_INFERENCE_API_BASE_URL}pipeline/${options.task}/${model}`;
+			}
 		}
 
 		return `${HF_INFERENCE_API_BASE_URL}models/${model}`;
