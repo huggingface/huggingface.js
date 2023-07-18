add1b72ddbd31e065fcedb457a0cd189ce7bc7ba
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-07-18 11:01:56 +0200

    fix docs

--- packages/doc-internal/fix-cdn-versions.ts
+++ packages/doc-internal/fix-cdn-versions.ts
@@ -3,7 +3,12 @@ import { readFileSync, writeFileSync } from "fs";
 const hubPackage = JSON.parse(readFileSync("../hub/package.json").toString());
 const inferencePackage = JSON.parse(readFileSync("../inference/package.json").toString());
 
-for (const readme of ["../../README.md", "../../packages/hub/README.md", "../../packages/inference/README.md"]) {
+for (const readme of [
+	"../../README.md",
+	"../../packages/hub/README.md",
+	"../../packages/inference/README.md",
+	"../../packages/agents/README.md",
+]) {
 	let content = readFileSync(readme, "utf-8");
 
 	content = content.replace(
