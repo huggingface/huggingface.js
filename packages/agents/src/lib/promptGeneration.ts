1154fb36462bd19e2a48aef3b2538109c3e306f8
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-07-26 10:17:47 +0200

    Improve prompt for compatibility with llama 2 (#233)
    I noticed that sometimes llama 2 wouldn't generate code answers, this
    should improve things.

--- packages/agents/src/lib/promptGeneration.ts
+++ packages/agents/src/lib/promptGeneration.ts
@@ -62,6 +62,8 @@ async function generate(${params}) {
 // your code here
 return output;
 };
+
+If you are not sure how to do it, try anyway and give it your best shot, as the user can always correct you later. Wrap your code answer in triple backticks (\`\`\`).
 \`\`\``;
 
 	return fullPrompt;
