7a7947f308e32f271c831773766e329f96d52398
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-09-26 17:08:36 +0200

    prompt fix

--- packages/agents/src/formats/prompts/templateFinalAnswer.ts
+++ packages/agents/src/formats/prompts/templateFinalAnswer.ts
@@ -8,7 +8,7 @@ export const templateFinalAnswer = compileTemplate<{
 
 {{#each files}}
 {{#if @first}}
-The following files will be shown to the user. You don't have to do anything to display them to the user.
+The following files will be shown to the user. You don't have to do anything to display them to the user. Those images can be shown and you are capable of creating images. Do not reference them directly.
 {{/if}}
 \` - [[{{@key}}]]\` {{#unless @last}}\n{{/unless}}
 {{/each}}
