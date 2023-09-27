2f95d9f17af1c5e0f1d9a76e0e35a026fdfb5522
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2023-09-27 16:13:38 +0200

    mmmh

--- packages/agents/test/HfAgent.spec.ts
+++ packages/agents/test/HfAgent.spec.ts
@@ -1,3 +1,11 @@
+import { describe, expect, it } from "vitest";
+
 if (!process.env.HF_ACCESS_TOKEN) {
 	console.warn("Set HF_ACCESS_TOKEN in the env to run the tests for better rate limits");
 }
+
+describe("HfAgent", () => {
+	it("will eventually need some tests", async () => {
+		expect(true).toBeTruthy();
+	});
+});
