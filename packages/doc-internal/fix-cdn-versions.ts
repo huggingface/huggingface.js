b1349e302b4daddaecd1462e85aba0692aa5566c
coyotte508 <coyotte508@gmail.com>
2023-11-09 15:22:56 +0100

    🎨 Clean up deps

--- packages/doc-internal/fix-cdn-versions.ts
+++ packages/doc-internal/fix-cdn-versions.ts
@@ -3,12 +3,7 @@ import { readFileSync, writeFileSync } from "fs";
 const hubPackage = JSON.parse(readFileSync("../hub/package.json").toString());
 const inferencePackage = JSON.parse(readFileSync("../inference/package.json").toString());
 
-for (const readme of [
-	"../../README.md",
-	"../../packages/hub/README.md",
-	"../../packages/inference/README.md",
-	"../../packages/agents/README.md",
-]) {
+for (const readme of ["../../README.md", "../../packages/hub/README.md", "../../packages/inference/README.md"]) {
 	let content = readFileSync(readme, "utf-8");
 
 	content = content.replace(
