280ce8b454685f1de9c469c96efdd4cce7a27c16
Julien Chaumond <julien@huggingface.co>
2023-11-10 15:41:29 +0100

    typing not really needed no?
    cc @coyotte508

--- packages/languages/src/language.ts
+++ packages/languages/src/language.ts
@@ -5,9 +5,6 @@ import type { Language } from "./types";
 /**
  * Be careful importing this function in frontend code, as it will import all languages.
  */
-export function language(code: keyof typeof LANGUAGES_ISO_639_1): Language;
-export function language(code: keyof typeof LANGUAGES_ISO_639_3): Language;
-export function language(code: string): Language | null;
 export function language(code: string): Language | null {
 	if (code in LANGUAGES_ISO_639_1) {
 		return LANGUAGES_ISO_639_1[code as keyof typeof LANGUAGES_ISO_639_1];
