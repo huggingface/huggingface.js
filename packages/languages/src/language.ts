36be99fd996059d9fcf1281378c0295bfcec8a45
coyotte508 <coyotte508@gmail.com>
2023-11-10 14:47:37 +0100

    💡 Add warning

--- packages/languages/src/language.ts
+++ packages/languages/src/language.ts
@@ -2,6 +2,9 @@ import { LANGUAGES_ISO_639_1 } from "./languages_iso_639_1";
 import { LANGUAGES_ISO_639_3 } from "./languages_iso_639_3";
 import type { Language } from "./types";
 
+/**
+ * Be careful importing this function in frontend code, as it will import all languages.
+ */
 export function language(code: keyof typeof LANGUAGES_ISO_639_1): Language;
 export function language(code: keyof typeof LANGUAGES_ISO_639_3): Language;
 export function language(code: string): Language | null;
