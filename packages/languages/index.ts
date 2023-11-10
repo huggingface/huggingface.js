6d83643432423494e6a8f8feaaed5dd5c730c66a
coyotte508 <coyotte508@gmail.com>
2023-11-10 14:51:48 +0100

    🔧 Configure tsup

--- packages/languages/index.ts
+++
@@ -1,5 +0,0 @@
-export type { Language } from "./src/types";
-export { language } from "./src/language";
-export { wikiLink } from "./src/wikiLink";
-export { LANGUAGES_ISO_639_1 } from "./src/languages_iso_639_1";
-export { LANGUAGES_ISO_639_3 } from "./src/languages_iso_639_3";
---
+++ packages/languages/src/index.ts
@@ -0,0 +1,5 @@
+export type { Language } from "./types";
+export { language } from "./language";
+export { wikiLink } from "./wikiLink";
+export { LANGUAGES_ISO_639_1 } from "./languages_iso_639_1";
+export { LANGUAGES_ISO_639_3 } from "./languages_iso_639_3";
