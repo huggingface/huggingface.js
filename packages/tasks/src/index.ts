9f94b9b75dbb29a8c14c3864dd3e802d5bd0eda6
coyotte508 <coyotte508@gmail.com>
2023-11-20 18:05:10 +0100

    ✨ Export more stuff

--- packages/tasks/src/index.ts
+++ packages/tasks/src/index.ts
@@ -1,5 +1,5 @@
 export type { TaskData, TaskDemo, TaskDemoEntry, ExampleRepo } from "./Types";
 export { TASKS_DATA } from "./tasksData";
-export { PIPELINE_DATA, PIPELINE_TYPES, type PipelineType, type PipelineData, type Modality, MODALITIES } from "./pipelines";
+export { PIPELINE_DATA, PIPELINE_TYPES, type PipelineType, type PipelineData, type Modality, MODALITIES, MODALITY_LABELS } from "./pipelines";
 export { ModelLibrary } from "./modelLibraries";
 export type { ModelLibraryKey } from "./modelLibraries";
--- packages/widgets/src/lib/index.ts
+++ packages/widgets/src/lib/index.ts
@@ -1,6 +1,17 @@
 import InferenceWidget from "./components/InferenceWidget/InferenceWidget.svelte";
 import WidgetOutputChart from "./components/InferenceWidget/shared/WidgetOutputChart/WidgetOutputChart.svelte";
 import WidgetOutputTokens from "./components/InferenceWidget/shared/WidgetOutputTokens/WidgetOutputTokens.svelte";
+import PipelineIcon from "./components/PipelineIcon/PipelineIcon.svelte";
+import { modelLoadStates } from "./components/InferenceWidget/stores.js";
+import { InferenceDisplayability } from "./interfaces/InferenceDisplayability.js";
+import * as serveCurl from "./inferenceSnippets/serveCurl.js";
+import * as serveJs from "./inferenceSnippets/serveJs.js";
+import * as servePython from "./inferenceSnippets/servePython.js";
+import * as snippetInputs from "./inferenceSnippets/inputs.js";
+import { MODEL_LIBRARIES_UI_ELEMENTS } from "./interfaces/Libraries.js";
+import type { LibraryUiElement, } from "./interfaces/Libraries.js";
+import type { TransformersInfo } from "./interfaces/Types.js";
 
-export { InferenceWidget, WidgetOutputChart, WidgetOutputTokens };
+export { InferenceWidget, WidgetOutputChart, WidgetOutputTokens, modelLoadStates, InferenceDisplayability, PipelineIcon, serveCurl, serveJs, servePython, snippetInputs, MODEL_LIBRARIES_UI_ELEMENTS };
 export type {WidgetExample, WidgetExampleOutput, WidgetExampleOutputUrl, WidgetExampleTextInput} from "./components/InferenceWidget/shared/WidgetExample.js";
+export type { LibraryUiElement, TransformersInfo };
