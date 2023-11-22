c94cb5737dd1d940a924346c8530ed824fbf127e
Victor Mustar <victor.mustar@gmail.com>
2023-11-22 15:22:15 +0100

    update icons

--- packages/widgets/src/lib/components/Icons/IconMaskGeneration.svelte
+++ packages/widgets/src/lib/components/Icons/IconMaskGeneration.svelte
@@ -1,1 +1,28 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...$$props}><path fill="currentColor" d="M27 3H5a2 2 0 0 0-2 2v22a2 2 0 0 0 2 2h22a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2ZM9 21H5v-4h4Zm2 2h4v4h-4Zm6 0h4v4h-4Zm0-8v-4h4v4Zm4-6h-4V5h4Zm2 2h4v4h-4Zm-8-6v10H5V5Zm8 22V17h4v10Z"/></svg>
+<script lang="ts">
+	export let classNames = "";
+</script>
+
+<svg
+	class={classNames}
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	aria-hidden="true"
+	fill="currentColor"
+	focusable="false"
+	role="img"
+	width="1em"
+	height="1em"
+	preserveAspectRatio="xMidYMid meet"
+	viewBox="0 0 12 12"
+	><path
+		fill="currentColor"
+		fill-rule="evenodd"
+		d="M1.84.73h6.63a.7.7 0 0 1 .7.7v4.36h-.7V1.43H1.84v3.54l.9-.9a.7.7 0 0 1 1 0l1.74 1.75a.79.79 0 0 0-.52.47L3.24 4.57l-1.4 1.4v2.08h3.07v.7H1.84a.7.7 0 0 1-.7-.7V1.43a.7.7 0 0 1 .7-.7Zm5.38 4.74.32.32H5.91l.32-.32a.7.7 0 0 1 .99 0Zm-.61-1.43A1.05 1.05 0 1 1 5.45 2.3 1.05 1.05 0 0 1 6.6 4.04Zm-.39-1.16a.35.35 0 1 0-.39.58.35.35 0 0 0 .4-.58Z"
+		clip-rule="evenodd"
+	/><path
+		fill="currentColor"
+		stroke="currentColor"
+		stroke-width=".03"
+		d="M10.12 11.34c.18 0 .35-.08.47-.2.13-.13.2-.3.2-.47V9.35h-.68v1.3H8.8V11.34h1.31ZM6.22 8.44h.01v-1.3h1.3v-.68H6.22a.67.67 0 0 0-.67.67v1.31h.67ZM5.55 10.67c0 .18.07.35.2.47.12.12.29.2.47.2h1.31V10.65h-1.3v-1.3h-.68v1.32ZM8.8 7.12v.02h1.31v1.3h.68V7.13a.67.67 0 0 0-.67-.67H8.81v.66Z"
+	/></svg
+>
--- packages/widgets/src/lib/components/Icons/IconZeroShotObjectDetection.svelte
+++ packages/widgets/src/lib/components/Icons/IconZeroShotObjectDetection.svelte
@@ -1,1 +1,27 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...$$props}><path fill="currentColor" d="M4 22H2V4a2.002 2.002 0 0 1 2-2h18v2H4zm17-5a3 3 0 1 0-3-3a3.003 3.003 0 0 0 3 3zm0-4a1 1 0 1 1-1 1a1 1 0 0 1 1-1z"/><path fill="currentColor" d="M28 7H9a2.002 2.002 0 0 0-2 2v19a2.002 2.002 0 0 0 2 2h19a2.002 2.002 0 0 0 2-2V9a2.002 2.002 0 0 0-2-2Zm0 21H9v-6l4-3.997l5.586 5.586a2 2 0 0 0 2.828 0L23 22.003L28 27Zm0-3.828l-3.586-3.586a2 2 0 0 0-2.828 0L20 22.172l-5.586-5.586a2 2 0 0 0-2.828 0L9 19.172V9h19Z"/></svg>
+<script lang="ts">
+	export let classNames = "";
+</script>
+
+<svg
+	class={classNames}
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	aria-hidden="true"
+	focusable="false"
+	role="img"
+	width="1em"
+	height="1em"
+	preserveAspectRatio="xMidYMid meet"
+	viewBox="0 0 12 12"
+	><path
+		fill="currentColor"
+		fill-rule="evenodd"
+		d="M1.84.73h6.63a.7.7 0 0 1 .7.7v4.36h-.7V1.43H1.84v3.54l.9-.9a.7.7 0 0 1 1 0l1.74 1.75a.79.79 0 0 0-.52.47L3.24 4.57l-1.4 1.4v2.08h3.07v.7H1.84a.7.7 0 0 1-.7-.7V1.43a.7.7 0 0 1 .7-.7Zm5.38 4.74.32.32H5.91l.32-.32a.7.7 0 0 1 .99 0Zm-.61-1.43A1.05 1.05 0 1 1 5.45 2.3 1.05 1.05 0 0 1 6.6 4.04Zm-.39-1.16a.35.35 0 1 0-.39.58.35.35 0 0 0 .4-.58Z"
+		clip-rule="evenodd"
+	/><path
+		fill="currentColor"
+		fill-rule="evenodd"
+		d="M7.77 7.07h-1.6v1.42h1.6V7.07Zm-2.2-.6v2.62h2.8V6.47h-2.8ZM8.53 10.17H6.17v.43h2.36v-.43Zm-2.96-.6v1.63h3.55V9.57H5.58ZM10.16 7.07h-.72v1.42h.72V7.07Zm-1.32-.6v2.62h1.92V6.47H8.84Z"
+		clip-rule="evenodd"
+	/></svg
+>
