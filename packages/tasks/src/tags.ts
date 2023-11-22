ffe6536115ad610a60abb432e21fc5b47e1fd838
Eliott C <coyotte508@gmail.com>
2023-11-22 16:16:01 +0100

    ✨ Export tags + helpers (#345)

---
+++ packages/tasks/src/tags.ts
@@ -0,0 +1,15 @@
+export const TAG_NFAA_CONTENT = "not-for-all-audiences";
+export const OTHER_TAGS_SUGGESTIONS = [
+	"chemistry",
+	"biology",
+	"finance",
+	"legal",
+	"music",
+	"art",
+	"code",
+	"climate",
+	"medical",
+	TAG_NFAA_CONTENT,
+];
+export const TAG_TEXT_GENERATION_INFERENCE = "text-generation-inference";
+export const TAG_CUSTOM_CODE = "custom_code";
