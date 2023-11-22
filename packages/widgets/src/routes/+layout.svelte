0a9f211c9943152095d98550672f056763bd1a50
coyotte508 <coyotte508@gmail.com>
2023-11-22 18:47:41 +0100

    🔥 Remove unused fonts

--- packages/widgets/src/routes/+layout.svelte
+++ packages/widgets/src/routes/+layout.svelte
@@ -2,19 +2,10 @@
 	import "../tailwind.css";
 	import "@fontsource/ibm-plex-mono/400.css";
 	import "@fontsource/ibm-plex-mono/600.css";
-	import "@fontsource/ibm-plex-mono/700.css";
-	import "@fontsource/source-sans-pro/200.css";
-	import "@fontsource/source-sans-pro/300.css";
 	import "@fontsource/source-sans-pro/400.css";
 	import "@fontsource/source-sans-pro/600.css";
-	import "@fontsource/source-sans-pro/700.css";
-	import "@fontsource/source-sans-pro/900.css";
-	import "@fontsource/source-sans-pro/200-italic.css";
-	import "@fontsource/source-sans-pro/300-italic.css";
 	import "@fontsource/source-sans-pro/400-italic.css";
 	import "@fontsource/source-sans-pro/600-italic.css";
-	import "@fontsource/source-sans-pro/700-italic.css";
-	import "@fontsource/source-sans-pro/900-italic.css";
 </script>
 
 <div class="min-h-screen bg-gray-50 text-black dark:bg-gray-950">
