9f10b494f24084acd39dac9ba1835639622ffc2b
Joshua Lochner <admin@xenova.com>
2023-11-23 03:49:54 +0200

    Update vitest.config.ts

--- packages/templates/vitest.config.ts
+++ packages/templates/vitest.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		// ...
+		exclude: [...configDefaults.exclude],
 	},
 });
