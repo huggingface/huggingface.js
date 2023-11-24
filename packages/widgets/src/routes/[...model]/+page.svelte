5c806678d3fc9cec015c49e45bbddad900d4d142
coyotte508 <coyotte508@gmail.com>
2023-11-24 18:37:59 +0100

    fixup! ♻️ Remove "$lib" from source

--- packages/widgets/src/routes/[...model]/+page.svelte
+++ packages/widgets/src/routes/[...model]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import ModeSwitcher from "$lib/components/DemoThemeSwitcher/DemoThemeSwitcher.svelte";
-	import InferenceWidget from "../../InferenceWidget.svelte";
+	import InferenceWidget from "$lib/components/InferenceWidget/InferenceWidget.svelte";
 
 	export let data;
 </script>
