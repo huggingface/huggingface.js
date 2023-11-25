88231a70a8e9355c95c2e8a42a7868f4cafbf24a
Joshua Lochner <admin@xenova.com>
2023-11-25 03:39:00 +0200

    Apply `lstrip_blocks` pre-processing step

--- packages/jinja/src/index.ts
+++ packages/jinja/src/index.ts
@@ -23,6 +23,10 @@ export class Template {
 	 * @param {string} template The template string
 	 */
 	constructor(template: string) {
+		// Since `lstrip_blocks` is enabled, we strip tabs and spaces from the beginning of a line to the start of a block.
+		// We can achieve the same effect by replacing all instances of `%}\s+{%` with `%}{%` (as a pre-processing step).
+		template = template.replace(/%}\s+{%/g, "%}{%");
+
 		const tokens = tokenize(template);
 		this.parsed = parse(tokens);
 	}
