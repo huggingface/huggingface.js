457f392f55c32e8d96a364e19f48029c34a6bca9
Vova Manannikov <vova@promail.spb.ru>
2023-11-26 15:36:01 +0000

    :art: prettier

--- packages/inference/test/vitest.d.ts
+++ packages/inference/test/vitest.d.ts
@@ -1,23 +1,23 @@
 /// <reference types="vite/client" />
 // https://vitest.dev/guide/extending-matchers
-import 'vitest';
+import "vitest";
 
 interface MatcherResult {
-    pass: boolean
-    message: () => string
-    // If you pass these, they will automatically appear inside a diff when
-    // the matcher does not pass, so you don't need to print the diff yourself
-    actual?: unknown
-    expected?: unknown
+	pass: boolean;
+	message: () => string;
+	// If you pass these, they will automatically appear inside a diff when
+	// the matcher does not pass, so you don't need to print the diff yourself
+	actual?: unknown;
+	expected?: unknown;
 }
 
 interface CustomMatchers {
-    closeTo(expected: number, precision: number): MatcherResult
+	closeTo(expected: number, precision: number): MatcherResult;
 }
 
-declare module 'vitest' {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface Assertion extends CustomMatchers {}
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface AsymmetricMatchersContaining extends CustomMatchers {}
+declare module "vitest" {
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface Assertion extends CustomMatchers {}
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
+	interface AsymmetricMatchersContaining extends CustomMatchers {}
 }
