862dbe8a5a282140dd93e8b6e9dfbb9a6e0e7913
hackpk <kadian303@hotmail.com>
2023-11-28 17:07:01 +0530

    clean up get collections file

--- packages/hub/src/lib/get-collections.spec.ts
+++ packages/hub/src/lib/get-collections.spec.ts
@@ -1,83 +1,27 @@
 import { describe, it } from "vitest";
-// import type { CleEntry } from "./list-models";
+//
 import type { CollectionEntry } from "./get-collections";
 import { getCollections } from "./get-collections";
-// import { exit } from "process";
-// import type { Collection } from "../types/api/api-collection";
+//
 
 describe("listCollections", () => {
 	it("should fetch LFS file info", async () => {
 		const results: CollectionEntry[] = [];
 		for await (const entry of getCollections({
 			slug: "latent-consistency/latent-consistency-models-loras-654cdd24e111e16f0865fba6",
-			// additionalFields: ["subdomain"],
+			//
 		})) {
-			// if (entry.slug !== "TheBloke/recent-models-64f9a55bb3115b4f513ec026") {
-			// 	continue;
-			// }
-			// if (typeof entry.likes === "number") {
-			// 	entry.likes = 0;
-			// }
-			// if (entry.updatedAt instanceof Date) {
-			// 	entry.updatedAt = new Date(0);
-			// }
+			//
+			//
 			results.push(entry);
-			// results.push(entry);
+			//
 			console.log(entry);
-			// if (entry.slug === "TheBloke/recent-models-64f9a55bb3115b4f513ec026") {
+			//
 
-			// }
+			//
 		}
-		// assert.strictEqual(info?., 183);
-		// assert.strictEqual(info?.etag, '"41a0e56472bad33498744818c8b1ef2c-64"');
-		// assert(info?.downloadLink);
+		//
 	});
 
-	// it("should list collections by clem", async () => {
-	// 	const results: Collection[] = [];
-	// 	for await (const entry of getCollections({
-	// 		search: { slug: "TheBloke/recent-models-64f9a55bb3115b4f513ec026", owner: "TheBloke" },
-	// 	})) {
-	// 		if (entry.slug !== "TheBloke/recent-models-64f9a55bb3115b4f513ec026") {
-	// 			continue;
-	// 		}
-	// 		// if (typeof entry.likes === "number") {
-	// 		// 	entry.likes = 0;
-	// 		// }
-	// 		// if (entry.updatedAt instanceof Date) {
-	// 		// 	entry.updatedAt = new Date(0);
-	// 		// }
-	// 		// results.push(entry);
-	// 		results.push(entry);
-	// 		console.log(results);
-	// 		if (entry.slug === "TheBloke/recent-models-64f9a55bb3115b4f513ec026") {
-	// 			exit;
-	// 		}
-	// 	}
-	// 	// console.log(await getCollections({ slug: "TheBloke/recent-models-64f9a55bb3115b4f513ec026" }));
-	// 	// AsyncGenerato = await getCollections({ slug: "TheBloke/recent-models-64f9a55bb3115b4f513ec026" })
-	// 	// results.sort((a, b) => a.id.localeCompare(b.id));
-	// 	// expect(results).deep.equal([
-	// 	// 	{
-	// 	// 		id: "621ffdc136468d709f17e709",
-	// 	// 		name: "Intel/dpt-large",
-	// 	// 		private: false,
-	// 	// 		gated: false,
-	// 	// 		downloads: 0,
-	// 	// 		likes: 0,
-	// 	// 		task: "depth-estimation",
-	// 	// 		updatedAt: new Date(0),
-	// 	// 	},
-	// 	// 	{
-	// 	// 		id: "638f07977559bf9a2b2b04ac",
-	// 	// 		name: "Intel/dpt-hybrid-midas",
-	// 	// 		gated: false,
-	// 	// 		private: false,
-	// 	// 		downloads: 0,
-	// 	// 		likes: 0,
-	// 	// 		task: "depth-estimation",
-	// 	// 		updatedAt: new Date(0),
-	// 	// 	},
-	// 	// ]);
-	// });
+	//
 });
--- packages/hub/src/lib/get-collections.ts
+++ packages/hub/src/lib/get-collections.ts
@@ -7,11 +7,7 @@ import type { ApiSpaceInfo } from "../types/api/api-space";
 import type { ApiDatasetInfo } from "../types/api/api-dataset";
 import type { ApiModelInfo } from "../types/api/api-model";
 import type { ApiWhoAmIAuthInfo } from "../types/api/api-who-am-i";
-// import { pick } from "../utils/pick";
-// import { parseLinkHeader } from "../utils/parseLinkHeader";
-
-// const EXPAND_KEYS = ["private", "position", "theme", "upvotes", "isUpvotedByUser"];
-
+//
 export interface CollectionEntry {
 	slug: string;
 	title: string;
@@ -28,11 +24,9 @@ export interface CollectionEntry {
 }
 
 export async function* getCollections(params: {
-	// token?: string;
-	// search?: {
-	// owner: string;
+	//
 	slug: string;
-	// };
+	//
 	credentials?: Credentials;
 	hubUrl?: string;
 	/**
@@ -42,13 +36,10 @@ export async function* getCollections(params: {
 	additionalFields?: Array<keyof Collection>;
 }): AsyncGenerator<CollectionEntry> {
 	checkCredentials(params.credentials);
-	// const search = new URLSearchParams([
-	// 	...Object.entries({ limit: "500", ...(params?.slug ? { slug: params?.slug } : undefined) }),
-	// 	...[...EXPAND_KEYS, ...(params?.additionalFields ?? [])].map((val) => ["expand", val] satisfies [string, string]),
-	// ]).toString();
+
 	const url: string | undefined = `${params?.hubUrl || HUB_URL}/api/collections?${params.slug}`;
 
-	// while (url) {
+	//
 	if (url) {
 		const res: Response = await (params.fetch ?? fetch)(url, {
 			headers: {
@@ -62,11 +53,11 @@ export async function* getCollections(params: {
 		}
 
 		const items: Collection[] = await res.json();
-		// console.log(items);
+		//
 		for (const item of items) {
 			if (item.slug == params.slug) {
 				yield {
-					// ...(params?.additionalFields && pick(item, params.additionalFields)),
+					//
 					slug: item.slug,
 					title: item.title,
 					description: item.description,
@@ -84,114 +75,4 @@ export async function* getCollections(params: {
 		}
 	}
 }
-/*{
-	"slug": "TheBloke/recent-models-last-100-repos-sorted-by-creation-date-64f9a55bb3115b4f513ec026",
-	"title": "Recent models: last 100 repos, sorted by creation date",
-	"description": "The last 100 repos I have created. Sorted by creation date descending, so the most recently created repos appear at the top.",
-	"lastUpdated": "2023-11-25T02:06:56.628Z",
-	"owner": {
-		"avatarUrl": "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/6426d3f3a7723d62b53c259b/tvPikpAzKTKGN5wrpadOJ.jpeg?w=200&h=200&f=face",
-		"fullname": "Tom Jobbins",
-		"name": "TheBloke",
-		"type": "user",
-		"isPro": true,
-		"isHf": false
-	},
-	"items": [
-		{
-			"_id": "656156b4280cd6b7108c6308",
-			"position": 0,
-			"type": "model",
-			"author": "TheBloke",
-			"authorData": {
-				"avatarUrl": "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/6426d3f3a7723d62b53c259b/tvPikpAzKTKGN5wrpadOJ.jpeg?w=200&h=200&f=face",
-				"fullname": "Tom Jobbins",
-				"name": "TheBloke",
-				"type": "user",
-				"isPro": true,
-				"isHf": false
-			},
-			"downloads": 0,
-			"gated": false,
-			"id": "TheBloke/juanako-7B-v1-GPTQ",
-			"lastModified": "2023-11-25T01:59:26.000Z",
-			"likes": 0,
-			"pipeline_tag": "text-generation",
-			"private": false,
-			"repoType": "model",
-			"isLikedByUser": false
-		},
-		{
-			"_id": "656156b47a465cdcb34618f5",
-			"position": 1,
-			"type": "model",
-			"author": "TheBloke",
-			"authorData": {
-				"avatarUrl": "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/6426d3f3a7723d62b53c259b/tvPikpAzKTKGN5wrpadOJ.jpeg?w=200&h=200&f=face",
-				"fullname": "Tom Jobbins",
-				"name": "TheBloke",
-				"type": "user",
-				"isPro": true,
-				"isHf": false
-			},
-			"downloads": 0,
-			"gated": false,
-			"id": "TheBloke/juanako-7B-v1-AWQ",
-			"lastModified": "2023-11-25T01:47:31.000Z",
-			"likes": 0,
-			"pipeline_tag": "text-generation",
-			"private": false,
-			"repoType": "model",
-			"isLikedByUser": false
-		},
-		{
-			"_id": "656156b5462e5ebcbfeb381d",
-			"position": 2,
-			"type": "model",
-			"author": "TheBloke",
-			"authorData": {
-				"avatarUrl": "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/6426d3f3a7723d62b53c259b/tvPikpAzKTKGN5wrpadOJ.jpeg?w=200&h=200&f=face",
-				"fullname": "Tom Jobbins",
-				"name": "TheBloke",
-				"type": "user",
-				"isPro": true,
-				"isHf": false
-			},
-			"downloads": 0,
-			"gated": false,
-			"id": "TheBloke/juanako-7B-v1-GGUF",
-			"lastModified": "2023-11-25T01:33:55.000Z",
-			"likes": 0,
-			"private": false,
-			"repoType": "model",
-			"isLikedByUser": false
-		},
-		{
-			"_id": "6561329c2c671784e2b2ccb6",
-			"position": 3,
-			"type": "model",
-			"author": "TheBloke",
-			"authorData": {
-				"avatarUrl": "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/6426d3f3a7723d62b53c259b/tvPikpAzKTKGN5wrpadOJ.jpeg?w=200&h=200&f=face",
-				"fullname": "Tom Jobbins",
-				"name": "TheBloke",
-				"type": "user",
-				"isPro": true,
-				"isHf": false
-			},
-			"downloads": 0,
-			"gated": false,
-			"id": "TheBloke/smartyplats-7B-v2-GPTQ",
-			"lastModified": "2023-11-24T23:12:17.000Z",
-			"likes": 0,
-			"pipeline_tag": "text-generation",
-			"private": false,
-			"repoType": "model",
-			"isLikedByUser": false
-		}
-	],
-	"theme": "green",
-	"private": false,
-	"upvotes": 180,
-	"isUpvotedByUser": false
-},*/
+//
