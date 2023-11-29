1f13ac407cce9313e7b4e88dc21bfccebb7a4258
Eliott C <coyotte508@gmail.com>
2023-11-29 14:32:14 +0100

    ✨ Save login after "sign in with HF" (#382)

--- packages/widgets/src/routes/+layout.server.ts
+++ packages/widgets/src/routes/+layout.server.ts
@@ -1,11 +1,11 @@
 import { env } from "$env/dynamic/private";
 import type { LayoutServerLoad } from "./$types.js";
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-	const session = await locals.getSession();
+const supportsOAuth = !!env.OAUTH_CLIENT_ID && !!env.OAUTH_CLIENT_SECRET;
 
+export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
-		access_token: session?.access_token,
-		supportsOAuth: !!env.OAUTH_CLIENT_ID && !!env.OAUTH_CLIENT_SECRET,
+		session: supportsOAuth ? locals.getSession() : undefined,
+		supportsOAuth,
 	};
 };
