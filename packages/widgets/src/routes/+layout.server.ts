0df27357280eddb6b951649a7b70aa7ebfbd3514
coyotte508 <coyotte508@gmail.com>
2023-11-29 14:30:01 +0100

    🐛 Fix bug on local session

--- packages/widgets/src/routes/+layout.server.ts
+++ packages/widgets/src/routes/+layout.server.ts
@@ -1,11 +1,11 @@
 import { env } from "$env/dynamic/private";
 import type { LayoutServerLoad } from "./$types.js";
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-	const session = await locals.getSession();
+const supportsOAuth = !!env.OAUTH_CLIENT_ID && !!env.OAUTH_CLIENT_SECRET;
 
+export const load: LayoutServerLoad = async ({ locals }) => {
 	return {
-		session,
-		supportsOAuth: !!env.OAUTH_CLIENT_ID && !!env.OAUTH_CLIENT_SECRET,
+		session: supportsOAuth ? locals.getSession() : undefined,
+		supportsOAuth,
 	};
 };
