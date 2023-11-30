a86edcb301d8cd436640ba857890d03ba2f7f759
Bertrand CHEVRIER <bertrand@huggingface.co>
2023-11-30 14:47:49 +0100

    feat: bundle widgets for different sources

--- packages/widgets/vite.config.ts
+++ packages/widgets/vite.config.ts
@@ -1,6 +1,49 @@
+import type { OutputOptions } from 'rollup';
+import type { UserConfig } from 'vite';
 import { sveltekit } from "@sveltejs/kit/vite";
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	plugins: [sveltekit()],
+const formats = ['es', 'cjs'];
+
+const isSSR = process.argv.includes('--ssr')
+
+export default defineConfig( ({mode}) => {
+	if (mode === 'lib') {
+		return {
+			plugins: [svelte({
+				configFile: false,
+				extensions: ['.svelte'],
+				preprocess: sveltePreprocess({
+					typescript: { tsconfigFile: `${__dirname}/tsconfig.json` },
+				}) ,
+				emitCss: false,
+			})],
+			build: {
+				manifest: true,
+				outDir: `dist/${isSSR ? 'server' : 'client'}`,
+				rollupOptions: {
+					input: {
+						index: 'src/lib/index.ts',
+					},
+					preserveEntrySignatures: true,
+					output: formats.map(format => ({
+						preserveModules: true,
+						format: format,
+						entryFileNames: `[name].${format === 'cjs' ? 'cjs' : 'js'}`,
+					})) as OutputOptions[],
+					external: ['svelte', /svelte\/(.*)/, "@huggingface/tasks"],
+				}
+			},  
+		}
+	}
+	
+	if (mode === 'app') {
+		return {
+			plugins: [sveltekit()],
+		}
+	}
+
+	return {};
 });
