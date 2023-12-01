8eff96218ea021ab8cf72135bf7c04a975bc8d1a
Bertrand CHEVRIER <bertrand@huggingface.co>
2023-12-01 14:20:54 +0100

    chore: generate types and use sveltekit tsconfig

--- packages/widgets/vite.config.ts
+++ packages/widgets/vite.config.ts
@@ -1,47 +1,62 @@
-import type { OutputOptions } from 'rollup';
+
 import { sveltekit } from "@sveltejs/kit/vite";
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-const formats = ['es', 'cjs'];
 
-const isSSR = process.argv.includes('--ssr')
 
-export default defineConfig( ({mode}) => {
-	if (mode === 'lib') {
+const isSSR = process.argv.includes("--ssr");
+
+export default defineConfig(({ mode }) => {
+	if (mode === "lib") {
 		return {
-			plugins: [svelte({
-				configFile: false,
-				extensions: ['.svelte'],
-				preprocess: sveltePreprocess({
-					typescript: { tsconfigFile: `${__dirname}/tsconfig.json` },
-				}) ,
-				emitCss: false,
-			})],
+			plugins: [
+				svelte({
+					configFile: false,
+					extensions: [".svelte"],
+					
+					// eslint-disable-next-line 
+					// @ts-ignore see https://github.com/sveltejs/svelte-preprocess/issues/591
+					preprocess: sveltePreprocess({
+						typescript: { tsconfigFile: `${__dirname}/tsconfig.json` },
+					}),
+					emitCss: false,
+				}),
+				dts({
+					entryRoot: "src/lib",
+				}),
+			],
 			build: {
 				manifest: true,
-				outDir: `dist/${isSSR ? 'server' : 'client'}`,
+				outDir: `dist/${isSSR ? "server" : "client"}`,
 				rollupOptions: {
 					input: {
-						index: 'src/lib/index.ts',
+						index: "src/lib/index.ts",
 					},
-					preserveEntrySignatures: true,
-					output: formats.map(format => ({
+					preserveEntrySignatures: "strict",
+					output: [{
 						preserveModules: true,
-						format: format,
-						entryFileNames: `[name].${format === 'cjs' ? 'cjs' : 'js'}`,
-					})) as OutputOptions[],
-					external: ['svelte', /svelte\/(.*)/, "@huggingface/tasks"],
-				}
-			},  
-		}
+						
+						format: "cjs",
+						entryFileNames: "[name].cjs",
+					}, {
+						preserveModules: true,
+						preserveEntrySignatures: true,
+						format: "es",
+						entryFileNames: "[name].js",
+					}],
+					external: ["svelte", /svelte\/(.*)/, "@huggingface/tasks"],
+				},
+			},
+		};
 	}
-	
-	if (mode === 'app') {
+
+	if (mode === "app") {
 		return {
 			plugins: [sveltekit()],
-		}
+		};
 	}
 
 	return {};
