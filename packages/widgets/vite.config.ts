c86929bf71b148e1897e8c143a11c217fdd341f8
Bertrand CHEVRIER <bertrand@huggingface.co>
2023-12-01 19:15:25 +0100

    bundle widgets for different sources (#386)
    Co-authored-by: coyotte508 <coyotte508@gmail.com>

--- packages/widgets/vite.config.ts
+++ packages/widgets/vite.config.ts
@@ -1,6 +1,66 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig({
-	plugins: [sveltekit()],
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const isSSR = process.argv.includes("--ssr");
+
+export default defineConfig(({ mode }) => {
+	if (mode === "lib") {
+		return {
+			plugins: [
+				svelte({
+					configFile: false,
+					extensions: [".svelte"],
+
+					// eslint-disable-next-line
+					// @ts-ignore see https://github.com/sveltejs/svelte-preprocess/issues/591
+					preprocess: sveltePreprocess({
+						typescript: { tsconfigFile: `${__dirname}/tsconfig.json` },
+					}),
+					emitCss: false,
+				}),
+				dts({
+					entryRoot: "src/lib",
+				}),
+			],
+			build: {
+				manifest: true,
+				outDir: `dist/${isSSR ? "server" : "client"}`,
+				rollupOptions: {
+					input: {
+						index: "src/lib/index.ts",
+					},
+					preserveEntrySignatures: "strict",
+					output: [
+						{
+							preserveModules: true,
+
+							format: "cjs",
+							entryFileNames: "[name].cjs",
+						},
+						{
+							preserveModules: true,
+							preserveEntrySignatures: true,
+							format: "es",
+							entryFileNames: "[name].js",
+						},
+					],
+					external: ["svelte", /svelte\/(.*)/, "@huggingface/tasks"],
+				},
+			},
+		};
+	}
+
+	if (mode === "app") {
+		return {
+			plugins: [sveltekit()],
+		};
+	}
+
+	return {};
 });
