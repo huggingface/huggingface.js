d57525152121712714110787ba7bc1b27394e3bc
Joshua Lochner <admin@xenova.com>
2023-12-13 19:16:26 +0200

    Reorder test case data objects

--- packages/jinja/test/e2e.test.js
+++ packages/jinja/test/e2e.test.js
@@ -24,76 +24,76 @@ const EXAMPLE_CHAT_WITH_SYTEM = [
  */
 const TEST_DEFAULT_TEMPLATES = Object.freeze({
 	_base: {
+		chat_template: `{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			add_generation_prompt: false,
 		},
-		chat_template: `{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}`,
 		target: `<|im_start|>user\nHello, how are you?<|im_end|>\n<|im_start|>assistant\nI'm doing great. How can I help you today?<|im_end|>\n<|im_start|>user\nI'd like to show off how chat templating works!<|im_end|>\n`,
 	},
 	blenderbot: {
 		// facebook/blenderbot-400M-distill
+		chat_template: `{% for message in messages %}{% if message['role'] == 'user' %}{{ ' ' }}{% endif %}{{ message['content'] }}{% if not loop.last %}{{ '  ' }}{% endif %}{% endfor %}{{ eos_token }}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			eos_token: "</s>",
 		},
-		chat_template: `{% for message in messages %}{% if message['role'] == 'user' %}{{ ' ' }}{% endif %}{{ message['content'] }}{% if not loop.last %}{{ '  ' }}{% endif %}{% endfor %}{{ eos_token }}`,
 		target: ` Hello, how are you?  I'm doing great. How can I help you today?   I'd like to show off how chat templating works!</s>`,
 	},
 	blenderbot_small: {
 		// facebook/blenderbot_small-90M
+		chat_template: `{% for message in messages %}{% if message['role'] == 'user' %}{{ ' ' }}{% endif %}{{ message['content'] }}{% if not loop.last %}{{ '  ' }}{% endif %}{% endfor %}{{ eos_token }}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			eos_token: "</s>",
 		},
-		chat_template: `{% for message in messages %}{% if message['role'] == 'user' %}{{ ' ' }}{% endif %}{{ message['content'] }}{% if not loop.last %}{{ '  ' }}{% endif %}{% endfor %}{{ eos_token }}`,
 		target: ` Hello, how are you?  I'm doing great. How can I help you today?   I'd like to show off how chat templating works!</s>`,
 	},
 	bloom: {
 		// bigscience/bloom
+		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			eos_token: "</s>",
 		},
-		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		target: `Hello, how are you?</s>I'm doing great. How can I help you today?</s>I'd like to show off how chat templating works!</s>`,
 	},
 	gpt_neox: {
 		// EleutherAI/gpt-neox-20b
+		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			eos_token: "<|endoftext|>",
 		},
-		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		target: `Hello, how are you?<|endoftext|>I'm doing great. How can I help you today?<|endoftext|>I'd like to show off how chat templating works!<|endoftext|>`,
 	},
 	gpt2: {
 		// gpt2
+		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			eos_token: "<|endoftext|>",
 		},
-		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		target: `Hello, how are you?<|endoftext|>I'm doing great. How can I help you today?<|endoftext|>I'd like to show off how chat templating works!<|endoftext|>`,
 	},
 	llama: {
 		// hf-internal-testing/llama-tokenizer
+		chat_template: `{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% elif USE_DEFAULT_PROMPT == true and not '<<SYS>>' in messages[0]['content'] %}{% set loop_messages = messages %}{% set system_message = 'DEFAULT_SYSTEM_MESSAGE' %}{% else %}{% set loop_messages = messages %}{% set system_message = false %}{% endif %}{% for message in loop_messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if loop.index0 == 0 and system_message != false %}{% set content = '<<SYS>>\\n' + system_message + '\\n<</SYS>>\\n\\n' + message['content'] %}{% else %}{% set content = message['content'] %}{% endif %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}{% elif message['role'] == 'system' %}{{ '<<SYS>>\\n' + content.strip() + '\\n<</SYS>>\\n\\n' }}{% elif message['role'] == 'assistant' %}{{ ' ' + content.strip() + ' ' + eos_token }}{% endif %}{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT_WITH_SYTEM,
 			bos_token: "<s>",
 			eos_token: "</s>",
 			USE_DEFAULT_PROMPT: true,
 		},
-		chat_template: `{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = messages[0]['content'] %}{% elif USE_DEFAULT_PROMPT == true and not '<<SYS>>' in messages[0]['content'] %}{% set loop_messages = messages %}{% set system_message = 'DEFAULT_SYSTEM_MESSAGE' %}{% else %}{% set loop_messages = messages %}{% set system_message = false %}{% endif %}{% for message in loop_messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if loop.index0 == 0 and system_message != false %}{% set content = '<<SYS>>\\n' + system_message + '\\n<</SYS>>\\n\\n' + message['content'] %}{% else %}{% set content = message['content'] %}{% endif %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + content.strip() + ' [/INST]' }}{% elif message['role'] == 'system' %}{{ '<<SYS>>\\n' + content.strip() + '\\n<</SYS>>\\n\\n' }}{% elif message['role'] == 'assistant' %}{{ ' ' + content.strip() + ' ' + eos_token }}{% endif %}{% endfor %}`,
 		target: `<s>[INST] <<SYS>>\nYou are a friendly chatbot who always responds in the style of a pirate\n<</SYS>>\n\nHello, how are you? [/INST] I'm doing great. How can I help you today? </s><s>[INST] I'd like to show off how chat templating works! [/INST]`,
 	},
 	whisper: {
 		// openai/whisper-large-v3
+		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			eos_token: "<|endoftext|>",
 		},
-		chat_template: `{% for message in messages %}{{ message.content }}{{ eos_token }}{% endfor %}`,
 		target: `Hello, how are you?<|endoftext|>I'm doing great. How can I help you today?<|endoftext|>I'd like to show off how chat templating works!<|endoftext|>`,
 	},
 });
@@ -104,21 +104,21 @@ const TEST_DEFAULT_TEMPLATES = Object.freeze({
  */
 const TEST_CUSTOM_TEMPLATES = Object.freeze({
 	"HuggingFaceH4/zephyr-7b-beta": {
+		chat_template: `{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT_WITH_SYTEM,
 			eos_token: "</s>",
 			add_generation_prompt: false,
 		},
-		chat_template: `{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}`,
 		target: `<|system|>\nYou are a friendly chatbot who always responds in the style of a pirate</s>\n<|user|>\nHello, how are you?</s>\n<|assistant|>\nI'm doing great. How can I help you today?</s>\n<|user|>\nI'd like to show off how chat templating works!</s>\n`,
 	},
 	"mistralai/Mistral-7B-Instruct-v0.1": {
+		chat_template: `{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token + ' ' }}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}`,
 		data: {
 			messages: EXAMPLE_CHAT,
 			bos_token: "<s>",
 			eos_token: "</s>",
 		},
-		chat_template: `{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'user') != (loop.index0 % 2 == 0) %}{{ raise_exception('Conversation roles must alternate user/assistant/user/assistant/...') }}{% endif %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token + ' ' }}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}`,
 		target: `<s>[INST] Hello, how are you? [/INST]I'm doing great. How can I help you today?</s> [INST] I'd like to show off how chat templating works! [/INST]`,
 	},
 });
