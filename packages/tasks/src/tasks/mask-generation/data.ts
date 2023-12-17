afa6d482b7439f1145c1f5a7db0ae03f36fe7fef
Nebula <nebulaanish@gmail.com>
2023-12-17 15:28:53 +0545

    Added summary for mask generation

---
+++ packages/tasks/src/tasks/mask-generation/data.ts
@@ -0,0 +1,27 @@
+import type { TaskDataCustom } from "..";
+
+const taskData: TaskDataCustom = {
+	datasets: [
+	],
+	demo: {
+		inputs: [
+			
+		],
+		outputs: [
+			
+		],
+	},
+	metrics: [],
+	models: [
+		
+	],
+	spaces: [
+		
+	],
+	summary:
+		"Mask generation refers to the process of creating a binary image that identifies a specific object or region of interest in an input image. Masks are often used in object detection and segmentation tasks, where they provide a precise way to isolate the object of interest for further processing or analysis.",
+	widgetModels: [""],
+	youtubeId: "",
+};
+
+export default taskData;
