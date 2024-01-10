dc2c58f0787e32626d34ec1c886bc1d72756f9b4
Anish Neupane <50112251+NebulaAnish@users.noreply.github.com>
2024-01-10 11:51:03 +0545

    Update packages/tasks/src/tasks/mask-generation/data.ts
    Co-authored-by: Merve Noyan <merveenoyan@gmail.com>

--- packages/tasks/src/tasks/mask-generation/data.ts
+++ packages/tasks/src/tasks/mask-generation/data.ts
@@ -7,7 +7,7 @@ const taskData: TaskDataCustom = {
 		outputs: [],
 	},
 	metrics: [],
-	models: [
+	models: [],
 	spaces: [],
 	summary:
 		"Mask generation is creating a binary image that identifies a specific object or region of interest in an input image. Masks are often used in segmentation tasks, where they provide a precise way to isolate the object of interest for further processing or analysis.",
