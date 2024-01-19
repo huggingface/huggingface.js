dbd0254d3929b272b023e02b9801fba5652b3e2f
SBrandeis <simon@huggingface.co>
2024-01-19 18:01:28 +0100

    ✨ Add text2text-generation task & reference it from summarization/translation

---
+++ packages/tasks/src/tasks/text2text-generation/inference.ts
@@ -0,0 +1,55 @@
+/**
+ * Inference code generated from the JSON schema spec in ./spec
+ *
+ * Using src/scripts/inference-codegen
+ */
+
+/**
+ * Inputs for Text2text Generation inference
+ */
+export interface Text2TextGenerationInput {
+	/**
+	 * One or more texts to use for text2text generation
+	 */
+	inputs: string[] | string;
+	/**
+	 * Additional inference parameters
+	 */
+	parameters?: Parameters;
+	[property: string]: any;
+}
+
+/**
+ * Additional inference parameters
+ *
+ * Additional inference parameters for Text2text Generation
+ */
+export interface Parameters {
+	/**
+	 * Whether to clean up the potential extra spaces in the text output.
+	 */
+	cleanUpTokenizationSpaces?: boolean;
+	/**
+	 * Additional parametrization of the text generation algorithm
+	 */
+	generateParameters?: { [key: string]: any };
+	/**
+	 * The truncation strategy to use
+	 */
+	truncation?: Truncation;
+	[property: string]: any;
+}
+
+export type Truncation = "do_not_truncate" | "longest_first" | "only_first" | "only_second";
+
+/**
+ * Outputs of inference for the Text2text Generation task
+ */
+export interface Text2TextGenerationOutput {
+	generatedTex: any;
+	/**
+	 * The generated text.
+	 */
+	generatedText?: string;
+	[property: string]: any;
+}
