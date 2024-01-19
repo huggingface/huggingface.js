fad594b648e1b61fadcb1a0f97aa0028c2697b90
SBrandeis <simon@huggingface.co>
2024-01-19 16:04:07 +0100

    💄format with pnpm

--- packages/tasks/src/tasks/audio-classification/inference.ts
+++ packages/tasks/src/tasks/audio-classification/inference.ts
@@ -1,24 +1,22 @@
-
 /**
  * Inference code generated from the JSON schema spec in ./spec
- * 
+ *
  * Generated on 2024-01-19T14:59:10.562Z
  */
-
 
 /**
  * Inputs for Audio Classification inference
  */
 export interface AudioClassificationInput {
-    /**
-     * On or several audio files to classify
-     */
-    inputs: any;
-    /**
-     * Additional inference parameters
-     */
-    parameters?: AudioClassificationParameters;
-    [property: string]: any;
+	/**
+	 * On or several audio files to classify
+	 */
+	inputs: any;
+	/**
+	 * Additional inference parameters
+	 */
+	parameters?: AudioClassificationParameters;
+	[property: string]: any;
 }
 
 /**
@@ -27,24 +25,24 @@ export interface AudioClassificationInput {
  * Additional inference parameters for Audio Classification
  */
 export interface AudioClassificationParameters {
-    /**
-     * When specified, limits the output to the top K most probable classes.
-     */
-    topK?: number;
-    [property: string]: any;
+	/**
+	 * When specified, limits the output to the top K most probable classes.
+	 */
+	topK?: number;
+	[property: string]: any;
 }
 
 /**
  * Outputs for Audio Classification inference
  */
 export interface AudioClassificationOutput {
-    /**
-     * The predicted class label (model specific).
-     */
-    label: string;
-    /**
-     * The corresponding probability.
-     */
-    score: number;
-    [property: string]: any;
+	/**
+	 * The predicted class label (model specific).
+	 */
+	label: string;
+	/**
+	 * The corresponding probability.
+	 */
+	score: number;
+	[property: string]: any;
 }
--- packages/tasks/src/tasks/text-generation/inference.ts
+++ packages/tasks/src/tasks/text-generation/inference.ts
@@ -1,24 +1,22 @@
-
 /**
  * Inference code generated from the JSON schema spec in ./spec
- * 
+ *
  * Generated on 2024-01-19T14:59:10.562Z
  */
-
 
 /**
  * Inputs for Text Generation inference
  */
 export interface TextGenerationInput {
-    /**
-     * The text to initialize generation with
-     */
-    inputs: string[] | string;
-    /**
-     * Additional inference parameters
-     */
-    parameters?: TextGenerationParameters;
-    [property: string]: any;
+	/**
+	 * The text to initialize generation with
+	 */
+	inputs: string[] | string;
+	/**
+	 * Additional inference parameters
+	 */
+	parameters?: TextGenerationParameters;
+	[property: string]: any;
 }
 
 /**
@@ -27,63 +25,63 @@ export interface TextGenerationInput {
  * Additional inference parameters for Text Generation
  */
 export interface TextGenerationParameters {
-    /**
-     * Whether to use logit sampling (true) or greedy search (false).
-     */
-    doSample?: boolean;
-    /**
-     * Maximum number of generated tokens.
-     */
-    maxNewTokens?: number;
-    /**
-     * The parameter for repetition penalty. A value of 1.0 means no penalty. See [this
-     * paper](https://hf.co/papers/1909.05858) for more details.
-     */
-    repetitionPenalty?: number;
-    /**
-     * Whether to prepend the prompt to the generated text.
-     */
-    returnFullText?: boolean;
-    /**
-     * Stop generating tokens if a member of `stop_sequences` is generated.
-     */
-    stopSequences?: string[];
-    /**
-     * The value used to modulate the logits distribution.
-     */
-    temperature?: number;
-    /**
-     * The number of highest probability vocabulary tokens to keep for top-k-filtering.
-     */
-    topK?: number;
-    /**
-     * If set to < 1, only the smallest set of most probable tokens with probabilities that add
-     * up to `top_p` or higher are kept for generation.
-     */
-    topP?: number;
-    /**
-     * Truncate input tokens to the given size.
-     */
-    truncate?: number;
-    /**
-     * Typical Decoding mass. See [Typical Decoding for Natural Language
-     * Generation](https://hf.co/papers/2202.00666) for more information
-     */
-    typicalP?: number;
-    /**
-     * Watermarking with [A Watermark for Large Language Models](https://hf.co/papers/2301.10226)
-     */
-    watermark?: boolean;
-    [property: string]: any;
+	/**
+	 * Whether to use logit sampling (true) or greedy search (false).
+	 */
+	doSample?: boolean;
+	/**
+	 * Maximum number of generated tokens.
+	 */
+	maxNewTokens?: number;
+	/**
+	 * The parameter for repetition penalty. A value of 1.0 means no penalty. See [this
+	 * paper](https://hf.co/papers/1909.05858) for more details.
+	 */
+	repetitionPenalty?: number;
+	/**
+	 * Whether to prepend the prompt to the generated text.
+	 */
+	returnFullText?: boolean;
+	/**
+	 * Stop generating tokens if a member of `stop_sequences` is generated.
+	 */
+	stopSequences?: string[];
+	/**
+	 * The value used to modulate the logits distribution.
+	 */
+	temperature?: number;
+	/**
+	 * The number of highest probability vocabulary tokens to keep for top-k-filtering.
+	 */
+	topK?: number;
+	/**
+	 * If set to < 1, only the smallest set of most probable tokens with probabilities that add
+	 * up to `top_p` or higher are kept for generation.
+	 */
+	topP?: number;
+	/**
+	 * Truncate input tokens to the given size.
+	 */
+	truncate?: number;
+	/**
+	 * Typical Decoding mass. See [Typical Decoding for Natural Language
+	 * Generation](https://hf.co/papers/2202.00666) for more information
+	 */
+	typicalP?: number;
+	/**
+	 * Watermarking with [A Watermark for Large Language Models](https://hf.co/papers/2301.10226)
+	 */
+	watermark?: boolean;
+	[property: string]: any;
 }
 
 /**
  * Outputs for Text Generation inference
  */
 export interface TextGenerationOutput {
-    /**
-     * The generated text
-     */
-    generatedText: string;
-    [property: string]: any;
+	/**
+	 * The generated text
+	 */
+	generatedText: string;
+	[property: string]: any;
 }
