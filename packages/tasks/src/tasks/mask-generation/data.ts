5c87fbbf75b5b257b77947a7ad356e53fef3cb73
Merve Noyan <merveenoyan@gmail.com>
2024-01-26 02:59:47 +0300

    Add models and spaces to mask gen

--- packages/tasks/src/tasks/mask-generation/data.ts
+++ packages/tasks/src/tasks/mask-generation/data.ts
@@ -7,10 +7,36 @@ const taskData: TaskDataCustom = {
 		outputs: [],
 	},
 	metrics: [],
-	models: [],
-	spaces: [],
+	models: [
+		{
+			description: "Small yet powerful mask generation model.",
+			id: "Zigeng/SlimSAM-uniform-50",
+		},
+		{
+			description: "Very strong mask generation model.",
+			id: "facebook/sam-vit-huge",
+		},
+	],
+	spaces: [
+		{
+			description: "An application that combines a mask generation model with an image embedding model for open-vocabulary image segmentation.",
+			id: "SkalskiP/SAM_and_MetaCLIP",
+		},
+		{
+			description: "An application that compares the performance of a large and a small mask generation model.",
+			id: "merve/slimsam",
+		},
+		{
+			description: "An application based on an improved mask generation model.",
+			id: "linfanluntan/Grounded-SAM",
+		},
+		{
+			description: "An application to remove objects from videos using mask generation models.",
+			id: "SkalskiP/SAM_and_ProPainter",
+		},
+	],
 	summary:
-		"Mask generation is creating a binary image that identifies a specific object or region of interest in an input image. Masks are often used in segmentation tasks, where they provide a precise way to isolate the object of interest for further processing or analysis.",
+		"Mask generation is the task of generating masks that identifies a specific object or region of interest in a given image. Masks are often used in segmentation tasks, where they provide a precise way to isolate the object of interest for further processing or analysis.",
 	widgetModels: [],
 	youtubeId: "",
 };
