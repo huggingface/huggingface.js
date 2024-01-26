bf48f5e3426852499d3ec746911ae6bc5e291210
SBrandeis <simon@huggingface.co>
2024-01-26 15:51:34 +0100

    Update image-to-image inputs

--- packages/tasks/src/tasks/image-to-image/inference.ts
+++ packages/tasks/src/tasks/image-to-image/inference.ts
@@ -4,8 +4,6 @@
  * Using src/scripts/inference-codegen
  */
 
-export type ImageToImageOutput = unknown[];
-
 /**
  * Inputs for Image To Image inference
  */
@@ -17,6 +15,53 @@ export interface ImageToImageInput {
 	/**
 	 * Additional inference parameters
 	 */
-	parameters?: unknown;
+	parameters?: ImageToImageParameters;
+	[property: string]: unknown;
+}
+
+/**
+ * Additional inference parameters
+ *
+ * Additional inference parameters for Image To Image
+ */
+export interface ImageToImageParameters {
+	/**
+	 * For diffusion models. A higher guidance scale value encourages the model to generate
+	 * images closely linked to the text prompt at the expense of lower image quality.
+	 */
+	guidanceScale?: number;
+	/**
+	 * One or several prompt to guide what NOT to include in image generation.
+	 */
+	negativePrompt?: string[];
+	/**
+	 * For diffusion models. The number of denoising steps. More denoising steps usually lead to
+	 * a higher quality image at the expense of slower inference.
+	 */
+	numInferenceSteps?: number;
+	/**
+	 * The size in pixel of the output image
+	 */
+	targetSize?: TargetSize;
+	[property: string]: unknown;
+}
+
+/**
+ * The size in pixel of the output image
+ */
+export interface TargetSize {
+	height: number;
+	width: number;
+	[property: string]: unknown;
+}
+
+/**
+ * Outputs of inference for the Image To Image task
+ */
+export interface ImageToImageOutput {
+	/**
+	 * The output image
+	 */
+	image?: unknown;
 	[property: string]: unknown;
 }
