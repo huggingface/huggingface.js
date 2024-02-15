c60f1453772b407318bb548ba6964e6d7a35f1b7
Julien Chaumond <julien@huggingface.co>
2024-02-15 15:52:28 +0100

    clearer prop name

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -10,7 +10,7 @@ export interface LibraryUiElement {
 	 * displayed in tags, and on the main
 	 * call-to-action button on the model page.
 	 */
-	btnLabel: string;
+	prettyName: string;
 	/**
 	 * Repo name of the library's (usually on GitHub) code repo
 	 */
@@ -51,7 +51,7 @@ export interface LibraryUiElement {
 
 export const MODEL_LIBRARIES_UI_ELEMENTS = {
 	"adapter-transformers": {
-		btnLabel: "Adapters",
+		prettyName: "Adapters",
 		repoName: "adapters",
 		repoUrl: "https://github.com/Adapter-Hub/adapters",
 		docsUrl: "https://huggingface.co/docs/hub/adapters",
@@ -59,7 +59,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	allennlp: {
-		btnLabel: "AllenNLP",
+		prettyName: "AllenNLP",
 		repoName: "AllenNLP",
 		repoUrl: "https://github.com/allenai/allennlp",
 		docsUrl: "https://huggingface.co/docs/hub/allennlp",
@@ -67,7 +67,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	asteroid: {
-		btnLabel: "Asteroid",
+		prettyName: "Asteroid",
 		repoName: "Asteroid",
 		repoUrl: "https://github.com/asteroid-team/asteroid",
 		docsUrl: "https://huggingface.co/docs/hub/asteroid",
@@ -75,14 +75,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	bertopic: {
-		btnLabel: "BERTopic",
+		prettyName: "BERTopic",
 		repoName: "BERTopic",
 		repoUrl: "https://github.com/MaartenGr/BERTopic",
 		snippets: snippets.bertopic,
 		filter: true,
 	},
 	diffusers: {
-		btnLabel: "Diffusers",
+		prettyName: "Diffusers",
 		repoName: "🤗/diffusers",
 		repoUrl: "https://github.com/huggingface/diffusers",
 		docsUrl: "https://huggingface.co/docs/hub/diffusers",
@@ -90,12 +90,12 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	doctr: {
-		btnLabel: "docTR",
+		prettyName: "docTR",
 		repoName: "doctr",
 		repoUrl: "https://github.com/mindee/doctr",
 	},
 	espnet: {
-		btnLabel: "ESPnet",
+		prettyName: "ESPnet",
 		repoName: "ESPnet",
 		repoUrl: "https://github.com/espnet/espnet",
 		docsUrl: "https://huggingface.co/docs/hub/espnet",
@@ -103,14 +103,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	fairseq: {
-		btnLabel: "Fairseq",
+		prettyName: "Fairseq",
 		repoName: "fairseq",
 		repoUrl: "https://github.com/pytorch/fairseq",
 		snippets: snippets.fairseq,
 		filter: true,
 	},
 	flair: {
-		btnLabel: "Flair",
+		prettyName: "Flair",
 		repoName: "Flair",
 		repoUrl: "https://github.com/flairNLP/flair",
 		docsUrl: "https://huggingface.co/docs/hub/flair",
@@ -118,7 +118,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	keras: {
-		btnLabel: "Keras",
+		prettyName: "Keras",
 		repoName: "Keras",
 		repoUrl: "https://github.com/keras-team/keras",
 		docsUrl: "https://huggingface.co/docs/hub/keras",
@@ -126,33 +126,33 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	k2: {
-		btnLabel: "K2",
+		prettyName: "K2",
 		repoName: "k2",
 		repoUrl: "https://github.com/k2-fsa/k2",
 	},
 	mlx: {
-		btnLabel: "MLX",
+		prettyName: "MLX",
 		repoName: "MLX",
 		repoUrl: "https://github.com/ml-explore/mlx-examples/tree/main",
 		snippets: snippets.mlx,
 		filter: true,
 	},
 	nemo: {
-		btnLabel: "NeMo",
+		prettyName: "NeMo",
 		repoName: "NeMo",
 		repoUrl: "https://github.com/NVIDIA/NeMo",
 		snippets: snippets.nemo,
 		filter: true,
 	},
 	open_clip: {
-		btnLabel: "OpenCLIP",
+		prettyName: "OpenCLIP",
 		repoName: "OpenCLIP",
 		repoUrl: "https://github.com/mlfoundations/open_clip",
 		snippets: snippets.open_clip,
 		filter: true,
 	},
 	paddlenlp: {
-		btnLabel: "paddlenlp",
+		prettyName: "paddlenlp",
 		repoName: "PaddleNLP",
 		repoUrl: "https://github.com/PaddlePaddle/PaddleNLP",
 		docsUrl: "https://huggingface.co/docs/hub/paddlenlp",
@@ -160,21 +160,21 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	peft: {
-		btnLabel: "PEFT",
+		prettyName: "PEFT",
 		repoName: "PEFT",
 		repoUrl: "https://github.com/huggingface/peft",
 		snippets: snippets.peft,
 		filter: true,
 	},
 	"pyannote-audio": {
-		btnLabel: "pyannote.audio",
+		prettyName: "pyannote.audio",
 		repoName: "pyannote-audio",
 		repoUrl: "https://github.com/pyannote/pyannote-audio",
 		snippets: snippets.pyannote_audio,
 		filter: true,
 	},
 	"sentence-transformers": {
-		btnLabel: "sentence-transformers",
+		prettyName: "sentence-transformers",
 		repoName: "sentence-transformers",
 		repoUrl: "https://github.com/UKPLab/sentence-transformers",
 		docsUrl: "https://huggingface.co/docs/hub/sentence-transformers",
@@ -182,7 +182,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	setfit: {
-		btnLabel: "setfit",
+		prettyName: "setfit",
 		repoName: "setfit",
 		repoUrl: "https://github.com/huggingface/setfit",
 		docsUrl: "https://huggingface.co/docs/hub/setfit",
@@ -190,14 +190,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	sklearn: {
-		btnLabel: "Scikit-learn",
+		prettyName: "Scikit-learn",
 		repoName: "Scikit-learn",
 		repoUrl: "https://github.com/scikit-learn/scikit-learn",
 		snippets: snippets.sklearn,
 		filter: true,
 	},
 	fastai: {
-		btnLabel: "fastai",
+		prettyName: "fastai",
 		repoName: "fastai",
 		repoUrl: "https://github.com/fastai/fastai",
 		docsUrl: "https://huggingface.co/docs/hub/fastai",
@@ -205,7 +205,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	spacy: {
-		btnLabel: "spaCy",
+		prettyName: "spaCy",
 		repoName: "spaCy",
 		repoUrl: "https://github.com/explosion/spaCy",
 		docsUrl: "https://huggingface.co/docs/hub/spacy",
@@ -213,7 +213,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	"span-marker": {
-		btnLabel: "SpanMarker",
+		prettyName: "SpanMarker",
 		repoName: "SpanMarkerNER",
 		repoUrl: "https://github.com/tomaarsen/SpanMarkerNER",
 		docsUrl: "https://huggingface.co/docs/hub/span_marker",
@@ -221,7 +221,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	speechbrain: {
-		btnLabel: "speechbrain",
+		prettyName: "speechbrain",
 		repoName: "speechbrain",
 		repoUrl: "https://github.com/speechbrain/speechbrain",
 		docsUrl: "https://huggingface.co/docs/hub/speechbrain",
@@ -229,7 +229,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	stanza: {
-		btnLabel: "Stanza",
+		prettyName: "Stanza",
 		repoName: "stanza",
 		repoUrl: "https://github.com/stanfordnlp/stanza",
 		docsUrl: "https://huggingface.co/docs/hub/stanza",
@@ -237,13 +237,13 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	tensorflowtts: {
-		btnLabel: "TensorFlowTTS",
+		prettyName: "TensorFlowTTS",
 		repoName: "TensorFlowTTS",
 		repoUrl: "https://github.com/TensorSpeech/TensorFlowTTS",
 		snippets: snippets.tensorflowtts,
 	},
 	timm: {
-		btnLabel: "timm",
+		prettyName: "timm",
 		repoName: "pytorch-image-models",
 		repoUrl: "https://github.com/rwightman/pytorch-image-models",
 		docsUrl: "https://huggingface.co/docs/hub/timm",
@@ -251,7 +251,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	transformers: {
-		btnLabel: "Transformers",
+		prettyName: "Transformers",
 		repoName: "🤗/transformers",
 		repoUrl: "https://github.com/huggingface/transformers",
 		docsUrl: "https://huggingface.co/docs/hub/transformers",
@@ -259,7 +259,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	"transformers.js": {
-		btnLabel: "Transformers.js",
+		prettyName: "Transformers.js",
 		repoName: "transformers.js",
 		repoUrl: "https://github.com/xenova/transformers.js",
 		docsUrl: "https://huggingface.co/docs/hub/transformers-js",
@@ -267,14 +267,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	fasttext: {
-		btnLabel: "fastText",
+		prettyName: "fastText",
 		repoName: "fastText",
 		repoUrl: "https://fasttext.cc/",
 		snippets: snippets.fasttext,
 		filter: true,
 	},
 	"sample-factory": {
-		btnLabel: "sample-factory",
+		prettyName: "sample-factory",
 		repoName: "sample-factory",
 		repoUrl: "https://github.com/alex-petrenko/sample-factory",
 		docsUrl: "https://huggingface.co/docs/hub/sample-factory",
@@ -282,7 +282,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	"stable-baselines3": {
-		btnLabel: "stable-baselines3",
+		prettyName: "stable-baselines3",
 		repoName: "stable-baselines3",
 		repoUrl: "https://github.com/huggingface/huggingface_sb3",
 		docsUrl: "https://huggingface.co/docs/hub/stable-baselines3",
@@ -290,12 +290,12 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	mindspore: {
-		btnLabel: "MindSpore",
+		prettyName: "MindSpore",
 		repoName: "mindspore",
 		repoUrl: "https://github.com/mindspore-ai/mindspore",
 	},
 	"ml-agents": {
-		btnLabel: "ml-agents",
+		prettyName: "ml-agents",
 		repoName: "ml-agents",
 		repoUrl: "https://github.com/Unity-Technologies/ml-agents",
 		docsUrl: "https://huggingface.co/docs/hub/ml-agents",
@@ -303,14 +303,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 	},
 	"unity-sentis": {
-		btnLabel: "unity-sentis",
+		prettyName: "unity-sentis",
 		repoName: "unity-sentis",
 		repoUrl: "https://github.com/Unity-Technologies/sentis-samples",
 		snippets: snippets.sentis,
 		filter: true,
 	},
 	pythae: {
-		btnLabel: "pythae",
+		prettyName: "pythae",
 		repoName: "pythae",
 		repoUrl: "https://github.com/clementchadebec/benchmark_VAE",
 		snippets: snippets.pythae,
