e4961b84e6f410f204ae66cb6329ae336b1a5acc
SBrandeis <simon@huggingface.co>
2024-02-20 11:32:53 +0100

    mention TGI in the widget

--- packages/widgets/src/lib/components/InferenceWidget/shared/WidgetHeader/WidgetHeader.svelte
+++ packages/widgets/src/lib/components/InferenceWidget/shared/WidgetHeader/WidgetHeader.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { updateWidgetState } from "../../stores.js";
+	import { tgiSupportedModels, updateWidgetState } from "../../stores.js";
 	import { TASKS_DATA } from "@huggingface/tasks";
 	import type { WidgetExample, WidgetExampleAttribute } from "@huggingface/tasks";
 	import type { WidgetProps, ExampleRunOpts } from "../types.js";
@@ -53,7 +53,7 @@
 			<div class="flex items-center text-lg">
 				{#if !isDisabled}
 					<IconLightning classNames="-ml-1 mr-1 text-yellow-500" />
-					Inference Endpoints (serverless)
+					Inference API (serverless)
 				{:else}
 					Inference Examples
 				{/if}
@@ -66,15 +66,23 @@
 </div>
 <div class="mb-0.5 flex w-full max-w-full flex-wrap items-center justify-between text-sm text-gray-500">
 	{#if pipeline && task}
-		<a
-			class={TASKS_DATA[task] ? "hover:underline" : undefined}
-			href={TASKS_DATA[task] ? `/tasks/${task}` : undefined}
-			target="_blank"
-			title={TASKS_DATA[task] ? `Learn more about ${task}` : undefined}
-		>
-			<PipelineTag classNames="mr-2 mb-1.5" {pipeline} />
-		</a>
+		<div class="flex gap-4 items-center mb-1.5">
+			<a
+				class={TASKS_DATA[task] ? "hover:underline" : undefined}
+				href={TASKS_DATA[task] ? `/tasks/${task}` : undefined}
+				target="_blank"
+				title={TASKS_DATA[task] ? `Learn more about ${task}` : undefined}
+			>
+				<PipelineTag {pipeline} />
+			</a>
+			{#if $tgiSupportedModels?.has(model.id)}
+				<p class="text-xs text-gray-400">
+					Streaming with <a href="https://huggingface.co/docs/text-generation-inference" class="underline">TGI</a>
+				</p>
+			{/if}
+		</div>
 	{/if}
+
 	{#if validExamples.length && applyWidgetExample}
 		<WidgetExamples {validExamples} {isLoading} {applyWidgetExample} {callApiOnMount} {exampleQueryParams} />
 	{/if}
