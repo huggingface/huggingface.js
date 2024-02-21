55b73a3933fb42f0c35ae5934b1663242af63df7
Victor Mustar <victor.mustar@gmail.com>
2024-02-21 19:03:58 +0100

    text adjustements

--- packages/widgets/src/lib/components/InferenceWidget/shared/WidgetOutputConvo/WidgetOutputConvo.svelte
+++ packages/widgets/src/lib/components/InferenceWidget/shared/WidgetOutputConvo/WidgetOutputConvo.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <div bind:this={wrapperEl} class="h-64 overflow-y-auto rounded-t-lg border border-b-0 leading-tight">
-	<div class="p-3 pt-6 text-center text-sm text-gray-400">
+	<div class="p-3 pt-6 text-center text-sm text-gray-400 text-balance">
 		Input a message to start chatting with
 		<strong>{modelId}</strong>.
 	</div>
