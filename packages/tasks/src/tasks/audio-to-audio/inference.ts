f80bf582c9c99480ba3970f8b748a0e388939f51
Wauplin <lucainp@gmail.com>
2024-02-23 11:59:29 +0100

    generated-file

---
+++ packages/tasks/src/tasks/audio-to-audio/inference.ts
@@ -0,0 +1,36 @@
+/**
+ * Inference code generated from the JSON schema spec in ./spec
+ *
+ * Using src/scripts/inference-codegen
+ */
+/**
+ * Inputs for Audio to Audio inference
+ */
+export interface AudioToAudioInput {
+	/**
+	 * The input audio data
+	 */
+	inputs: unknown;
+	[property: string]: unknown;
+}
+export type AudioToAudioOutput = AudioToAudioOutputElement[];
+/**
+ * Outputs of inference for the Audio To Audio task
+ *
+ * A generated audio file with its label.
+ */
+export interface AudioToAudioOutputElement {
+	/**
+	 * The generated audio file.
+	 */
+	blob: unknown;
+	/**
+	 * The content type of audio file.
+	 */
+	"content-type": string;
+	/**
+	 * The label of the audio file.
+	 */
+	label: string;
+	[property: string]: unknown;
+}
