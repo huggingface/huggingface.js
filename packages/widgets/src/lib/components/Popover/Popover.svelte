a7b20b7abdbe74536f2d3fa188ea95b1607ba62a
Mishig Davaadorj <dmishig@gmail.com>
2024-03-27 11:47:29 +0100

    auto alignemnt should consider center as well

--- packages/widgets/src/lib/components/Popover/Popover.svelte
+++ packages/widgets/src/lib/components/Popover/Popover.svelte
@@ -56,7 +56,9 @@
 	function updateAlignment(anchorBbox: DOMRect, pageWidth: number) {
 		if (alignment === "auto" && pageWidth > 0) {
 			const popoverWidth = popoverElement.getBoundingClientRect().width;
-			if (anchorBbox.left + popoverWidth > pageWidth - HIT_ZONE_MARGIN) {
+			if (anchorBbox.left < HIT_ZONE_MARGIN) {
+				computedAlignment = "center";
+			} else if (anchorBbox.left + popoverWidth > pageWidth - HIT_ZONE_MARGIN) {
 				computedAlignment = "end";
 			} else {
 				computedAlignment = "start";
