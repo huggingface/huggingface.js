3fbf3f79ce59985ddca2dbaf0de3ec2d75bb92c1
Wauplin <lucainp@gmail.com>
2024-04-15 17:34:41 +0200

    gen

--- packages/tasks/src/tasks/text-generation/inference.ts
+++ packages/tasks/src/tasks/text-generation/inference.ts
@@ -5,204 +5,94 @@
  */
 
 /**
- * Inputs for Text Generation inference
+ * Text Generation Input
  */
 export interface TextGenerationInput {
-	/**
-	 * The text to initialize generation with
-	 */
 	inputs: string;
-	/**
-	 * Additional inference parameters
-	 */
-	parameters?: TextGenerationParameters;
-	/**
-	 * Whether to stream output tokens
-	 */
-	stream?: boolean;
+	parameters?: Parameters;
 	[property: string]: unknown;
 }
 
-/**
- * Additional inference parameters
- *
- * Additional inference parameters for Text Generation
- */
-export interface TextGenerationParameters {
-	/**
-	 * The number of sampling queries to run. Only the best one (in terms of total logprob) will
-	 * be returned.
-	 */
+export interface Parameters {
 	best_of?: number;
-	/**
-	 * Whether or not to output decoder input details
-	 */
 	decoder_input_details?: boolean;
-	/**
-	 * Whether or not to output details
-	 */
 	details?: boolean;
-	/**
-	 * Whether to use logits sampling instead of greedy decoding when generating new tokens.
-	 */
 	do_sample?: boolean;
-	/**
-	 * The maximum number of tokens to generate.
-	 */
+	frequency_penalty?: number;
+	grammar?: Grammar;
 	max_new_tokens?: number;
-	/**
-	 * The parameter for repetition penalty. A value of 1.0 means no penalty. See [this
-	 * paper](https://hf.co/papers/1909.05858) for more details.
-	 */
 	repetition_penalty?: number;
-	/**
-	 * Whether to prepend the prompt to the generated text.
-	 */
 	return_full_text?: boolean;
-	/**
-	 * The random sampling seed.
-	 */
 	seed?: number;
-	/**
-	 * Stop generating tokens if a member of `stop_sequences` is generated.
-	 */
-	stop_sequences?: string[];
-	/**
-	 * The value used to modulate the logits distribution.
-	 */
+	stop?: string[];
 	temperature?: number;
-	/**
-	 * The number of highest probability vocabulary tokens to keep for top-k-filtering.
-	 */
 	top_k?: number;
-	/**
-	 * If set to < 1, only the smallest set of most probable tokens with probabilities that add
-	 * up to `top_p` or higher are kept for generation.
-	 */
+	top_n_tokens?: number;
 	top_p?: number;
-	/**
-	 * Truncate input tokens to the given size.
-	 */
 	truncate?: number;
-	/**
-	 * Typical Decoding mass. See [Typical Decoding for Natural Language
-	 * Generation](https://hf.co/papers/2202.00666) for more information
-	 */
 	typical_p?: number;
-	/**
-	 * Watermarking with [A Watermark for Large Language Models](https://hf.co/papers/2301.10226)
-	 */
 	watermark?: boolean;
 	[property: string]: unknown;
 }
 
+export interface Grammar {
+	type: Type;
+	/**
+	 * A string that represents a [JSON Schema](https://json-schema.org/).
+	 *
+	 * JSON Schema is a declarative language that allows to annotate JSON documents
+	 * with types and descriptions.
+	 */
+	value: unknown;
+	[property: string]: unknown;
+}
+
+export type Type = "json" | "regex";
+
 /**
- * Outputs for Text Generation inference
+ * Text Generation Output
  */
 export interface TextGenerationOutput {
-	/**
-	 * When enabled, details about the generation
-	 */
 	details?: TextGenerationOutputDetails;
-	/**
-	 * The generated text
-	 */
 	generated_text: string;
 	[property: string]: unknown;
 }
 
-/**
- * When enabled, details about the generation
- */
 export interface TextGenerationOutputDetails {
-	/**
-	 * Details about additional sequences when best_of is provided
-	 */
-	best_of_sequences?: TextGenerationOutputSequenceDetails[];
-	/**
-	 * The reason why the generation was stopped.
-	 */
-	finish_reason: TextGenerationFinishReason;
-	/**
-	 * The number of generated tokens
-	 */
+	best_of_sequences?: BestOfSequenceElement[];
+	finish_reason: FinishReason;
 	generated_tokens: number;
-	prefill: TextGenerationPrefillToken[];
-	/**
-	 * The random seed used for generation
-	 */
+	prefill: PrefillElement[];
 	seed?: number;
-	/**
-	 * The generated tokens and associated details
-	 */
-	tokens: TextGenerationOutputToken[];
-	/**
-	 * Most likely tokens
-	 */
-	top_tokens?: Array<TextGenerationOutputToken[]>;
+	tokens: TokenElement[];
+	top_tokens?: Array<TokenElement[]>;
 	[property: string]: unknown;
 }
 
-export interface TextGenerationOutputSequenceDetails {
-	finish_reason: TextGenerationFinishReason;
-	/**
-	 * The generated text
-	 */
+export interface BestOfSequenceElement {
+	finish_reason: FinishReason;
 	generated_text: string;
-	/**
-	 * The number of generated tokens
-	 */
 	generated_tokens: number;
-	prefill: TextGenerationPrefillToken[];
-	/**
-	 * The random seed used for generation
-	 */
+	prefill: PrefillElement[];
 	seed?: number;
-	/**
-	 * The generated tokens and associated details
-	 */
-	tokens: TextGenerationOutputToken[];
-	/**
-	 * Most likely tokens
-	 */
-	top_tokens?: Array<TextGenerationOutputToken[]>;
+	tokens: TokenElement[];
+	top_tokens?: Array<TokenElement[]>;
 	[property: string]: unknown;
 }
 
-/**
- * The reason why the generation was stopped.
- *
- * length: The generated sequence reached the maximum allowed length
- *
- * eos_token: The model generated an end-of-sentence (EOS) token
- *
- * stop_sequence: One of the sequence in stop_sequences was generated
- */
-export type TextGenerationFinishReason = "length" | "eos_token" | "stop_sequence";
+export type FinishReason = "length" | "eos_token" | "stop_sequence";
 
-export interface TextGenerationPrefillToken {
+export interface PrefillElement {
 	id: number;
 	logprob: number;
-	/**
-	 * The text associated with that token
-	 */
 	text: string;
 	[property: string]: unknown;
 }
 
-/**
- * Generated token.
- */
-export interface TextGenerationOutputToken {
+export interface TokenElement {
 	id: number;
-	logprob?: number;
-	/**
-	 * Whether or not that token is a special one
-	 */
+	logprob: number;
 	special: boolean;
-	/**
-	 * The text associated with that token
-	 */
 	text: string;
 	[property: string]: unknown;
 }
@@ -211,40 +101,25 @@ export interface TextGenerationOutputToken {
  * Text Generation Stream Output
  */
 export interface TextGenerationStreamOutput {
-	/**
-	 * Generation details. Only available when the generation is finished.
-	 */
-	details?: TextGenerationStreamDetails;
-	/**
-	 * The complete generated text. Only available when the generation is finished.
-	 */
+	details?: TextGenerationStreamOutputDetails;
 	generated_text?: string;
-	/**
-	 * The token index within the stream. Optional to support older clients that omit it.
-	 */
-	index?: number;
-	/**
-	 * Generated token.
-	 */
-	token: TextGenerationOutputToken;
+	index: number;
+	token: Token;
+	top_tokens?: Token[];
 	[property: string]: unknown;
 }
 
-/**
- * Generation details. Only available when the generation is finished.
- */
-export interface TextGenerationStreamDetails {
-	/**
-	 * The reason why the generation was stopped.
-	 */
-	finish_reason: TextGenerationFinishReason;
-	/**
-	 * The number of generated tokens
-	 */
+export interface TextGenerationStreamOutputDetails {
+	finish_reason: FinishReason;
 	generated_tokens: number;
-	/**
-	 * The random seed used for generation
-	 */
-	seed: number;
+	seed?: number;
+	[property: string]: unknown;
+}
+
+export interface Token {
+	id: number;
+	logprob: number;
+	special: boolean;
+	text: string;
 	[property: string]: unknown;
 }
