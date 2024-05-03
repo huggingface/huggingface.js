d5cb9bd0177a756d011ca25d41c44712d67f2859
Mishig Davaadorj <dmishig@gmail.com>
2024-05-03 12:09:30 +0200

    format

--- packages/widgets/src/routes/+page.svelte
+++ packages/widgets/src/routes/+page.svelte
@@ -42,7 +42,8 @@
 				architectures: ["LlamaForCausalLM"],
 				model_type: "llama",
 				tokenizer_config: {
-					chat_template: "{% set loop_messages = messages %}{% for message in loop_messages %}{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}",
+					chat_template:
+						"{% set loop_messages = messages %}{% for message in loop_messages %}{% set content = '<|start_header_id|>' + message['role'] + '<|end_header_id|>\n\n'+ message['content'] | trim + '<|eot_id|>' %}{% if loop.index0 == 0 %}{% set content = bos_token + content %}{% endif %}{{ content }}{% endfor %}{% if add_generation_prompt %}{{ '<|start_header_id|>assistant<|end_header_id|>\n\n' }}{% endif %}",
 					bos_token: "<|begin_of_text|>",
 					eos_token: "<|end_of_text|>",
 				},
