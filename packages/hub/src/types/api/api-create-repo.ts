b07184a38c82eb0fee500123656fcfa4ad7a32d4
coyotte508 <coyotte508@gmail.com>
2024-05-06 16:50:18 +0200

    ➕ Add missing files

--- packages/hub/src/types/api/api-create-repo.ts
+++ packages/hub/src/types/api/api-create-repo.ts
@@ -1,4 +1,4 @@
-import type { SetRequired } from "type-fest";
+import type { SetRequired } from "../../vendor/type-fest/set-required";
 import type { RepoType, SpaceHardwareFlavor, SpaceSdk } from "../public";
 import type { ApiCommitFile } from "./api-commit";
 
