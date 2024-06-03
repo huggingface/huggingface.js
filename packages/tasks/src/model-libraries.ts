83f4bfb721eb728399f86da9041f7060258a1773
Niels <niels.rogge1@gmail.com>
2024-06-03 16:55:30 +0200

    Add library

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -111,6 +111,15 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 			wildcard: { path: "*.npz" },
 		},
 	},
+	chat_tts: {
+		prettyLabel: "ChatTTS",
+		repoName: "ChatTTS",
+		repoUrl: "https://github.com/2noise/ChatTTS.git",
+		filter: false,
+		countDownloads: {
+			wildcard: { path: "asset/GPT.pt" },
+		},
+	},
 	diffusers: {
 		prettyLabel: "Diffusers",
 		repoName: "🤗/diffusers",
