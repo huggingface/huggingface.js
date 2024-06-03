ac41bc14ec8dcd0d367ad4fe722e03e2c575b52d
Niels <niels.rogge1@gmail.com>
2024-06-03 16:58:07 +0200

    Update repoName

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -113,7 +113,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 	},
 	chat_tts: {
 		prettyLabel: "ChatTTS",
-		repoName: "ChatTTS",
+		repoName: "chat_tts",
 		repoUrl: "https://github.com/2noise/ChatTTS.git",
 		filter: false,
 		countDownloads: {
