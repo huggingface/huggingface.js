f925d996a9960ed82743de6428a104bc102e9dc8
enzo <eesteves@hotmail.fr>
2024-06-14 09:39:42 -0400

    core: ✨ add new package space-header (#752)
    Add the `mini header` into your project if running on a custom/sub
    domain
    
    <img width="443" alt="Screenshot 2024-06-13 at 11 24 02 AM"
    src="https://github.com/huggingface/huggingface.js/assets/15983122/f7bb8817-b883-412c-8384-44b748998940">
    
    Example:
    
    ```js
    import { init } from "@huggingface/space-header"
    
    // ...
    
    init("enzostvs/lora-studio")
    ```
    
    Must be called once the page has been mounted. If this is not the case,
    a log error is displayed to prevent the user
    
    
    we've decided to stick with vanilla JS, which may evolve if necessary.
    (web component, etc)
    
    ---------
    
    Co-authored-by: coyotte508 <coyotte508@gmail.com>

---
+++ packages/space-header/src/header/components/box.ts
@@ -0,0 +1,35 @@
+export const Box = (): HTMLDivElement => {
+	const box = document.createElement("div");
+
+	box.style.backgroundImage = "linear-gradient(to top, #f9fafb, white)";
+	box.style.border = "1px solid #e5e7eb";
+	box.style.borderRadius = "0.75rem";
+	box.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
+	box.style.color = "#374151";
+	box.style.display = "flex";
+	box.style.flexDirection = "row";
+	box.style.alignItems = "center";
+	box.style.height = "40px";
+	box.style.justifyContent = "space-between";
+	box.style.overflow = "hidden";
+	box.style.position = "fixed";
+	box.style.right = "1.5rem";
+	box.style.top = "1.25rem";
+	box.style.width = "auto";
+	box.style.zIndex = "20";
+	box.style.padding = "0.75rem";
+	box.style.paddingLeft = "1rem";
+	box.style.paddingRight = "0rem";
+
+	box.setAttribute("id", "huggingface-space-header");
+
+	window.matchMedia("(max-width: 768px)").addEventListener("change", (e) => {
+		if (e.matches) {
+			box.style.display = "none";
+		} else {
+			box.style.display = "flex";
+		}
+	});
+
+	return box;
+};
