3d5db7d20e46b0d8e7569fbfb8023914dbb7773b
ngxson <thichthat@gmail.com>
2024-06-28 15:40:20 +0200

    remove guide to install deps

--- packages/tasks/src/local-apps.ts
+++ packages/tasks/src/local-apps.ts
@@ -81,13 +81,9 @@ const snippetLlamacpp = (model: ModelData, filepath?: string): Snippet[] => {
 		{
 			title: "Build from source code",
 			setup: [
-				"# Install required packages",
-				"sudo apt install build-essential libcurl4-openssl-dev",
-				"",
-				"# Clone and build",
 				"git clone https://github.com/ggerganov/llama.cpp.git",
 				"cd llama.cpp",
-				"LLAMA_CURL=1 make -j llama-cli",
+				"LLAMA_CURL=1 make llama-cli",
 			].join("\n"),
 			command: command("./llama-cli"),
 		},
