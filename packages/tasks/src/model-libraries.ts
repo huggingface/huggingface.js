15433ea59521d3906d57223d5b809a32880e38e4
Wauplin <lucainp@gmail.com>
2024-07-02 15:18:51 +0200

    update tf-keras docsUrl

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -249,7 +249,7 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		prettyLabel: "TF-Keras",
 		repoName: "TF-Keras",
 		repoUrl: "https://github.com/keras-team/tf-keras",
-		docsUrl: "https://huggingface.co/docs/hub/keras#tf-keras-legacy",
+		docsUrl: "https://huggingface.co/docs/hub/tf-keras",
 		snippets: snippets.tf_keras,
 		filter: true,
 		countDownloads: `path:"saved_model.pb"`,
