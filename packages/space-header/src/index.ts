24c0b881289726eaf72ed84afe4e53ca0c538c14
enzo <eesteves@hotmail.fr>
2024-07-15 08:51:34 -0400

    avoid duplicate header, check the ancestor origin

--- packages/space-header/src/index.ts
+++ packages/space-header/src/index.ts
@@ -9,7 +9,10 @@ import { inject } from "./inject";
 async function main(initialSpace: string | Space, options?: Options) {
 	if (window === undefined) return console.error("Please run this script in a browser environment");
 	// Don't run on huggingface.co to avoid duplicate headers
-	if (window.location?.origin === "https://huggingface.co") return;
+	const has_huggingface_ancestor = Object.values(window.location?.ancestorOrigins ?? {}).some((origin) =>
+		origin.includes("huggingface.co")
+	);
+	if (has_huggingface_ancestor) return;
 
 	inject_fonts();
 
