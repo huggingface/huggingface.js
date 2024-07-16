1718dea171bb9091d8b7e0d29dea8157bc6063ae
Niels <niels.rogge1@gmail.com>
2024-07-16 21:45:34 +0200

    Add library

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -160,6 +160,14 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: true,
 		/// diffusers has its own more complex "countDownloads" query
 	},
+	"dynamicrafter": {
+		prettyLabel: "DynamiCrafter",
+		repoName: "DynamiCrafter",
+		repoUrl: "https://github.com/Doubiiu/DynamiCrafter",
+		snippets: snippets.dynamicrafter,
+		filter: false,
+		countDownloads: `path_extension:"ckpt"`,
+	},
 	doctr: {
 		prettyLabel: "docTR",
 		repoName: "doctr",
