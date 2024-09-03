8735fed7e2aa143d54db94cb7fb0bfd9067b36a8
Alex Kiura <kiuraalex@gmail.com>
2024-09-03 20:23:33 +0300

    add correct indentation for qualcomm gpus

--- packages/tasks/src/hardware.ts
+++ packages/tasks/src/hardware.ts
@@ -276,22 +276,22 @@ export const SKUS = {
 			},
 		},
     QUALCOMM: {
-        "Snapdragon X Elite X1E-00-1DE": {
-          tflops: 4.6
-        },
-        "Snapdragon X Elite X1E-84-100": {
-          tflops: 4.6
-        },
-        "Snapdragon X Elite X1E-80-100": {
-          tflops: 3.8
-        },
-        "Snapdragon X Elite X1E-78-100": {
-          tflops: 3.8
-        },
-        "Snapdragon X Plus X1P-64-100": {
-          tflops: 3.8
-        },
+      "Snapdragon X Elite X1E-00-1DE": {
+        tflops: 4.6
       },
+      "Snapdragon X Elite X1E-84-100": {
+        tflops: 4.6
+      },
+      "Snapdragon X Elite X1E-80-100": {
+        tflops: 3.8
+      },
+      "Snapdragon X Elite X1E-78-100": {
+        tflops: 3.8
+      },
+      "Snapdragon X Plus X1P-64-100": {
+        tflops: 3.8
+      },
+    },
 	},
 	CPU: {
 		Intel: {
