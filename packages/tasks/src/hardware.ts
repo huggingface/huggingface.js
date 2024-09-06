e4c3eaee24db31bf13ba89854226b5833df67c39
Alex <kiuraalex@gmail.com>
2024-09-06 15:05:26 +0300

    Adds Snapdragon X Elite and X Plus GPU scores to hardware.ts (#881)
    Added:
    * Snapdragon X Elite
      * X1E-00-1DE
      * X1E-84-100
      * X1E-80-100
      * X1E-78-100
      
    * Snapdragon X Plus
      * X1P-64-100
     
    Addresses: #764 
    Source:
    https://www.qualcomm.com/products/mobile/snapdragon/laptops-and-tablets/snapdragon-x-elite
    
    ---------
    
    Co-authored-by: Vaibhav Srivastav <vaibhavs10@gmail.com>

--- packages/tasks/src/hardware.ts
+++ packages/tasks/src/hardware.ts
@@ -275,6 +275,23 @@ export const SKUS = {
 				memory: [16],
 			},
 		},
+		QUALCOMM: {
+			"Snapdragon X Elite X1E-00-1DE": {
+				tflops: 4.6,
+			},
+			"Snapdragon X Elite X1E-84-100": {
+				tflops: 4.6,
+			},
+			"Snapdragon X Elite X1E-80-100": {
+				tflops: 3.8,
+			},
+			"Snapdragon X Elite X1E-78-100": {
+				tflops: 3.8,
+			},
+			"Snapdragon X Plus X1P-64-100": {
+				tflops: 3.8,
+			},
+		},
 	},
 	CPU: {
 		Intel: {
