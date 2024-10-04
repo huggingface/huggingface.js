ffedef4b392092b87161b74fb22114461c4c4876
Mishig Davaadorj <dmishig@gmail.com>
2024-10-04 15:10:36 +0200

    [vLLM Snippet] Delete extra newline

--- packages/tasks/src/local-apps.ts
+++ packages/tasks/src/local-apps.ts
@@ -144,7 +144,6 @@ const snippetLocalAI = (model: ModelData, filepath?: string): LocalAppSnippet[] 
 
 const snippetVllm = (model: ModelData): LocalAppSnippet[] => {
 	const runCommand = [
-		"",
 		"# Call the server using curl:",
 		`curl -X POST "http://localhost:8000/v1/chat/completions" \\ `,
 		`	-H "Content-Type: application/json" \\ `,
