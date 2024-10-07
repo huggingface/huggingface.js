bd1c0a21dad98a440d7378796f4daf457450627f
Mishig Davaadorj <dmishig@gmail.com>
2024-10-07 16:42:25 +0200

    [vLLM Snippet] pipeline has to be one of "text-generation", "image-text-to-text"

--- packages/tasks/src/local-apps.ts
+++ packages/tasks/src/local-apps.ts
@@ -205,12 +205,13 @@ export const LOCAL_APPS = {
 		docsUrl: "https://docs.vllm.ai",
 		mainTask: "text-generation",
 		displayOnModelPage: (model: ModelData) =>
-			isAwqModel(model) ||
-			isGptqModel(model) ||
-			isAqlmModel(model) ||
-			isMarlinModel(model) ||
-			isLlamaCppGgufModel(model) ||
-			isTransformersModel(model),
+			(isAwqModel(model) ||
+				isGptqModel(model) ||
+				isAqlmModel(model) ||
+				isMarlinModel(model) ||
+				isLlamaCppGgufModel(model) ||
+				isTransformersModel(model)) &&
+			(model.pipeline_tag === "text-generation" || model.pipeline_tag === "image-text-to-text"),
 		snippet: snippetVllm,
 	},
 	lmstudio: {
