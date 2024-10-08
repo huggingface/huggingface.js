60d8defe8c1a697e4a0e401a957044f1e568d105
Pedro Cuenca <pedro@huggingface.co>
2024-10-08 20:43:55 +0200

    Remove PIL import

--- packages/tasks/src/model-libraries-snippets.ts
+++ packages/tasks/src/model-libraries-snippets.ts
@@ -144,15 +144,14 @@ export const depth_pro = (model: ModelData): string[] => {
 pip install huggingface-hub
 huggingface-cli download --local-dir checkpoints ${model.id}`;
 
-	const inferenceSnippet = `from PIL import Image
-import depth_pro
+	const inferenceSnippet = `import depth_pro
 
 # Load model and preprocessing transform
 model, transform = depth_pro.create_model_and_transforms()
 model.eval()
 
 # Load and preprocess an image.
-image, _, f_px = depth_pro.load_rgb(image_path)
+image, _, f_px = depth_pro.load_rgb("example.png")
 image = transform(image)
 
 # Run inference.
