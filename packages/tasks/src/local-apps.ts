683cbd07b0b5f74d7ac4970562b01547ea07a38b
Gilad S. <7817232+giladgd@users.noreply.github.com>
2024-10-15 17:13:04 +0300

    feat: add `node-llama-cpp` library and CLI to GGUF models (#949)
    This PR adds a code snippet of using
    [`node-llama-cpp`](https://node-llama-cpp.withcat.ai) on GGUF models,
    and also CLI commands to:
    * Chat with the model
    * Estimate the model compatibility with the current hardware without
    downloading it
    
    [`node-llama-cpp`](https://node-llama-cpp.withcat.ai) makes using
    `llama.cpp` in Node.js easy, and also provides convenient CLI commands
    that can be used without even installing it (assuming Node.js is
    installed).
    
    I couldn't find where to add the icon for `node-llama-cpp`, so in case
    it's not in this repo, you can use [this
    icon](https://github.com/withcatai/node-llama-cpp/blob/master/assets/icon.v3.svg).
    
    ---------
    
    Co-authored-by: vb <vaibhavs10@gmail.com>

--- packages/tasks/src/local-apps.ts
+++ packages/tasks/src/local-apps.ts
@@ -126,6 +126,23 @@ const snippetLlamacpp = (model: ModelData, filepath?: string): LocalAppSnippet[]
 	];
 };
 
+const snippetNodeLlamaCppCli = (model: ModelData, filepath?: string): LocalAppSnippet[] => {
+	return [
+		{
+			title: "Chat with the model",
+			content: [
+				`npx -y node-llama-cpp chat \\`,
+				`  --model "hf:${model.id}/${filepath ?? "{{GGUF_FILE}}"}" \\`,
+				`  --prompt 'Hi there!'`,
+			].join("\n"),
+		},
+		{
+			title: "Estimate the model compatibility with your hardware",
+			content: `npx -y node-llama-cpp inspect estimate "hf:${model.id}/${filepath ?? "{{GGUF_FILE}}"}"`,
+		},
+	];
+};
+
 const snippetLocalAI = (model: ModelData, filepath?: string): LocalAppSnippet[] => {
 	const command = (binary: string) =>
 		["# Load and run the model:", `${binary} huggingface://${model.id}/${filepath ?? "{{GGUF_FILE}}"}`].join("\n");
@@ -234,6 +251,13 @@ export const LOCAL_APPS = {
 		mainTask: "text-generation",
 		displayOnModelPage: isLlamaCppGgufModel,
 		snippet: snippetLlamacpp,
+	},
+	"node-llama-cpp": {
+		prettyLabel: "node-llama-cpp",
+		docsUrl: "https://node-llama-cpp.withcat.ai",
+		mainTask: "text-generation",
+		displayOnModelPage: isLlamaCppGgufModel,
+		snippet: snippetNodeLlamaCppCli,
 	},
 	vllm: {
 		prettyLabel: "vLLM",
