451a223f52740986702ab48814ceb7f6220e3116
coyotte508 <coyotte508@protonmail.com>
2024-11-14 02:36:17 +0100

    ✨ Also export snippet types

--- packages/tasks/src/snippets/index.ts
+++ packages/tasks/src/snippets/index.ts
@@ -2,5 +2,6 @@ import * as inputs from "./inputs";
 import * as curl from "./curl";
 import * as python from "./python";
 import * as js from "./js";
+export * from "./types";
 
 export { inputs, curl, python, js };
