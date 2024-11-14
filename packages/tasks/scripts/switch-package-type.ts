730de17befbe9b293636ecd891d94322e701d8f3
coyotte508 <coyotte508@protonmail.com>
2024-11-14 23:41:39 +0100

    🔧 Use tshy for deploy

--- packages/tasks/scripts/switch-package-type.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-
-const arg = process.argv[2];
-
-const file = readFileSync("package.json", "utf-8");
-
-writeFileSync("package.json", file.replace(/"type": ".*"/, `"type": "${arg}"`));
