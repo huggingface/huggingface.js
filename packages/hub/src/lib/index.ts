57df98ebbda165d55007dfd0532ae4afaec6a955
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-11-15 15:54:06 +0100

    refactor: moving to download-file-cache.ts file

--- packages/hub/src/lib/index.ts
+++ packages/hub/src/lib/index.ts
@@ -8,6 +8,7 @@ export * from "./delete-file";
 export * from "./delete-files";
 export * from "./delete-repo";
 export * from "./download-file";
+export * from "./download-file-cache";
 export * from "./file-download-info";
 export * from "./file-exists";
 export * from "./list-commits";
