a27700eddd80318bc6707423914e1606f50c6f50
Zeke Sikelianos <zeke@sikelianos.com>
2025-01-14 14:06:28 -0800

    support versioned and unversioned replicate models

--- packages/inference/src/providers/replicate.ts
+++ packages/inference/src/providers/replicate.ts
@@ -17,5 +17,5 @@ type ReplicateId = string;
 export const REPLICATE_MODEL_IDS: Record<ModelId, ReplicateId> = {
 	/** text-to-image */
 	"black-forest-labs/FLUX.1-schnell": "black-forest-labs/flux-schnell",
-	"ByteDance/SDXL-Lightning": "bytedance/sdxl-lightning-4step",
+	"ByteDance/SDXL-Lightning": "bytedance/sdxl-lightning-4step:5599ed30703defd1d160a25a63321b4dec97101d98b4674bcc56e41f62f35637",
 };
