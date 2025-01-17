e44c7deaa24a552ef51ab0a96ff76c0478b5b81f
SBrandeis <simon@huggingface.co>
2025-01-17 12:09:20 +0100

    fix: exported constants

--- packages/inference/src/index.ts
+++ packages/inference/src/index.ts
@@ -1,8 +1,8 @@
 export { HfInference, HfInferenceEndpoint } from "./HfInference";
 export { InferenceOutputError } from "./lib/InferenceOutputError";
-export { FAL_AI_SUPPORTED_MODEL_IDS as FAL_AI_MODEL_IDS } from "./providers/fal-ai";
-export { REPLICATE_SUPPORTED_MODEL_IDS as REPLICATE_MODEL_IDS } from "./providers/replicate";
-export { SAMBANOVA_SUPPORTED_MODEL_IDS as SAMBANOVA_MODEL_IDS } from "./providers/sambanova";
-export { TOGETHER_API_BASE_URL } from "./providers/together";
+export { FAL_AI_SUPPORTED_MODEL_IDS } from "./providers/fal-ai";
+export { REPLICATE_SUPPORTED_MODEL_IDS } from "./providers/replicate";
+export { SAMBANOVA_SUPPORTED_MODEL_IDS } from "./providers/sambanova";
+export { TOGETHER_SUPPORTED_MODEL_IDS } from "./providers/together";
 export * from "./types";
 export * from "./tasks";
