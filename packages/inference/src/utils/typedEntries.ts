2c8429f34007bc2a3d600538143eecc84f020bee
SBrandeis <simon@huggingface.co>
2025-01-21 17:56:44 +0100

    rm unused typedEntries

--- packages/inference/src/utils/typedEntries.ts
+++
@@ -1,5 +0,0 @@
-import type { Entries } from "type-fest";
-
-export function typedEntries<T extends { [s: string]: T[keyof T] } | ArrayLike<T[keyof T]>>(obj: T): Entries<T> {
-	return Object.entries(obj) as Entries<T>;
-}
