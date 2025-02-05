huggingface/huggingface.js

commit 7ee542d81c52710a4c5c75d3b6ed30a59507e2b2

Update tasks (important ones) (#1163)
---------

Co-authored-by: vb <vaibhavs10@gmail.com>
Co-authored-by: Pedro Cuenca <pedro@huggingface.co>

packages/tasks/src/tasks/text-generation/data.ts
@@ -15,6 +15,10 @@ const taskData: TaskDataCustom = {
 			id: "HuggingFaceH4/ultrachat_200k",
 		},
 		{
+			description: "A reasoning dataset.",
+			id: "open-r1/OpenThoughts-114k-math",
+		},
+		{
 			description: "A multilingual instruction dataset with preference ratings on responses.",
 			id: "allenai/tulu-3-sft-mixture",
 		},
@@ -57,9 +61,12 @@ const taskData: TaskDataCustom = {
 		},
 	],
 	models: [
-		{
-			description: "A text-generation model trained to follow instructions.",
+		{	description: "A text-generation model trained to follow instructions.",
 			id: "google/gemma-2-2b-it",
+		},
+		{
+			description: "Smaller variant of one of the most powerful models.",
+			id: "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
 		},
 		{
 			description: "Very powerful text generation model trained to follow instructions.",
@@ -74,12 +81,16 @@ const taskData: TaskDataCustom = {
 			id: "PowerInfer/SmallThinker-3B-Preview",
 		},
 		{
-			description: "Strong text generation model to follow instructions.",
-			id: "Qwen/Qwen2.5-7B-Instruct",
+			description: "Strong conversational model that supports very long instructions.",
+			id: "Qwen/Qwen2.5-7B-Instruct-1M",
 		},
 		{
 			description: "Text generation model used to write code.",
 			id: "Qwen/Qwen2.5-Coder-32B-Instruct",
+		},
+		{
+			description: "Powerful reasoning based open large language model.",
+			id: "deepseek-ai/DeepSeek-R1",
 		},
 	],
 	spaces: [

packages/tasks/src/tasks/image-text-to-text/data.ts
@@ -60,7 +60,7 @@ const taskData: TaskDataCustom = {
 		},
 		{
 			description: "Strong image-text-to-text model.",
-			id: "Qwen/Qwen2-VL-7B-Instruct",
+			id: "Qwen/Qwen2.5-VL-7B-Instruct",
 		},
 		{
 			description: "Image-text-to-text model with reasoning capabilities.",
@@ -98,12 +98,12 @@ const taskData: TaskDataCustom = {
 		},
 		{
 			description: "An application that detects gaze.",
-			id: "smoondream/gaze-demo",
+			id: "moondream/gaze-demo",
 		},
 	],
 	summary:
 		"Image-text-to-text models take in an image and text prompt and output text. These models are also called vision-language models, or VLMs. The difference from image-to-text models is that these models take an additional text input, not restricting the model to certain use cases like image captioning, and may also be trained to accept a conversation as input.",
-	widgetModels: ["meta-llama/Llama-3.2-11B-Vision-Instruct"],
+	widgetModels: ["Qwen/Qwen2-VL-7B-Instruct"],
 	youtubeId: "IoGaGfU1CIg",
 };
 

commit b6b7fc4c28367712f2aa761e837832acfcbac302

Fix lint issue (#1183)
should fix lint issue introduced in
https://github.com/huggingface/huggingface.js/pull/1163

will merge if lint CI gets green

packages/tasks/src/tasks/text-generation/data.ts
@@ -61,9 +61,7 @@ const taskData: TaskDataCustom = {
 		},
 	],
 	models: [
-		{	description: "A text-generation model trained to follow instructions.",
-			id: "google/gemma-2-2b-it",
-		},
+		{ description: "A text-generation model trained to follow instructions.", id: "google/gemma-2-2b-it" },
 		{
 			description: "Smaller variant of one of the most powerful models.",
 			id: "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",