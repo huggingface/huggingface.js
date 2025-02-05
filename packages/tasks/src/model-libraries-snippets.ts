5589a3a41b1fe98d03ab6d26308817bc68f2ea1c
Lucain <lucainp@gmail.com>
2025-02-05 15:29:47 +0100

    Update packages/tasks/src/model-libraries-snippets.ts

--- packages/tasks/src/model-libraries-snippets.ts
+++ packages/tasks/src/model-libraries-snippets.ts
@@ -87,6 +87,7 @@ export const ben2 = (model: ModelData): string[] => [
 	`import requests
 from PIL import Image
 from ben2 import AutoModel
+
 url = "https://upload.wikimedia.org/wikipedia/commons/b/b6/Felis_catus-cat_on_snow.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
