72de8ad72c5f34cd462f430f238a3810fd4a53aa
Julien Chaumond <julien@huggingface.co>
2025-02-05 17:40:22 +0100

    Example of empty mapping to let devs override locally

--- packages/inference/src/providers/sambanova.ts
+++ packages/inference/src/providers/sambanova.ts
@@ -2,24 +2,20 @@ import type { ProviderMapping } from "./types";
 
 export const SAMBANOVA_API_BASE_URL = "https://api.sambanova.ai";
 
-type SambanovaId = string;
+/**
+ * See the registered mapping of HF model ID => Sambanova model ID here:
+ *
+ * https://huggingface.co/api/partners/sambanova/models
+ *
+ * This is a publicly available mapping.
+ *
+ * If you want to try to run inference for a new model locally before it's registered on huggingface.co,
+ * you can add it to the dictionary below.
+ * The dictionary is keyed by model task type (ie. "conversational" for LLMs, "text-to-image" for Flux, etc)
+ */
 
-export const SAMBANOVA_SUPPORTED_MODEL_IDS: ProviderMapping<SambanovaId> = {
-	/** Chat completion / conversational */
+export const SAMBANOVA_SUPPORTED_MODEL_IDS: ProviderMapping = {
 	conversational: {
-		"allenai/Llama-3.1-Tulu-3-405B": "Llama-3.1-Tulu-3-405B",
-		"deepseek-ai/DeepSeek-R1-Distill-Llama-70B": "DeepSeek-R1-Distill-Llama-70B",
-		"Qwen/Qwen2.5-Coder-32B-Instruct": "Qwen2.5-Coder-32B-Instruct",
-		"Qwen/Qwen2.5-72B-Instruct": "Qwen2.5-72B-Instruct",
-		"Qwen/QwQ-32B-Preview": "QwQ-32B-Preview",
-		"meta-llama/Llama-3.3-70B-Instruct": "Meta-Llama-3.3-70B-Instruct",
-		"meta-llama/Llama-3.2-1B-Instruct": "Meta-Llama-3.2-1B-Instruct",
-		"meta-llama/Llama-3.2-3B-Instruct": "Meta-Llama-3.2-3B-Instruct",
-		"meta-llama/Llama-3.2-11B-Vision-Instruct": "Llama-3.2-11B-Vision-Instruct",
-		"meta-llama/Llama-3.2-90B-Vision-Instruct": "Llama-3.2-90B-Vision-Instruct",
-		"meta-llama/Llama-3.1-8B-Instruct": "Meta-Llama-3.1-8B-Instruct",
-		"meta-llama/Llama-3.1-70B-Instruct": "Meta-Llama-3.1-70B-Instruct",
-		"meta-llama/Llama-3.1-405B-Instruct": "Meta-Llama-3.1-405B-Instruct",
-		"meta-llama/Llama-Guard-3-8B": "Meta-Llama-Guard-3-8B",
+		// "Qwen/Qwen2.5-Coder-32B-Instruct": "Qwen2.5-Coder-32B-Instruct",
 	},
 };
