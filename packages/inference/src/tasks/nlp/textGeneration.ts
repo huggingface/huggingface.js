f1c729dfb3b6c2e9264f9fe9a1d099eaae78c0cd
Olivier Toromanoff <olivier.toromanoff@stability.ai>
2025-02-05 14:49:17 +0000

    text-generation for replicate

--- packages/inference/src/tasks/nlp/textGeneration.ts
+++ packages/inference/src/tasks/nlp/textGeneration.ts
@@ -6,10 +6,16 @@ import type {
 } from "@huggingface/tasks";
 import { InferenceOutputError } from "../../lib/InferenceOutputError";
 import type { BaseArgs, Options } from "../../types";
+import { omit } from "../../utils/omit";
 import { toArray } from "../../utils/toArray";
 import { request } from "../custom/request";
 
 export type { TextGenerationInput, TextGenerationOutput };
+
+interface ReplicateTextCompletionOutput {
+	status: string;
+	output?: string[];
+}
 
 interface TogeteherTextCompletionOutput extends Omit<ChatCompletionOutput, "choices"> {
 	choices: Array<{
@@ -42,6 +48,36 @@ export async function textGeneration(
 		const completion = raw.choices[0];
 		return {
 			generated_text: completion.text,
+		};
+	} else if (args.provider === "replicate") {
+		const payload = {
+			...omit(args, ["inputs", "parameters"]),
+			...args.parameters,
+			prompt: args.inputs,
+		};
+
+		const raw = await request<ReplicateTextCompletionOutput>(payload, {
+			...options,
+			taskHint: "text-generation",
+		});
+
+		if (typeof raw !== "object" || !("status" in raw)) {
+			throw new InferenceOutputError("Incomplete response");
+		}
+
+		const status = raw.status;
+		if (status === "starting") {
+			throw new InferenceOutputError("Replicate server-side time out");
+		}
+
+		if (!("output" in raw && Array.isArray(raw?.output))) {
+			throw new InferenceOutputError("Invalid response: no output");
+		}
+
+		const joined_output = raw.output.join("");
+
+		return {
+			generated_text: joined_output,
 		};
 	} else {
 		const res = toArray(
