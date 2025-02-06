671d03ac3ac3ed23efadba20407d931ac490d0bc
Omar Sanseviero <osanseviero@gmail.com>
2025-02-06 12:03:22 +0100

    Delete widgets code (#902)
    The code from widgets has been moved back to internal. Having it
    duplicated has led to confusion with the icons and widgets, so this PR
    proposes to delete all of it
    
    ---------
    
    Co-authored-by: Eliott C. <coyotte508@gmail.com>
    Co-authored-by: coyotte508 <coyotte508@protonmail.com>

--- packages/widgets/src/lib/components/Icons/IconKeypointDetection.svelte
+++
@@ -1,1 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...$$props}><path fill="currentColor" d="m28.316 13.949l-.632-1.898L17 15.612V4h-2v11.612L4.316 12.051l-.632 1.898l10.684 3.561L7.2 27.066l1.6 1.201l7.2-9.6l7.2 9.6l1.6-1.201l-7.168-9.556z"/></svg>
