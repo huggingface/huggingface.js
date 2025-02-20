d199314058eac8c77fe86eaed43db28f8e5788f4
SBrandeis <simon@huggingface.co>
2025-02-20 17:46:46 +0100

    fix fixture??

--- packages/tasks-gen/snippets-fixtures/text-to-image/1.fal-client.fal-ai.py
+++ packages/tasks-gen/snippets-fixtures/text-to-image/1.fal-client.fal-ai.py
@@ -2,7 +2,7 @@ import fal_client
 
 result = fal_client.subscribe(
 	# replace with correct id from fal.ai
-	"fal-ai/<fal-ai alias for black-forest-labs/FLUX.1-schnell>",
+	"<fal-ai alias for black-forest-labs/FLUX.1-schnell>",
 	arguments={
 		"prompt": "Astronaut riding a horse",
 	},
