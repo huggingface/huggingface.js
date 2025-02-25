179a5e9dffb3a9a08f2631e71e8439ea62f9c0e2
Julien Chaumond <julien@huggingface.co>
2025-02-25 14:51:23 +0100

    Fireworks=>HF

--- packages/inference/src/providers/hf-inference.ts
+++ packages/inference/src/providers/hf-inference.ts
@@ -4,9 +4,9 @@
  * If you want to try to run inference for a new model locally before it's registered on huggingface.co,
  * you can add it to the dictionary "HARDCODED_MODEL_ID_MAPPING" in consts.ts, for dev purposes.
  *
- * - If you work at Fireworks and want to update this mapping, please use the model mapping API we provide on huggingface.co
- * - If you're a community member and want to add a new supported HF model to Fireworks, please open an issue on the present repo
- * and we will tag Fireworks team members.
+ * - If you work at HF and want to update this mapping, please use the model mapping API we provide on huggingface.co
+ * - If you're a community member and want to add a new supported HF model to HF, please open an issue on the present repo
+ * and we will tag HF team members.
  *
  * Thanks!
  */
