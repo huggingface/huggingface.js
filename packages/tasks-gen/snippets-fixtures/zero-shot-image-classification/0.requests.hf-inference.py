7a498c6fe588a0739b6e1ceace1d277b71901f46
Wauplin <lucainp@gmail.com>
2025-03-06 18:50:18 +0100

    tabs -> whitespaces

--- packages/tasks-gen/snippets-fixtures/zero-shot-image-classification/0.requests.hf-inference.py
+++ packages/tasks-gen/snippets-fixtures/zero-shot-image-classification/0.requests.hf-inference.py
@@ -5,16 +5,16 @@ API_URL = "https://router.huggingface.co/hf-inference/models/openai/clip-vit-lar
 headers = {"Authorization": "Bearer api_token"}
 
 def query(data):
-	with open(data["image_path"], "rb") as f:
-		img = f.read()
-	payload={
-		"parameters": data["parameters"],
-		"inputs": base64.b64encode(img).decode("utf-8")
-	}
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+    with open(data["image_path"], "rb") as f:
+        img = f.read()
+    payload={
+        "parameters": data["parameters"],
+        "inputs": base64.b64encode(img).decode("utf-8")
+    }
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
 
 output = query({
-	"image_path": "cats.jpg",
-	"parameters": {"candidate_labels": ["cat", "dog", "llama"]},
+    "image_path": "cats.jpg",
+    "parameters": {"candidate_labels": ["cat", "dog", "llama"]},
 })
