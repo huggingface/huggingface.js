2a5443f6cd8dbbc5fff664c0c4f83c9458a90c23
Lucain <lucain@huggingface.co>
2025-03-12 10:24:23 +0100

    [Inference snippets] Templated snippets for inference snippet generation (#1255)
    ### What does this PR do?
    - add `@huggingface/jinja` as a dependency for `@huggingface/inference`
    - heavily refactor
    [`./packages/inference/src/snippets/python.ts`](https://github.com/huggingface/huggingface.js/blob/ddd62b60d0a858dc808b6dd7b3589977681f5a8b/packages/inference/src/snippets/python.ts)
    to use templates instead of inline strings
    - add some snippet templates under
    `packages/inference/src/snippets/templates/{language}/{client}/{templateName}.jinja`
        - `language` is Python
        - `client` is e.g. huggingface_hub, requests, etc.
    - `templateName` is related to task name e.g. textToImage,
    conversationalStream, etc.
    
    => overall goal is to make it rather easy to update the snippets for a
    given task + client without entering the JS code
    => if conclusive, we can extend to cURL / JS snippets in a follow-up PR 
    
    This PR also includes a lot of small fixes (usually whitespace<>tabs
    inconsistencies) + added some tests. I did not change anything major in
    the generated snippets.
    
    ### What to review?
    
    IMO, no need to review all templates. Better to check the generated
    snippets + the main JS file directly
    (https://github.com/huggingface/huggingface.js/blob/ddd62b60d0a858dc808b6dd7b3589977681f5a8b/packages/inference/src/snippets/python.ts).
    Sorry for the huge PR but I do believe it is a necessary move moving
    forward :hugs:
    
    ---------
    
    Co-authored-by: Simon Brandeis <33657802+SBrandeis@users.noreply.github.com>
    Co-authored-by: coyotte508 <coyotte508@protonmail.com>

---
+++ packages/tasks-gen/snippets-fixtures/tabular/0.requests.hf-inference.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = "https://router.huggingface.co/hf-inference/models/templates/tabular-classification"
+headers = {"Authorization": "Bearer api_token"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content
+
+response = query({
+    "inputs": {
+        "data": '{"Height":[11.52,12.48],"Length1":[23.2,24.0],"Length2":[25.4,26.3],"Species": ["Bream","Bream"]}'
+    },
+})
