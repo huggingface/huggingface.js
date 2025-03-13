9eca7897362a593c50c89a65e77e316d650a10cc
coyotte508 <coyotte508@protonmail.com>
2025-03-13 18:36:05 +0100

    Add failing test

--- packages/hub/src/utils/XetBlob.spec.ts
+++ packages/hub/src/utils/XetBlob.spec.ts
@@ -15,6 +15,92 @@ describe("XetBlob", () => {
 		expect(await blob.slice(10, 22).text()).toBe("__metadata__");
 	}, 30_000);
 
+	it("should load the first chunk correctly", async () => {
+		let xorbCount = 0;
+		const blob = new XetBlob({
+			repo: {
+				type: "model",
+				name: "celinah/xet-experiments",
+			},
+			hash: "7b3b6d07673a88cf467e67c1f7edef1a8c268cbf66e9dd9b0366322d4ab56d9b",
+			size: 5_234_139_343,
+			fetch: async (url, opts) => {
+				if (typeof url === "string" && url.includes("/xorbs/")) {
+					xorbCount++;
+				}
+				return fetch(url, opts);
+			},
+		});
+
+		const xetDownload = await blob.slice(0, 29928).arrayBuffer();
+		const bridgeDownload = await fetch(
+			"https://huggingface.co/celinah/xet-experiments/resolve/main/model5GB.safetensors",
+			{
+				headers: {
+					Range: "bytes=0-29927",
+				},
+			}
+		).then((res) => res.arrayBuffer());
+
+		expect(new Uint8Array(xetDownload)).toEqual(new Uint8Array(bridgeDownload));
+		expect(xorbCount).toBe(1);
+	}, 30_000);
+
+	it("should load just past the first chunk correctly", async () => {
+		let xorbCount = 0;
+		const blob = new XetBlob({
+			repo: {
+				type: "model",
+				name: "celinah/xet-experiments",
+			},
+			hash: "7b3b6d07673a88cf467e67c1f7edef1a8c268cbf66e9dd9b0366322d4ab56d9b",
+			size: 5_234_139_343,
+			fetch: async (url, opts) => {
+				if (typeof url === "string" && url.includes("/xorbs/")) {
+					xorbCount++;
+				}
+				return fetch(url, opts);
+			},
+		});
+
+		const xetDownload = await blob.slice(0, 29929).arrayBuffer();
+		const bridgeDownload = await fetch(
+			"https://huggingface.co/celinah/xet-experiments/resolve/main/model5GB.safetensors",
+			{
+				headers: {
+					Range: "bytes=0-29928",
+				},
+			}
+		).then((res) => res.arrayBuffer());
+
+		expect(new Uint8Array(xetDownload)).toEqual(new Uint8Array(bridgeDownload));
+		expect(xetDownload.byteLength).toBe(29929);
+		expect(xorbCount).toBe(2);
+	});
+
+	it("should load correctly when loading far into a chunk range", async () => {
+		const blob = new XetBlob({
+			repo: {
+				type: "model",
+				name: "celinah/xet-experiments",
+			},
+			hash: "7b3b6d07673a88cf467e67c1f7edef1a8c268cbf66e9dd9b0366322d4ab56d9b",
+			size: 5_234_139_343,
+		});
+
+		const xetDownload = await blob.slice(10_000_000, 10_100_000).arrayBuffer();
+		const bridgeDownload = await fetch(
+			"https://huggingface.co/celinah/xet-experiments/resolve/main/model5GB.safetensors",
+			{
+				headers: {
+					Range: "bytes=10000000-10100000",
+				},
+			}
+		).then((res) => res.arrayBuffer());
+
+		expect(new Uint8Array(xetDownload)).toEqual(new Uint8Array(bridgeDownload));
+	});
+
 	describe("bg4_regoup_bytes", () => {
 		it("should regroup bytes when the array is %4 length", () => {
 			expect(bg4_regoup_bytes(new Uint8Array([1, 5, 2, 6, 3, 7, 4, 8]))).toEqual(
