8adb58c956fc9b4c7df3ae219019050fbfe09cd9
coyotte508 <coyotte508@protonmail.com>
2025-03-13 23:14:48 +0100

    Use os.homedir() instead of process.env.HOME

--- packages/hub/src/utils/symlink.ts
+++ packages/hub/src/utils/symlink.ts
@@ -15,7 +15,7 @@ export function reset(): void {
 
 function expandUser(path: string): string {
 	if (path.startsWith("~")) {
-		return path.replace("~", process.env.HOME ?? "");
+		return path.replace("~", os.homedir());
 	}
 	return path;
 }
