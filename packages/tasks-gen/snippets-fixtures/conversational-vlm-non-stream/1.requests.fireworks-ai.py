52d32459aba5e3a2da8a7d499f899590be175181
Wauplin <lucainp@gmail.com>
2025-03-13 10:03:02 +0100

    add Python request conversational

---
+++ packages/tasks-gen/snippets-fixtures/conversational-vlm-non-stream/1.requests.fireworks-ai.py
@@ -0,0 +1,32 @@
+import requests
+
+API_URL = "https://api.fireworks.ai/inference/v1/chat/completions"
+headers = {"Authorization": "Bearer api_token"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+response = query({
+    "model": "<fireworks-ai alias for meta-llama/Llama-3.2-11B-Vision-Instruct>",
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe this image in one sentence."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
+                    }
+                }
+            ]
+        }
+    ],
+    "max_tokens": 500
+})
+
+print(response["choices"][0]["message"])
