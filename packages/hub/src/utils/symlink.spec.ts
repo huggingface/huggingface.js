609660e92bb448eed05a6cc60639dcedabf3e2a6
coyotte508 <coyotte508@protonmail.com>
2025-03-21 19:07:36 +0100

    one more test

--- packages/hub/src/utils/symlink.spec.ts
+++ packages/hub/src/utils/symlink.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { createSymlink } from "./symlink";
 import { readFileSync, writeFileSync } from "node:fs";
-import { lstat } from "node:fs/promises";
+import { lstat, rm } from "node:fs/promises";
 
 describe("utils/symlink", () => {
 	it("should create a symlink", async () => {
@@ -17,5 +17,34 @@ describe("utils/symlink", () => {
 		// Test file content
 		const content = readFileSync("/tmp/test-symlink.txt", "utf8");
 		expect(content).toBe("hello world");
+
+		// Cleanup
+		await rm("/tmp/test-symlink.txt");
+		await rm("/tmp/test.txt");
+	});
+
+	it("should work when symlinking twice", async () => {
+		writeFileSync("/tmp/test.txt", "hello world");
+		writeFileSync("/tmp/test2.txt", "hello world2");
+		await createSymlink({
+			sourcePath: "/tmp/test.txt",
+			finalPath: "/tmp/test-symlink.txt",
+		});
+		await createSymlink({
+			sourcePath: "/tmp/test2.txt",
+			finalPath: "/tmp/test-symlink.txt",
+		});
+
+		const stats = await lstat("/tmp/test-symlink.txt");
+		expect(stats.isSymbolicLink()).toBe(true);
+
+		// Test file content
+		const content = readFileSync("/tmp/test-symlink.txt", "utf8");
+		expect(content).toBe("hello world2");
+
+		// Cleanup
+		await rm("/tmp/test-symlink.txt");
+		await rm("/tmp/test.txt");
+		await rm("/tmp/test2.txt");
 	});
 });
