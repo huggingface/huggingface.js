a384e10b3af8e499c84ba27b58244c455dc1275d
coyotte508 <coyotte508@protonmail.com>
2025-03-21 23:23:23 +0100

    use os-spcific dir instead of /tmp

--- packages/hub/src/utils/symlink.spec.ts
+++ packages/hub/src/utils/symlink.spec.ts
@@ -4,6 +4,8 @@ import { describe, expect, it, vi } from "vitest";
 import { createSymlink } from "./symlink";
 import { readFileSync, writeFileSync } from "node:fs";
 import { lstat, rm } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
 
 let failSymlink = false;
 vi.mock("node:fs/promises", async (importOriginal) => ({
@@ -21,67 +23,67 @@ vi.mock("node:fs/promises", async (importOriginal) => ({
 
 describe("utils/symlink", () => {
 	it("should create a symlink", async () => {
-		writeFileSync("/tmp/test.txt", "hello world");
+		writeFileSync(join(tmpdir(), "test.txt"), "hello world");
 		await createSymlink({
-			sourcePath: "/tmp/test.txt",
-			finalPath: "/tmp/test-symlink.txt",
+			sourcePath: join(tmpdir(), "test.txt"),
+			finalPath: join(tmpdir(), "test-symlink.txt"),
 		});
 
-		const stats = await lstat("/tmp/test-symlink.txt");
+		const stats = await lstat(join(tmpdir(), "test-symlink.txt"));
 		expect(stats.isSymbolicLink()).toBe(true);
 
 		// Test file content
-		const content = readFileSync("/tmp/test-symlink.txt", "utf8");
+		const content = readFileSync(join(tmpdir(), "test-symlink.txt"), "utf8");
 		expect(content).toBe("hello world");
 
 		// Cleanup
-		await rm("/tmp/test-symlink.txt");
-		await rm("/tmp/test.txt");
+		await rm(join(tmpdir(), "test-symlink.txt"));
+		await rm(join(tmpdir(), "test.txt"));
 	});
 
 	it("should work when symlinking twice", async () => {
-		writeFileSync("/tmp/test.txt", "hello world");
-		writeFileSync("/tmp/test2.txt", "hello world2");
+		writeFileSync(join(tmpdir(), "test.txt"), "hello world");
+		writeFileSync(join(tmpdir(), "test2.txt"), "hello world2");
 		await createSymlink({
-			sourcePath: "/tmp/test.txt",
-			finalPath: "/tmp/test-symlink.txt",
+			sourcePath: join(tmpdir(), "test.txt"),
+			finalPath: join(tmpdir(), "test-symlink.txt"),
 		});
 		await createSymlink({
-			sourcePath: "/tmp/test2.txt",
-			finalPath: "/tmp/test-symlink.txt",
+			sourcePath: join(tmpdir(), "test2.txt"),
+			finalPath: join(tmpdir(), "test-symlink.txt"),
 		});
 
-		const stats = await lstat("/tmp/test-symlink.txt");
+		const stats = await lstat(join(tmpdir(), "test-symlink.txt"));
 		expect(stats.isSymbolicLink()).toBe(true);
 
 		// Test file content
-		const content = readFileSync("/tmp/test-symlink.txt", "utf8");
+		const content = readFileSync(join(tmpdir(), "test-symlink.txt"), "utf8");
 		expect(content).toBe("hello world2");
 
 		// Cleanup
-		await rm("/tmp/test-symlink.txt");
-		await rm("/tmp/test.txt");
-		await rm("/tmp/test2.txt");
+		await rm(join(tmpdir(), "test-symlink.txt"));
+		await rm(join(tmpdir(), "test.txt"));
+		await rm(join(tmpdir(), "test2.txt"));
 	});
 
 	it("should work when symlink doesn't work (windows)", async () => {
-		writeFileSync("/tmp/test.txt", "hello world");
+		writeFileSync(join(tmpdir(), "test.txt"), "hello world");
 
 		failSymlink = true;
 		await createSymlink({
-			sourcePath: "/tmp/test.txt",
-			finalPath: "/tmp/test-symlink.txt",
+			sourcePath: join(tmpdir(), "test.txt"),
+			finalPath: join(tmpdir(), "test-symlink.txt"),
 		});
 
-		const stats = await lstat("/tmp/test-symlink.txt");
+		const stats = await lstat(join(tmpdir(), "test-symlink.txt"));
 		expect(stats.isSymbolicLink()).toBe(false);
 
 		// Test file content
-		const content = readFileSync("/tmp/test-symlink.txt", "utf8");
+		const content = readFileSync(join(tmpdir(), "test-symlink.txt"), "utf8");
 		expect(content).toBe("hello world");
 
 		// Cleanup
-		await rm("/tmp/test-symlink.txt");
-		await rm("/tmp/test.txt");
+		await rm(join(tmpdir(), "test-symlink.txt"));
+		await rm(join(tmpdir(), "test.txt"));
 	});
 });
