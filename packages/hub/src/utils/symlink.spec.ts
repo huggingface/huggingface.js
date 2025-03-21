531ea58299fcadb1226b1374f3cabf53a00e1c03
coyotte508 <coyotte508@protonmail.com>
2025-03-21 23:14:21 +0100

    add unit test for windows

--- packages/hub/src/utils/symlink.spec.ts
+++ packages/hub/src/utils/symlink.spec.ts
@@ -1,7 +1,23 @@
-import { describe, expect, it } from "vitest";
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { describe, expect, it, vi } from "vitest";
 import { createSymlink } from "./symlink";
 import { readFileSync, writeFileSync } from "node:fs";
 import { lstat, rm } from "node:fs/promises";
+
+let failSymlink = false;
+vi.mock("node:fs/promises", async (importOriginal) => ({
+	...(await importOriginal<typeof import("node:fs/promises")>()),
+	symlink: async (...args: any[]) => {
+		if (failSymlink) {
+			failSymlink = false;
+			throw new Error("Symlink not supported");
+		}
+
+		// @ts-expect-error - ignore
+		return (await importOriginal<typeof import("node:fs/promises")>()).symlink(...args);
+	},
+}));
 
 describe("utils/symlink", () => {
 	it("should create a symlink", async () => {
@@ -46,5 +62,26 @@ describe("utils/symlink", () => {
 		await rm("/tmp/test-symlink.txt");
 		await rm("/tmp/test.txt");
 		await rm("/tmp/test2.txt");
+	});
+
+	it("should work when symlink doesn't work (windows)", async () => {
+		writeFileSync("/tmp/test.txt", "hello world");
+
+		failSymlink = true;
+		await createSymlink({
+			sourcePath: "/tmp/test.txt",
+			finalPath: "/tmp/test-symlink.txt",
+		});
+
+		const stats = await lstat("/tmp/test-symlink.txt");
+		expect(stats.isSymbolicLink()).toBe(false);
+
+		// Test file content
+		const content = readFileSync("/tmp/test-symlink.txt", "utf8");
+		expect(content).toBe("hello world");
+
+		// Cleanup
+		await rm("/tmp/test-symlink.txt");
+		await rm("/tmp/test.txt");
 	});
 });
