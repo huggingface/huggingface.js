b5bb4f4529eb934b054c2189528b9da794d3c7a0
célina <hanouticelina@gmail.com>
2025-04-24 09:53:04 +0200

    [Inference] Remove inference vcr tests (#1375)
    same as https://github.com/huggingface/huggingface_hub/pull/3021. 
    I’ve just skipped the tests for now and removed VCR related code so that
    we can quickly re-enable or adapt them when we introduce proper
    inference tests, as mentioned by @Wauplin.

--- packages/inference/test/InferenceClient.spec.ts
+++ packages/inference/test/InferenceClient.spec.ts
@@ -14,7 +14,6 @@ import {
 import { isUrl } from "../src/lib/isUrl";
 import { HARDCODED_MODEL_INFERENCE_MAPPING } from "../src/providers/consts";
 import { readTestFile } from "./test-files";
-import "./vcr";
 
 const TIMEOUT = 60000 * 3;
 const env = import.meta.env;
@@ -23,7 +22,7 @@ if (!env.HF_TOKEN) {
 	console.warn("Set HF_TOKEN in the env to run the tests for better rate limits");
 }
 
-describe.concurrent("InferenceClient", () => {
+describe.skip("InferenceClient", () => {
 	// Individual tests can be ran without providing an api key, however running all tests without an api key will result in rate limiting error.
 
 	describe("backward compatibility", () => {
