b175cf498f0b8f95a938d073b3c0a92482539f43
coyotte508 <coyotte508@protonmail.com>
2025-04-30 18:55:27 +0200

    add delete-branch

---
+++ packages/hub/src/lib/delete-branch.ts
@@ -0,0 +1,30 @@
+import { HUB_URL } from "../consts";
+import { createApiError } from "../error";
+import type { AccessToken, RepoDesignation } from "../types/public";
+
+export async function deleteBranch(params: {
+	repo: RepoDesignation;
+	/**
+	 * The name of the branch to delete
+	 */
+	branch: string;
+	hubUrl?: string;
+	accessToken?: AccessToken;
+	fetch?: typeof fetch;
+}): Promise<void> {
+	const res = await (params.fetch ?? fetch)(
+		`${params.hubUrl ?? HUB_URL}/api/repos/${params.repo}/branch/${encodeURIComponent(params.branch)}`,
+		{
+			method: "DELETE",
+			headers: {
+				...(params.accessToken && {
+					Authorization: `Bearer ${params.accessToken}`,
+				}),
+			},
+		}
+	);
+
+	if (!res.ok) {
+		throw await createApiError(res);
+	}
+}
