bb63f45033fe4cc3206d72eb62b1e5d3920b03e8
Eliott C. <coyotte508@gmail.com>
2025-05-01 14:50:20 +0200

    use smaller model instead for e2es (#1409)
    https://github.com/huggingface/huggingface.js/pull/1407#discussion_r2068601267

--- e2e/svelte/src/routes/+page.svelte
+++ e2e/svelte/src/routes/+page.svelte
@@ -13,7 +13,7 @@
 		}
 
 		const result = await hf.chatCompletion({
-			model: "microsoft/Phi-3-mini-4k-instruct",
+			model: "meta-llama/Llama-3.2-1B-Instruct",
 			messages: [{ role: "user", content: "Can you summarize the Eiffel Tower?" }],
 			parameters: {
 				max_length: 100,
