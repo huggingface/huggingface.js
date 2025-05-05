88989d21efbe7f85c05aa506eaa57c5054469a44
Pedro Cuenca <pedro@huggingface.co>
2025-05-05 18:16:40 +0200

    MLX snippet updates (#1397)
    h/t @gary149

--- packages/tasks/src/model-libraries-snippets.ts
+++ packages/tasks/src/model-libraries-snippets.ts
@@ -1309,11 +1309,31 @@ model = SwarmFormerModel.from_pretrained("${model.id}")
 `,
 ];
 
-export const mlx = (model: ModelData): string[] => [
+const mlx_unknown = (model: ModelData): string[] => [
 	`pip install huggingface_hub hf_transfer
 
 export HF_HUB_ENABLE_HF_TRANSFER=1
 huggingface-cli download --local-dir ${nameWithoutNamespace(model.id)} ${model.id}`,
+];
+
+const mlxlm = (model: ModelData): string[] => [
+	`pip install --upgrade mlx-lm
+
+mlx_lm.generate --model ${model.id} --prompt "Hello"`,
+];
+
+const mlxchat = (model: ModelData): string[] => [
+	`pip install --upgrade mlx-lm
+
+mlx_lm.chat --model ${model.id}`,
+];
+
+const mlxvlm = (model: ModelData): string[] => [
+	`pip install --upgrade mlx-vlm
+
+mlx_vlm.generate --model ${model.id} \\
+	--prompt "Describe this image." \\
+	--image "https://huggingface.co/datasets/huggingface/documentation-images/resolve/0052a70beed5bf71b92610a43a52df6d286cd5f3/diffusers/rabbit.jpg"`,
 ];
 
 export const mlxim = (model: ModelData): string[] => [
@@ -1321,6 +1341,20 @@ export const mlxim = (model: ModelData): string[] => [
 
 model = create_model(${model.id})`,
 ];
+
+export const mlx = (model: ModelData): string[] => {
+	if (model.tags.includes("image-text-to-text")) {
+		return mlxvlm(model);
+	}
+	if (model.tags.includes("conversational")) {
+		if (model.config?.tokenizer_config?.chat_template) {
+			return mlxchat(model);
+		} else {
+			return mlxlm(model);
+		}
+	}
+	return mlx_unknown(model);
+};
 
 export const model2vec = (model: ModelData): string[] => [
 	`from model2vec import StaticModel
