702e63d7f49088454125bf5bd8e8940e84ecdebc
Celina Hanouti <hanouticelina@gmail.com>
2025-05-07 16:52:43 +0200

    fix payload build for image-to-image

--- packages/inference/src/tasks/cv/imageToImage.ts
+++ packages/inference/src/tasks/cv/imageToImage.ts
@@ -14,24 +14,31 @@ export type ImageToImageArgs = BaseArgs & ImageToImageInput;
 export async function imageToImage(args: ImageToImageArgs, options?: Options): Promise<Blob> {
 	const provider = await resolveProvider(args.provider, args.model, args.endpointUrl);
 	const providerHelper = getProviderHelper(provider, "image-to-image");
-	let reqArgs: RequestArgs;
-	if (!args.parameters) {
-		reqArgs = {
-			accessToken: args.accessToken,
-			model: args.model,
-			data: args.inputs,
-		};
-	} else {
-		reqArgs = {
-			...args,
-			inputs: base64FromBytes(
-				new Uint8Array(args.inputs instanceof ArrayBuffer ? args.inputs : await args.inputs.arrayBuffer())
-			),
-		};
-	}
-	const { data: res } = await innerRequest<Blob>(reqArgs, providerHelper, {
+	const payload = await buildPayload(args);
+	const { data: res } = await innerRequest<Blob>(payload, providerHelper, {
 		...options,
 		task: "image-to-image",
 	});
 	return providerHelper.getResponse(res);
+}
+
+async function buildPayload(args: ImageToImageArgs): Promise<RequestArgs> {
+	if (args.provider === "hf-inference") {
+		if (!args.parameters) {
+			return {
+				accessToken: args.accessToken,
+				model: args.model,
+				data: args.inputs,
+			};
+		} else {
+			return {
+				...args,
+				inputs: base64FromBytes(
+					new Uint8Array(args.inputs instanceof ArrayBuffer ? args.inputs : await args.inputs.arrayBuffer())
+				),
+			};
+		}
+	} else {
+		return args;
+	}
 }
