4e592abedb74a8988402dfc403614f2d2ad69794
Julien Chaumond <julien@huggingface.co>
2025-05-20 14:14:24 +0200

    hook main loop from mcp-client/cli.ts

--- packages/tiny-agents/src/cli.ts
+++ packages/tiny-agents/src/cli.ts
@@ -7,7 +7,8 @@ import { PROVIDERS_OR_POLICIES } from "@huggingface/inference";
 import { Agent } from "@huggingface/mcp-client";
 import { version as packageVersion } from "../package.json";
 import { ServerConfigSchema } from "./lib/types";
-import { ANSI, debug, error } from "./lib/utils";
+import { debug, error } from "./lib/utils";
+import { mainCliLoop } from "./lib/mainCliLoop";
 
 const USAGE_HELP = `
 Usage:
@@ -122,9 +123,9 @@ async function main() {
 		error(`Serve is not implemented yet, coming soon!`);
 		process.exit(1);
 	} else {
-		console.debug(agent);
-
-		// TODO: hook main loop from mcp-client/cli.ts
+		debug(agent);
+		// main loop from mcp-client/cli.ts
+		await mainCliLoop(agent);
 	}
 }
 
