huggingface/huggingface.js

commit dc3b086e004bc88a861c0945a115da553b370721

add vui code snippet (#1522)
Waiting for this PR to be merged: 
https://huggingface.co/fluxions/vui/discussions/1/files

---------

Co-authored-by: Lucain <lucain@huggingface.co>
Co-authored-by: vb <vaibhavs10@gmail.com>

packages/tasks/src/model-libraries-snippets.ts
@@ -1354,6 +1354,24 @@ export const voicecraft = (model: ModelData): string[] => [
 model = VoiceCraft.from_pretrained("${model.id}")`,
 ];
 
+export const vui = (model: ModelData): string[] => [
+	`# !pip install git+https://github.com/fluxions-ai/vui
+
+import torchaudio
+
+from vui.inference import render
+from vui.model import Vui,
+
+model = Vui.from_pretrained().cuda()
+waveform = render(
+    model,
+    "Hey, here is some random stuff, usually something quite long as the shorter the text the less likely the model can cope!",
+)
+print(waveform.shape)
+torchaudio.save("out.opus", waveform[0], 22050)
+`,
+];
+
 export const chattts = (): string[] => [
 	`import ChatTTS
 import torchaudio

packages/tasks/src/model-libraries.ts
@@ -1063,6 +1063,13 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		docsUrl: "https://github.com/jasonppy/VoiceCraft",
 		snippets: snippets.voicecraft,
 	},
+	vui: {
+		prettyLabel: "Vui",
+		repoName: "Vui",
+		repoUrl: "https://github.com/vui-ai/vui",
+		countDownloads: `path_extension:"pt"`,
+		snippets: snippets.vui,
+	},
 	wham: {
 		prettyLabel: "WHAM",
 		repoName: "wham",

commit d4717a1085548f5b3d63c66d0a03127268bfe03f

Fix lint in CI (#1538)
Lint error introduced in
https://github.com/huggingface/huggingface.js/pull/1522. Will merge as
soon as lint CI is green

packages/tasks/src/model-libraries-snippets.ts
@@ -1354,7 +1354,7 @@ export const voicecraft = (model: ModelData): string[] => [
 model = VoiceCraft.from_pretrained("${model.id}")`,
 ];
 
-export const vui = (model: ModelData): string[] => [
+export const vui = (): string[] => [
 	`# !pip install git+https://github.com/fluxions-ai/vui
 
 import torchaudio