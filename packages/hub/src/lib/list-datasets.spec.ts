7018111880f97ea5cfe4421b461e08766280a299
quanghuynt14 <quanghuynt14@gmail.com>
2025-06-29 21:47:54 +0200

    fix test

--- packages/hub/src/lib/list-datasets.spec.ts
+++ packages/hub/src/lib/list-datasets.spec.ts
@@ -7,7 +7,7 @@ describe("listDatasets", () => {
 		const results: DatasetEntry[] = [];
 
 		for await (const entry of listDatasets({ search: { owner: "hf-doc-build" } })) {
-			if (entry.name === "hf-doc-build/doc-build-dev-test") {
+			if (entry.name !== "hf-doc-build/doc-build-dev-test") {
 				continue;
 			}
 			if (typeof entry.downloads === "number") {
@@ -23,25 +23,16 @@ describe("listDatasets", () => {
 			results.push(entry);
 		}
 
-		expect(results).toEqual(expect.arrayContaining([
+		expect(results).deep.equal([
 			{
-				id: "6356b19985da6f13863228bd",
-				name: "hf-doc-build/doc-build",
+				id: "659086ddbea632dc9491adaf",
+				name: "hf-doc-build/doc-build-dev-test",
 				private: false,
 				gated: false,
 				downloads: 0,
 				likes: 0,
 				updatedAt: new Date(0),
 			},
-			{
-				id: "636a1b69f2f9ec4289c4c19e",
-				name: "hf-doc-build/doc-build-dev",
-				gated: false,
-				private: false,
-				downloads: 0,
-				likes: 0,
-				updatedAt: new Date(0),
-			},
-		]));
+		]);
 	});
 });
