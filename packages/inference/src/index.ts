ed65ff8ccb9026c8cfa779f148d2e9a86094c5f6
SBrandeis <simon@huggingface.co>
2025-06-30 12:00:36 +0200

    comments

--- packages/inference/src/index.ts
+++ packages/inference/src/index.ts
@@ -3,6 +3,7 @@ export * from "./errors.js";
 export * from "./types.js";
 export * from "./tasks/index.js";
 import * as snippets from "./snippets/index.js";
-export * from "./lib/makeRequestOptions.js"
+export * from "./lib/getProviderHelper.js";
+export * from "./lib/makeRequestOptions.js";
 
 export { snippets };
