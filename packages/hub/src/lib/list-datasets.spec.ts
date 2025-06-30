27c14a1608b6b0b20ecc3a66e749115ea38826f4
quanghuynt14 <quanghuynt14@gmail.com>
2025-06-30 13:50:40 +0200

    fix test

--- packages/hub/src/lib/list-datasets.spec.ts
+++ packages/hub/src/lib/list-datasets.spec.ts
@@ -7,7 +7,7 @@ describe("listDatasets", () => {
 		const results: DatasetEntry[] = [];
 
 		for await (const entry of listDatasets({ search: { owner: "hf-doc-build" } })) {
-			if (entry.name !== "hf-doc-build/doc-build-dev-test") {
+			if (entry.name !== "hf-doc-build/doc-build" && entry.name !== "hf-doc-build/doc-build-dev") {
 				continue;
 			}
 			if (typeof entry.downloads === "number") {
@@ -23,12 +23,21 @@ describe("listDatasets", () => {
 			results.push(entry);
 		}
 
-		expect(results).deep.equal([
+		expect(results.sort((a, b) => a.id.localeCompare(b.id))).to.deep.equal([
 			{
-				id: "659086ddbea632dc9491adaf",
-				name: "hf-doc-build/doc-build-dev-test",
+				id: "6356b19985da6f13863228bd",
+				name: "hf-doc-build/doc-build",
 				private: false,
 				gated: false,
+				downloads: 0,
+				likes: 0,
+				updatedAt: new Date(0),
+			},
+			{
+				id: "636a1b69f2f9ec4289c4c19e",
+				name: "hf-doc-build/doc-build-dev",
+				gated: false,
+				private: false,
 				downloads: 0,
 				likes: 0,
 				updatedAt: new Date(0),
