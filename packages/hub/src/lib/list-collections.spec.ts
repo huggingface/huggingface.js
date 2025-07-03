0605243bf1c737d1a2141735b23ba9edef12f04c
quanghuynt14 <quanghuynt14@gmail.com>
2025-07-04 00:28:47 +0200

    use hub-ci.hf.co for test

--- packages/hub/src/lib/list-collections.spec.ts
+++ packages/hub/src/lib/list-collections.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { listCollections } from "./list-collections";
 import type { ApiCollectionInfo } from "../types/api/api-collection";
+import { TEST_HUB_URL } from "../test/consts";
 
 describe("listCollections", () => {
 	it("should list collections", async () => {
@@ -8,48 +9,23 @@ describe("listCollections", () => {
 
 		for await (const entry of listCollections({
 			search: { owner: "quanghuynt14" },
+			hubUrl: TEST_HUB_URL,
 		})) {
-			if (entry.slug !== "quanghuynt14/test-6862f3b263a5cc509b9ee24a") {
+			if (entry.slug !== "quanghuynt14/test-collection-6866ff686ca2d2e0a1931507") {
 				continue;
 			}
 
 			if (typeof entry.lastUpdated === "string") {
-				entry.lastUpdated = "2025-07-21T00:36:29.000Z";
-			}
-
-			if (typeof entry.upvotes === "number") {
-				entry.upvotes = 0;
-			}
-
-			if (typeof entry.owner.avatarUrl === "string") {
-				entry.owner.avatarUrl = "https://cdn-avatars.huggingface.co/user-avatar.jpeg";
+				entry.lastUpdated = "2025-07-03T22:18:56.239Z";
 			}
 
 			if (entry.items && Array.isArray(entry.items)) {
 				entry.items.map((item) => {
-					if ("owner" in item && typeof item.owner.avatarUrl === "string") {
-						item.owner.avatarUrl = "https://cdn-avatars.huggingface.co/user-avatar.jpeg";
-					}
-					if ("authorData" in item && typeof item.authorData.avatarUrl === "string") {
-						item.authorData.avatarUrl = "https://cdn-avatars.huggingface.co/user-avatar.jpeg";
-					}
-					if ("authorData" in item && typeof item.authorData.followerCount === "number") {
-						item.authorData.followerCount = 0;
-					}
-					if ("downloads" in item && typeof item.downloads === "number") {
-						item.downloads = 0;
-					}
-					if ("likes" in item && typeof item.likes === "number") {
-						item.likes = 0;
-					}
 					if ("lastModified" in item && typeof item.lastModified === "string") {
 						item.lastModified = "2025-07-01T00:36:29.000Z";
 					}
 					if ("lastUpdated" in item && typeof item.lastUpdated === "string") {
 						item.lastUpdated = "2025-07-01T00:41:27.525Z";
-					}
-					if ("upvotes" in item && typeof item.upvotes === "number") {
-						item.upvotes = 0;
 					}
 				});
 			}
@@ -63,15 +39,15 @@ describe("listCollections", () => {
 
 		// Check all properties of the collection except items
 		expect(collection).deep.equal({
-			slug: "quanghuynt14/test-6862f3b263a5cc509b9ee24a",
-			title: "Test",
-			description: "this collection is only for test",
+			slug: "quanghuynt14/test-collection-6866ff686ca2d2e0a1931507",
+			title: "Test Collection",
+			description: "This collection is only for test",
 			gating: false,
-			lastUpdated: "2025-07-21T00:36:29.000Z",
+			lastUpdated: "2025-07-03T22:18:56.239Z",
 			owner: {
-				_id: "6861a8be64cdb219ce188401",
-				avatarUrl: "https://cdn-avatars.huggingface.co/user-avatar.jpeg",
-				fullname: "Huy",
+				_id: "6866ff3936a7677f427f99e3",
+				avatarUrl: "/avatars/b51088e22fb7194888551365b1bafada.svg",
+				fullname: "Quang-Huy Tran",
 				name: "quanghuynt14",
 				type: "user",
 				isPro: false,
@@ -80,7 +56,7 @@ describe("listCollections", () => {
 				isMod: false,
 			},
 			items: [],
-			theme: "pink",
+			theme: "purple",
 			private: false,
 			upvotes: 0,
 			isUpvotedByUser: false,
@@ -88,14 +64,14 @@ describe("listCollections", () => {
 
 		// Check for item type model
 		expect(items[0]).deep.equal({
-			_id: "68632ede07867696ac5d7ab1",
+			_id: "686700086ca2d2e0a193150b",
 			position: 0,
 			type: "model",
 			author: "quanghuynt14",
 			authorData: {
-				_id: "6861a8be64cdb219ce188401",
-				avatarUrl: "https://cdn-avatars.huggingface.co/user-avatar.jpeg",
-				fullname: "Huy",
+				_id: "6866ff3936a7677f427f99e3",
+				avatarUrl: "/avatars/b51088e22fb7194888551365b1bafada.svg",
+				fullname: "Quang-Huy Tran",
 				name: "quanghuynt14",
 				type: "user",
 				isPro: false,
@@ -116,7 +92,7 @@ describe("listCollections", () => {
 
 		// Check for item type dataset
 		expect(items[1]).deep.equal({
-			_id: "68632ee9c76d34d6031bab15",
+			_id: "686701cd86ea6972ba6c9da5",
 			position: 1,
 			type: "dataset",
 			author: "quanghuynt14",
@@ -132,16 +108,14 @@ describe("listCollections", () => {
 
 		// Check for item type space
 		expect(items[2]).deep.equal({
-			_id: "68632eeff8d2d420b6ad4b0d",
+			_id: "6867000f6ca2d2e0a193150e",
 			position: 2,
 			type: "space",
-			ai_category: "Chatbots",
-			ai_short_description: "Chat with an advanced virtual assistant",
 			author: "quanghuynt14",
 			authorData: {
-				_id: "6861a8be64cdb219ce188401",
-				avatarUrl: "https://cdn-avatars.huggingface.co/user-avatar.jpeg",
-				fullname: "Huy",
+				_id: "6866ff3936a7677f427f99e3",
+				avatarUrl: "/avatars/b51088e22fb7194888551365b1bafada.svg",
+				fullname: "Quang-Huy Tran",
 				name: "quanghuynt14",
 				type: "user",
 				isPro: false,
@@ -149,60 +123,52 @@ describe("listCollections", () => {
 				isHfAdmin: false,
 				isMod: false,
 			},
-			colorFrom: "yellow",
-			colorTo: "purple",
-			createdAt: "2025-07-01T00:39:07.000Z",
-			emoji: "💬",
+			colorFrom: "pink",
+			colorTo: "indigo",
+			createdAt: "2025-07-03T22:10:39.000Z",
+			emoji: "🏆",
 			id: "quanghuynt14/TestSpace",
 			lastModified: "2025-07-01T00:36:29.000Z",
 			likes: 0,
 			pinned: false,
 			private: false,
-			sdk: "gradio",
+			sdk: "docker",
 			repoType: "space",
 			runtime: {
-				stage: "RUNNING",
+				stage: "BUILDING",
 				hardware: {
-					current: "cpu-basic",
+					current: null,
 					requested: "cpu-basic",
 				},
 				storage: null,
 				gcTimeout: 172800,
 				replicas: {
-					current: 1,
+					current: 0,
 					requested: 1,
 				},
-				devMode: false,
-				domains: [
-					{
-						domain: "quanghuynt14-testspace.hf.space",
-						stage: "READY",
-					},
-				],
-				sha: "dc1332681707e8c7cfda491bbaf17a9aba5ff6d9",
 			},
-			shortDescription: "This is the space for test",
+			shortDescription: "This space is only for test",
 			title: "TestSpace",
 			isLikedByUser: false,
 			trendingScore: 0,
-			tags: ["gradio", "region:us"],
+			tags: ["docker", "region:us"],
 		});
 
 		// Check for item type collection
 		expect(items[3]).deep.equal({
-			_id: "68632ef6b13e0dc3b640854a",
+			_id: "68670014f25517a0a7eaf505",
 			position: 3,
 			type: "collection",
-			id: "68632e8a911ddf966de476bc",
-			slug: "quanghuynt14/another-test-collection-68632e8a911ddf966de476bc",
-			title: "Another test collection",
+			id: "6866ff686ca2d2e0a1931507",
+			slug: "quanghuynt14/test-collection-6866ff686ca2d2e0a1931507",
+			title: "Test Collection",
 			description: "This collection is only for test",
 			lastUpdated: "2025-07-01T00:41:27.525Z",
-			numberItems: 1,
+			numberItems: 5,
 			owner: {
-				_id: "6861a8be64cdb219ce188401",
-				avatarUrl: "https://cdn-avatars.huggingface.co/user-avatar.jpeg",
-				fullname: "Huy",
+				_id: "6866ff3936a7677f427f99e3",
+				avatarUrl: "/avatars/b51088e22fb7194888551365b1bafada.svg",
+				fullname: "Quang-Huy Tran",
 				name: "quanghuynt14",
 				type: "user",
 				isPro: false,
@@ -210,8 +176,8 @@ describe("listCollections", () => {
 				isHfAdmin: false,
 				isMod: false,
 			},
-			theme: "blue",
-			shareUrl: "https://huggingface.co/collections/quanghuynt14/another-test-collection-68632e8a911ddf966de476bc",
+			theme: "purple",
+			shareUrl: "https://hub-ci.huggingface.co/collections/quanghuynt14/test-collection-6866ff686ca2d2e0a1931507",
 			upvotes: 0,
 			isUpvotedByUser: false,
 		});
