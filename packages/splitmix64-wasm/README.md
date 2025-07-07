fb8e5aac98e95ef7f9a15d77887a17719b99d3a1
coyotte508 <coyotte508@protonmail.com>
2025-07-07 16:34:23 +0200

    add splitmix64-wasm module

---
+++ packages/splitmix64-wasm/README.md
@@ -0,0 +1,16 @@
+JS and WASM implementations of splitmix-64
+
+Using [AssemblyScript](https://www.assemblyscript.org/) to generate a lean WASM.
+
+Used internally to reproduce rust tests
+
+Let us know if you want us to expose more functions.
+
+## Usage
+
+```javascript
+import { createRandomArray } from '@huggingface/splitmix64-wasm';
+
+// Create an ArrayBuffer of data, with u64s converted to le u8s
+const data = new createRandomArray(256_000, 1); // Example: 256kB of data
+```
--- packages/xetchunk-wasm/tests/index.js
+++ packages/xetchunk-wasm/tests/index.js
@@ -1,17 +1,37 @@
-import { createChunker, finalize, nextBlock } from "../build/debug.js";
+import { createChunker, finalize, nextBlock, generateRandomArray } from "../build/debug.js";
+import assert from "assert";
 
-const chunker = createChunker(Math.pow(2, 12));
+const data1 = generateRandomArray(100_000, 0);
 
-const data = new Uint8Array(100_000);
+// log first 8 bytes
+console.log(data1.slice(0, 8));
 
-for (let i = 0; i < data.length; i++) {
-	data[i] = i;
+// Do same with nextUint64 instead of nextUint8
+const prng64 = splitmix64([0, 0]);
+
+for (let i = 0; i < 8; i++) {
+	const value = nextUint64(prng64);
+	console.log(value);
+
+	// Log all 8 bytes
+	console.log(value[0].toString(16));
+	console.log(value[1].toString(16));
 }
 
-const chunks = nextBlock(chunker, data);
+assert.strictEqual(data1[0], 175);
 
-console.log("chunks", chunks);
+// const chunker = createChunker(Math.pow(2, 12));
 
-const lastChunk = finalize(chunker);
+// const data = new Uint8Array(100_000);
 
-console.log("lastChunk", lastChunk);
+// for (let i = 0; i < data.length; i++) {
+// 	data[i] = Math.floor(Math.random() * 256);
+// }
+
+// const chunks = nextBlock(chunker, data);
+
+// console.log("chunks", chunks);
+
+// const lastChunk = finalize(chunker);
+
+// console.log("lastChunk", lastChunk);
