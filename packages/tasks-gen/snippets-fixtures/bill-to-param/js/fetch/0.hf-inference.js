e2f6c6faec2f16c7acd1f652e2412bc7e34b6212
Wauplin <lucainp@gmail.com>
2025-07-17 13:49:59 +0200

    Add JS fetch conversational snippets

---
+++ packages/tasks-gen/snippets-fixtures/bill-to-param/js/fetch/0.hf-inference.js
@@ -0,0 +1,28 @@
+async function query(data) {
+	const response = await fetch(
+		"https://router.huggingface.co/v1/chat/completions",
+		{
+			headers: {
+				Authorization: `Bearer ${process.env.HF_TOKEN}`,
+				"Content-Type": "application/json",
+				"X-HF-Bill-To": "huggingface",
+			},
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+query({ 
+    messages: [
+        {
+            role: "user",
+            content: "What is the capital of France?",
+        },
+    ],
+    model: "meta-llama/Llama-3.1-8B-Instruct:hf-inference",
+}).then((response) => {
+    console.log(JSON.stringify(response));
+});
