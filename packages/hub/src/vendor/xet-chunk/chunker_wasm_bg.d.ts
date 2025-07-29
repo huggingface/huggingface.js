c093c645819be0c1ce69c77be6d5f53b6185c384
coyotte508 <coyotte508@protonmail.com>
2025-07-29 13:10:58 +0200

    dedup boolean when loading chunks from wasm

--- packages/hub/src/vendor/xet-chunk/chunker_wasm_bg.d.ts
+++ packages/hub/src/vendor/xet-chunk/chunker_wasm_bg.d.ts
@@ -6,6 +6,6 @@ export function compute_file_hash(chunks_array: Array<{ hash: string; length: nu
 export class Chunker {
 	free(): void;
 	constructor(target_chunk_size: number);
-	add_data(data: Uint8Array): Array<{ hash: string; length: number }>;
-	finish(): Array<{ hash: string; length: number }>;
+	add_data(data: Uint8Array): Array<{ hash: string; length: number; dedup: boolean }>;
+	finish(): Array<{ hash: string; length: number; dedup: boolean }>;
 }
