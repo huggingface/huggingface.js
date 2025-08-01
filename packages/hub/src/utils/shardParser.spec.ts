072d889353bc4858655e7600144275e9da74c12e
coyotte508 <coyotte508@protonmail.com>
2025-08-01 21:19:06 +0200

    shard parser works

--- packages/hub/src/utils/shardParser.spec.ts
+++ packages/hub/src/utils/shardParser.spec.ts
@@ -5,7 +5,8 @@ import { init, compute_hmac } from "../vendor/xet-chunk/chunker_wasm";
 
 describe("shardParser", () => {
 	it("should parse a shard", async () => {
-		const shard = await parseShardData(new Blob([await readFile("tests/gpt2-64-8bits.tflite.shard")]));
+		const buffer = await readFile("tests/gpt2-64-8bits.tflite.shard");
+		const shard = await parseShardData(new Blob([buffer]));
 		const expectedJson = JSON.parse(await readFile("tests/gpt2-64-8bits.tflite.shard.json", "utf-8"));
 
 		expect(shard.hmacKey).toBe(expectedJson.hmac_key);
@@ -21,12 +22,6 @@ describe("shardParser", () => {
 		await init();
 
 		const chunkHash = "9502eec19d4b0c9f7b389228fa801f68ecdf15d69ccd1da2f9ddbd0219898335";
-		const xorb = shard.xorbs.find((xorb) =>
-			xorb.chunks.some((chunk) => compute_hmac(chunk.hash, shard.hmacKey) === chunkHash)
-		);
-		expect(xorb).toBeDefined();
-		expect(xorb?.chunks.length).toBe(1);
-		expect(xorb?.chunks[0].startOffset).toBe(0);
-		expect(xorb?.chunks[0].unpackedLength).toBe(1024);
+		expect(compute_hmac(chunkHash, shard.hmacKey)).toEqual(shard.xorbs[1].chunks[0].hash);
 	});
 });
