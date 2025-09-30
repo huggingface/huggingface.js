7450ebedf705517b474a0840185d2fd0861c4a13
AlexKer <AlexKer@users.noreply.github.com>
2025-09-30 14:36:20 -0400

    restored .skip and added space

--- packages/inference/test/InferenceClient.spec.ts
+++ packages/inference/test/InferenceClient.spec.ts
@@ -22,7 +22,7 @@ if (!env.HF_TOKEN) {
 	console.warn("Set HF_TOKEN in the env to run the tests for better rate limits");
 }
 
-describe("InferenceClient", () => {
+describe.skip("InferenceClient", () => {
 	// Individual tests can be ran without providing an api key, however running all tests without an api key will result in rate limiting error.
 
 	describe("backward compatibility", () => {
