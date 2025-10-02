82b4d4af4bc78a655f486ef200e1d7db9c9995a0
machineuser <infra+machineuser@huggingface.co>
2025-10-02 08:29:03 +0000

    🔖 @huggingface/inference 4.10.0

--- packages/inference/src/package.ts
+++ packages/inference/src/package.ts
@@ -1,3 +1,3 @@
 // Generated file from package.json. Issues importing JSON directly when publishing on commonjs/ESM - see https://github.com/microsoft/TypeScript/issues/51783
-export const PACKAGE_VERSION = "4.9.0";
+export const PACKAGE_VERSION = "4.10.0";
 export const PACKAGE_NAME = "@huggingface/inference";
