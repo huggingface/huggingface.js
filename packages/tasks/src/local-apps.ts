4ac4bfa7c783285f88715de9c7f00310f8b5be37
rshemet <42696921+rshemet@users.noreply.github.com>
2025-10-03 15:26:15 +0100

    Update local-apps.ts to include Cactus

--- packages/tasks/src/local-apps.ts
+++ packages/tasks/src/local-apps.ts
@@ -94,6 +94,10 @@ function isAmdRyzenModel(model: ModelData) {
 	return model.tags.includes("ryzenai-hybrid") || model.tags.includes("ryzenai-npu");
 }
 
+function isCactusModel(model: ModelData) {
+	return model.tags.includes("cactus");
+}
+
 function isMlxModel(model: ModelData) {
 	return model.tags.includes("mlx");
 }
@@ -361,6 +365,21 @@ const snippetLemonade = (model: ModelData, filepath?: string): LocalAppSnippet[]
 	];
 };
 
+const snippetCactus = (model: ModelData, filepath?: string): LocalAppSnippet[] => {
+	return [
+		{
+			title: "Install Cactus for Flutter",
+			setup: "flutter pub get cactus",
+			content: "Follow the documentation",
+		},
+		{
+			title: "Install Cactus for React Native",
+			setup: "npm install cactus-react-native",
+			content: "Follow the documentation",
+		},
+	];
+};
+
 /**
  * Add your new local app here.
  *
@@ -544,6 +563,13 @@ export const LOCAL_APPS = {
 		mainTask: "text-generation",
 		displayOnModelPage: (model) => isLlamaCppGgufModel(model) || isAmdRyzenModel(model),
 		snippet: snippetLemonade,
+	},
+	cactus: {
+		prettyLabel: "Cactus",
+		docsUrl: "https://cactuscompute.com/docs",
+		mainTask: "text-generation",
+		displayOnModelPage: isCactusModel,
+		snippet: snippetCactus
 	},
 } satisfies Record<string, LocalApp>;
 
