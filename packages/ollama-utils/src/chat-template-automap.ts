1eadb8568475e0dd28075f786d8697442d27f1d1
machineuser <infra+machineuser@huggingface.co>
2025-12-01 07:10:17 +0000

    ollama update 2025-12-01

--- packages/ollama-utils/src/chat-template-automap.ts
+++ packages/ollama-utils/src/chat-template-automap.ts
@@ -5,31 +5,30 @@ import type { OllamaChatTemplateMapEntry } from "./types";
 
 /**
  * Skipped these models due to error:
- * - library/gpt-oss:20b
- * - library/llama3.1:8b
- * - library/llama3.2:latest
- * - library/gemma3:latest
- * - library/qwen2:0.5b
- * - library/tinyllama:1.1b
- * - library/qwq:latest
- * - library/codegemma:2b
- * - library/cogito:latest
- * - library/cogito:3b
- * - library/smollm:latest
- * - library/devstral:24b
- * - library/dolphin-llama3:8b
- * - library/opencoder:latest
- * - library/codegeex4:9b
- * - library/starcoder:latest
- * - library/command-r-plus:latest
- * - library/command-r-plus:104b
- * - library/llama3-chatqa:8b
- * - library/tulu3:latest
- * - library/llama3-groq-tool-use:latest
- * - library/yarn-llama2:latest
- * - library/command-r7b:7b
- * - library/phi4-mini-reasoning:latest
- * - library/reader-lm:latest
+ * - library/gemma2:latest
+ * - library/qwen2.5-coder:0.5b
+ * - library/dolphin3:8b
+ * - library/olmo2:7b
+ * - library/dolphin3:latest
+ * - library/orca-mini:3b
+ * - library/mistral-small3.2:latest
+ * - library/deepseek-coder-v2:latest
+ * - library/qwen2.5vl:3b
+ * - library/gemma3n:latest
+ * - library/command-r:35b
+ * - library/reflection:latest
+ * - library/llama3-chatqa:latest
+ * - library/llama3-gradient:latest
+ * - library/llama3-gradient:8b
+ * - library/xwinlm:latest
+ * - library/granite3-moe:latest
+ * - library/smallthinker:latest
+ * - library/llama-guard3:1b
+ * - library/nemotron:70b
+ * - library/nemotron:latest
+ * - library/stablelm-zephyr:latest
+ * - library/falcon2:latest
+ * - library/marco-o1:latest
  */
 
 export const OLLAMA_CHAT_TEMPLATE_MAPPING: OllamaChatTemplateMapEntry[] = [
