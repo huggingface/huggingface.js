2c2de8989f91496da71ac8a68de3477566641633
apolinário <joaopaulo.passos@gmail.com>
2025-12-05 16:41:54 +0100

    Add `image-text-to-image` and `image-text-to-video` tasks (#1866)
    The goal of this new tasks is to support models that take in both image
    and text input and output either image or video.
    
    The goal of this PR is making the tasks as analogous to `image-to-image`
    and `image-to-video` as possible, with the only difference that the
    image input should now be **optional**, as an empty image and a valid
    prompt should still work for a model like FLUX.2 (supports both
    text-to-image and image-to-image tasks) or LTX Video (both text-to-video
    and image-to-video)
    
    Once this is in, I'll also have a widget PR in Moon to support this task
    in the model cards / widgets etc. and a follow up PR adding this to the
    inference providers, so that we can then PR repos to change the task for
    compatible models
    
    ---------
    
    Co-authored-by: Merve Noyan <merveenoyan@gmail.com>

--- packages/inference/src/lib/getProviderHelper.ts
+++ packages/inference/src/lib/getProviderHelper.ts
@@ -28,6 +28,8 @@ import type {
 	ImageToImageTaskHelper,
 	ImageToTextTaskHelper,
 	ImageToVideoTaskHelper,
+	ImageTextToImageTaskHelper,
+	ImageTextToVideoTaskHelper,
 	ObjectDetectionTaskHelper,
 	QuestionAnsweringTaskHelper,
 	SentenceSimilarityTaskHelper,
@@ -276,6 +278,14 @@ export function getProviderHelper(
 	provider: InferenceProviderOrPolicy,
 	task: "image-to-video"
 ): ImageToVideoTaskHelper & TaskProviderHelper;
+export function getProviderHelper(
+	provider: InferenceProviderOrPolicy,
+	task: "image-text-to-image"
+): ImageTextToImageTaskHelper & TaskProviderHelper;
+export function getProviderHelper(
+	provider: InferenceProviderOrPolicy,
+	task: "image-text-to-video"
+): ImageTextToVideoTaskHelper & TaskProviderHelper;
 export function getProviderHelper(
 	provider: InferenceProviderOrPolicy,
 	task: "sentence-similarity"
