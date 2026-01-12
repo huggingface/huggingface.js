6c522ba2ba5765c1c951a0c7b3ba4c7d93c8b2ca
Wauplin <lucainp@gmail.com>
2026-01-12 13:19:14 +0100

    fix list collections test

--- packages/hub/src/lib/list-collections.spec.ts
+++ packages/hub/src/lib/list-collections.spec.ts
@@ -35,6 +35,7 @@ describe("listCollections", () => {
 					isHfAdmin: false,
 					isMod: false,
 					isEnterprise: false,
+					isUserFollowing: false,
 				},
 				items: [
 					{
@@ -50,6 +51,7 @@ describe("listCollections", () => {
 						downloads: expect.any(Number),
 						gated: false,
 						id: "huggingfacejs/tasks",
+						isBenchmark: false,
 						isLikedByUser: false,
 						lastModified: expect.any(String),
 						likes: expect.any(Number),
@@ -74,6 +76,7 @@ describe("listCollections", () => {
 							isHf: false,
 							isHfAdmin: false,
 							isMod: false,
+							isUserFollowing: false,
 							name: "huggingfacejs",
 							type: "org",
 						},
@@ -99,6 +102,7 @@ describe("listCollections", () => {
 							isHf: false,
 							isHfAdmin: false,
 							isMod: false,
+							isUserFollowing: false,
 							name: "huggingfacejs",
 							type: "org",
 						},
