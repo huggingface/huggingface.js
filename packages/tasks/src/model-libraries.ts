bff81de65ac83e01a6c8c993a3fbfe0c73972100
Gabriele <s252805@studenti.polito.it>
2026-02-09 07:36:17 +0100

    Add VisMatch library for image matching models

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -1425,6 +1425,13 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		countDownloads: `path_extension:"pkl"`,
 		snippets: snippets.vfimamba,
 	},
+	vismatch: {
+		prettyLabel: "VisMatch",
+		repoName: "VisMatch",
+		repoUrl: "https://github.com/gmberton/vismatch",
+		filter: false,
+		countDownloads: `path:"vismatch.yaml"`,
+	},
 	lvface: {
 		prettyLabel: "LVFace",
 		repoName: "LVFace",
