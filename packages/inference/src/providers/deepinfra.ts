dd4654091d6c81c52d6fe6d0220213f243c9eedd
Thach Nguyen <Thachnh@users.noreply.github.com>
2026-02-19 11:12:51 -0800

    Apply suggestion from @hanouticelina
    Co-authored-by: célina <hanouticelina@gmail.com>

--- packages/inference/src/providers/deepinfra.ts
+++ packages/inference/src/providers/deepinfra.ts
@@ -1,3 +1,24 @@
+/**
+ * See the registered mapping of HF model ID => DeepInfra model ID here:
+ *
+ * https://huggingface.co/api/partners/deepinfra/models
+ *
+ * This is a publicly available mapping.
+ *
+ * If you want to try to run inference for a new model locally before
+ it's registered on huggingface.co,
+* you can add it to the dictionary "HARDCODED_MODEL_ID_MAPPING" in
+consts.ts, for dev purposes.
+*
+* - If you work at DeepInfra and want to update this mapping, please
+use the model mapping API we provide on huggingface.co
+* - If you're a community member and want to add a new supported HF
+model to DeepInfra, please open an issue on the present repo
+* and we will tag DeepInfra team members.
+*
+* Thanks!
+*/
+
 import type { TextGenerationOutput } from "@huggingface/tasks";
 import { InferenceClientProviderOutputError } from "../errors.js";
 import type { BodyParams } from "../types.js";
