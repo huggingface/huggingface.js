de0a1c3b8ff3dc3617fcc0f2d5b80ac57e11360e
DoctorD90 <DoctorD90@users.noreply.github.com>
2026-04-29 19:15:16 +0200

    ReFactoring CPU and GPU database (AMD CPU)
    Refactored the CPU AMD database. Introduced:
    - files: hardware-cpu-amd.ts
    - variables: CPU_AMD_SKUS

--- packages/tasks/src/hardware.ts
+++ packages/tasks/src/hardware.ts
@@ -3,6 +3,7 @@ import { GPU_NVIDIA_SKUS } from "./hardware-gpu-nvidia.js";
 import { GPU_INTEL_SKUS } from "./hardware-gpu-intel.js";
 import { GPU_QUALCOMM_SKUS } from "./hardware-gpu-qualcomm.js";
 import { CPU_INTEL_SKUS } from "./hardware-cpu-intel.js";
+import { CPU_AMD_SKUS } from "./hardware-cpu-amd.js";
 
 /**
  * Biden AI Executive Order (since revoked by President Trump):
@@ -50,92 +51,7 @@ export const SKUS = {
 	},
 	CPU: {
 		Intel: CPU_INTEL_SKUS,
-		AMD: {
-			"EPYC 5th Generation Zen 5 (Turin)": {
-				tflops: 13.8,
-			},
-			"EPYC 4th Generation Zen 4 (Genoa)": {
-				tflops: 5,
-			},
-			"EPYC 3th Generation Zen 3 (Milan)": {
-				tflops: 2.4,
-			},
-			"EPYC 2th Generation Zen 2 (Rome)": {
-				tflops: 0.6,
-			},
-			"EPYC 1st Generation Zen (Naples)": {
-				tflops: 0.6,
-			},
-			"Ryzen Threadripper Zen 5 9000 (Shimada Peak)": {
-				tflops: 14.0,
-			},
-			"Ryzen Threadripper Zen 4 7000 (Storm Peak)": {
-				tflops: 10.0,
-			},
-			"Ryzen Threadripper Zen 3 5000 (Chagall)": {
-				tflops: 4.6,
-			},
-			"Ryzen Threadripper Zen 2 3000 (Castle Peak)": {
-				tflops: 3.2,
-			},
-			"Ryzen Threadripper Zen 1000 (Whitehaven)": {
-				tflops: 0.6,
-			},
-			"Ryzen 7 3800X (16)": {
-				tflops: 1.73,
-			},
-			"Ryzen Zen 5 9000 (Ryzen 9)": {
-				tflops: 0.56,
-			},
-			"Ryzen Zen 5 9000 (Ryzen 7)": {
-				tflops: 0.56,
-			},
-			"Ryzen Zen 5 9000 (Ryzen 5)": {
-				tflops: 0.56,
-			},
-			"Ryzen Zen 4 7000 (Ryzen 9)": {
-				tflops: 0.56,
-			},
-			"Ryzen Zen 4 7000 (Ryzen 7)": {
-				tflops: 0.56,
-			},
-			"Ryzen Zen 4 7000 (Ryzen 5)": {
-				tflops: 0.56,
-			},
-			"Ryzen Zen 3 5000 (Ryzen 9)": {
-				tflops: 1.33,
-			},
-			"Ryzen Zen 3 5000 (Ryzen 7)": {
-				tflops: 1.33,
-			},
-			"Ryzen Zen 3 5000 (Ryzen 5)": {
-				tflops: 0.72,
-			},
-			"Ryzen Zen 2 3000 (Ryzen 9)": {
-				tflops: 0.72,
-			},
-			"Ryzen Zen 2 3000 (Ryzen 7)": {
-				tflops: 0.72,
-			},
-			"Ryzen Zen 2 3000 (Ryzen 5)": {
-				tflops: 0.72,
-			},
-			"Ryzen Zen 2 3000 (Ryzen 3)": {
-				tflops: 0.72,
-			},
-			"Ryzen AI 300 (Ryzen AI 9 HX)": {
-				tflops: 5.52,
-			},
-			"Ryzen AI 300 (Ryzen AI 9)": {
-				tflops: 5.2,
-			},
-			"Ryzen AI 300 (Ryzen AI 7)": {
-				tflops: 4.34,
-			},
-			"Ryzen AI 300 (Ryzen AI 5)": {
-				tflops: 1.57,
-			},
-		},
+		AMD: CPU_AMD_SKUS,
 	},
 	"Apple Silicon": {
 		"-": {
