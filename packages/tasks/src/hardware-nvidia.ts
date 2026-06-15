25405e99cb43732d60def7d87f3ab53c2d4ecbaf
Rihaan Meher <sharktidedev@gmail.com>
2026-06-15 09:02:53 -0400

    Add multiple GPUs to hardware-nvidia.ts

--- packages/tasks/src/hardware-nvidia.ts
+++ packages/tasks/src/hardware-nvidia.ts
@@ -28,6 +28,14 @@ export enum NvidiaComputeCapabilities {
 }
 
 export const NVIDIA_SKUS: Record<string, NvidiaHardwareSpec> = {
+	B300: {
+	  tflops: 496.6,
+	  memory: [288],
+	  computeCapability: 10.0,
+	  msrp: 50_000,
+	  power: 1400,
+	  releaseYear: 2026,
+	},
 	B200: {
 		tflops: 496.6,
 		memory: [192],
@@ -43,6 +51,22 @@ export const NVIDIA_SKUS: Record<string, NvidiaHardwareSpec> = {
 		msrp: 32_000,
 		power: 700,
 		releaseYear: 2024,
+	},
+	H20: {
+		tflops: 148,
+		memory: [96],
+		computeCapability: 9.0,
+		msrp: 13_500,
+		power: 400,
+		releaseYear: 2024,
+	},
+	H800: {
+		tflops: 197.9,
+		memory: [80],
+		computeCapability: 9.0,
+		msrp: 30_000,
+		power: 700,
+		releaseYear: 2023,
 	},
 	H100: {
 		tflops: 267.6,
@@ -300,6 +324,14 @@ export const NVIDIA_SKUS: Record<string, NvidiaHardwareSpec> = {
 		power: 95,
 		releaseYear: 2021,
 	},
+	A800: {
+		tflops: 19.49,
+		memory: [40, 80],
+		computeCapability: 8.0,
+		msrp: 12_000,
+		power: 250,
+		releaseYear: 2022,
+	},
 	A100: {
 		tflops: 77.97,
 		memory: [80, 40],
@@ -433,6 +465,22 @@ export const NVIDIA_SKUS: Record<string, NvidiaHardwareSpec> = {
 		memory: [8],
 		computeCapability: 12.0,
 		msrp: 300,
+		power: 100,
+		releaseYear: 2025,
+	},
+	"RTX 5050": {
+		tflops: 13.17,
+		memory: [8],
+		computeCapability: 12.0,
+		msrp: 249,
+		power: 130,
+		releaseYear: 2025,
+	},
+	"RTX 5050 Mobile": {
+		tflops: 13.17,
+		memory: [8],
+		computeCapability: 12.0,
+		msrp: 250,
 		power: 100,
 		releaseYear: 2025,
 	},
