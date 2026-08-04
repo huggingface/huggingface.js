e165299df727f8aefc228f0e2a1d1fd883cac790
bacca87 <bacca87@users.noreply.github.com>
2026-08-04 14:55:03 +0200

    feat: add Piper library

--- packages/tasks/src/model-libraries.ts
+++ packages/tasks/src/model-libraries.ts
@@ -1149,6 +1149,13 @@ export const MODEL_LIBRARIES_UI_ELEMENTS = {
 		filter: false,
 		countDownloads: `path_extension:"pth"`,
 	},
+	piper: {
+		prettyLabel: "Piper",
+		repoName: "Piper",
+		repoUrl: "https://github.com/rhasspy/piper",
+		filter: false,
+		countDownloads: `path_extension:"onnx"`,
+	},
 	"pocket-tts": {
 		prettyLabel: "Pocket-TTS",
 		repoName: "PocketTTS",
